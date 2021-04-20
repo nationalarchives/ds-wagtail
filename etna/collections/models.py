145891ffeb02a621379ddf8eb8b3ef63653617cd
Dan Bentley <dan@numiko.com>
2021-04-20 13:49:15 +0100

    Add comments to collection pages

--- etna/collections/models.py
+++ etna/collections/models.py
@@ -5,7 +5,11 @@ from wagtail.core.models import Page
 
 
 class ExplorerPage(Page):
-    """Collection Explorer landing page."""
+    """Collection Explorer landing page.
+
+    This page is the starting point for a user's journey through the collection
+    explorer.
+    """
 
     introduction = models.CharField(max_length=200, blank=False)
 
@@ -16,7 +20,15 @@ class ExplorerPage(Page):
 
 
 class CategoryPage(Page):
-    """Category page."""
+    """Category page.
+
+    This page represents one of the many categories a user may select in the
+    collection explorer.
+
+    A category page is responsible for listing its child pages, which may be either
+    another CategoryPage (to allow the user to make a more fine-grained choice) or a
+    single ResultsPage (to output the results of their selection).
+    """
 
     introduction = models.CharField(max_length=200, blank=False)
 
@@ -32,7 +44,7 @@ class ResultsPage(Page):
     This page is a placeholder for the results page at the end of a user's
     journey through the collection explorer.
 
-    Eventually, this page will run an editor-defined query against the
+    Eventually this page will run an editor-defined query against the
     collections API and display the results.
     """
 
