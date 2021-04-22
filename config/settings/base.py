2cd4ad9fbb9cb3d38f6fa889b946bec9e30e3818
Dan Bentley <dan@numiko.com>
2021-04-22 14:08:08 +0100

    Allow DEBUG to be set with environment variable

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from distutils.util import strtobool
 import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
+
+DEBUG = strtobool(os.getenv("DEBUG", "False"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
