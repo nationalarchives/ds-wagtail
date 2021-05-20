ae8b002ba6f6a4ceb4fb248f458b002af67fbdcf
Dan Bentley <dan@numiko.com>
2021-05-20 16:04:15 +0100

    Add Kong settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -170,3 +170,10 @@ WAGTAIL_SITE_NAME = "etna"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+
+# Kong client
+
+KONG_CLIENT_BASE_URL = os.getenv("KONG_CLIENT_BASE_URL")
+KONG_CLIENT_TEST_MODE = strtobool(os.getenv("KONG_CLIENT_TEST_MODE", "False"))
+KONG_CLIENT_TEST_FILENAME = os.getenv("KONG_CLIENT_TEST_FILENAME")
