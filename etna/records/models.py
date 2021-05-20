8b4fe07b4f6d4ce1966de96627abe84e696b78d4
Dan Bentley <dan@numiko.com>
2021-05-20 16:11:52 +0100

    Add API resonse fields to RecordPage

--- etna/records/models.py
+++ etna/records/models.py
@@ -14,3 +14,15 @@ class RecordPage(Page):
     see: views.record_page_view
     """
     is_creatable = False
+
+    catalogue_id = models.IntegerField()
+    reference_number = models.IntegerField()
+    closure_status = models.TextField()
+    created_by = models.TextField()
+    description = models.TextField()
+    date_start = models.IntegerField()
+    date_end = models.IntegerField()
+    date_range = models.TextField()
+    legal_status = models.TextField()
+
+    search = SearchManager("records.RecordPage")
