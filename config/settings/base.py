8c3407f53d138bcef8a94fbbc0c27b1550c78e02
Craig Wood <craigwood@numiko.com>
2021-05-26 10:50:05 +0100

    Add flat menus for footer etc.

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
+    'wagtail.contrib.modeladmin',
+
     'wagtail.embeds',
     'wagtail.sites',
     'wagtail.users',
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
     'wagtail.admin',
     'wagtail.core',
 
+    'wagtailmenus',
     'modelcluster',
     'taggit',
 
@@ -80,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
     },
