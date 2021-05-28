7c1c5f1996447e940321864d03095a0514268cac
Dan Bentley <dan@numiko.com>
2021-05-28 11:24:18 +0100

    Add view for disambiguation page

--- etna/records/views.py
+++ etna/records/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 
 from .models import RecordPage
+from ..ciim.exceptions import DoesNotExist
+
+
+def record_page_disambiguation_view(request, reference_number):
+    """View to render a record's details page or disambiguation page if multiple records are found.
+
+    Details pages differ from all other page types within Etna in that their
+    data isn't fetched from the CMS but an external API. And unlike standrard
+    Wagtail pages, this view is accessible from a fixed URL.
+
+    Fetches by reference_number may return multiple results.
+
+    For example ADM 223/3. This is both the catalogue reference for 1 piece and
+    for the 499 item records within:
+
+    https://discovery.nationalarchives.gov.uk/browse/r/h/C4122893
+    """
+    pages = RecordPage.search.filter(reference_number=reference_number)
+    if len(pages) == 0:
+        raise Http404
+
+    if len(pages) > 1:
+        return render(
+            request, "records/record_disambiguation_page.html", {"pages": pages}
+        )
+
+    page = pages[0]
+
+    return render(
+        request,
+        page.get_template(request),
+        {
+            "page": page,
+        },
+    )
 
 
 def record_page_view(request, iaid):
@@ -9,19 +44,11 @@ def record_page_view(request, iaid):
     Details pages differ from all other page types within Etna in that their
     data isn't fetched from the CMS but an external API. And unlike pages, this
     view is accessible from a fixed URL.
-
-    This view will eventually fetch a record from the Kong API by catalogue ID if:
-
-     - nothing is found, 404
-     - a record is found, render the page
-
-    A RecordPage instance exists so we can map the record data to a page,
-    allowing us to take advantage of the site's shared components.
-
-    I haven't yet decided whether this object should be a concrete page type,
-    or - in the spirit of Python - something that looks like a page.
     """
-    page = RecordPage.search.get(iaid)
+    try:
+        page = RecordPage.search.get(iaid)
+    except DoesNotExist:
+        raise Http404
 
     return render(
         request,
