b9b28483e3825537ecd51e0af5b4a9f71c7a9e6b
Dan Bentley <dan@numiko.com>
2021-06-24 14:54:07 +0100

    Fix RecordPage.reference_number mapping issue
    Reference number returned by Kong is a string and not an int

--- etna/records/models.py
+++ etna/records/models.py
@@ -6,17 +6,18 @@ from ..ciim.models import SearchManager
 
 
 class RecordPage(Page):
-    """Non-creatable page used to render record data in templates. 
+    """Non-creatable page used to render record data in templates.
 
     This stub page allows us to use common templates to render external record
     data as though the data was fetched from the CMS.
 
     see: views.record_page_view
     """
+
     is_creatable = False
 
     iaid = models.IntegerField()
-    reference_number = models.IntegerField()
+    reference_number = models.TextField()
     closure_status = models.TextField()
     created_by = models.TextField()
     description = models.TextField()
@@ -29,3 +30,6 @@ class RecordPage(Page):
     parent = models.JSONField(null=True)
 
     search = SearchManager("records.RecordPage")
+
+    def __str__(self):
+        return f"{self.title} ({self.iaid})"
