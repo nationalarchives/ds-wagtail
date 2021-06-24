d10526cedfbc6403dd8941bb12370a0c3bc8f3bb
Dan Bentley <dan@numiko.com>
2021-06-24 14:46:55 +0100

    Fix issue where collectstatic is required before running tests

---
+++ config/settings/test.py
@@ -0,0 +1,5 @@
+from .dev import *  # noqa
+
+# Allow integration tests to run without needing to collectstatic
+# See https://docs.djangoproject.com/en/3.1/ref/contrib/staticfiles/#staticfilesstorage
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
