3e49298c8341687e1635a80cdec485fb96c932cf
Dan Bentley <dan@numiko.com>
2021-06-24 14:46:55 +0100

    Fix issue where collectstatic is required before running tests

--- manage.py
+++ manage.py
@@ -5,6 +5,13 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
+    try:
+        if sys.argv[1] == 'test':
+            os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.test"
+    except IndexError:
+        # No arguments passed to manage.py
+        ...
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
