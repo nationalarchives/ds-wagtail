9a211768eab6478431edcdaf528351b9ff7eff78
AshTNA <ashleyaburnett@gmail.com>
2021-07-05 12:21:59 +0100

    Hardcode homepage cards

--- etna/home/models.py
+++ etna/home/models.py
@@ -2,6 +2,25 @@ from django.db import models
 
 from wagtail.core.models import Page
 
-
 class HomePage(Page):
-    pass
+    def get_context(self, request):
+        context = super().get_context(request)
+
+        # Add extra variables and return the updated context
+        context['etna_index_pages'] = [{
+            "title": "Collection Explorer",
+            "introduction": "A new way to discover collections at The National Archives, through records hand-picked by our experts.",
+            "url": "#"
+        },
+        {
+            "title": "Collection Insights",
+            "introduction": "Learn about the people, themes and events featured in our records, told through words, pictures and audio - discover the human stories behind the collection.",
+            "url": "#"
+        },
+        {
+            "title": "Collection Details",
+            "introduction": "View and navigate records from The National Archives catalogue.",
+            "url": "#"
+        },]
+        return context
+
