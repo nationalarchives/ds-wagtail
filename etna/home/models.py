5ca817827444d636ce082e54a43850d890c91b23
Craig Wood <craigwood@numiko.com>
2021-07-07 09:37:01 +0100

    Add option to select promoted pages to homepage
    [DS-232]

--- etna/home/models.py
+++ etna/home/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    InlinePanel,
+    PageChooserPanel,
+)
+from wagtail.core.fields import RichTextField
+
+
+class HomePageRelatedContent(Orderable):
+    page = ParentalKey('home.HomePage', related_name='related_content')
+    related = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name='Promoted page'
+    )
+
+    panels = [PageChooserPanel('related')]
 
 
 class HomePage(Page):
-    pass
+    introduction = models.CharField(max_length=200, blank=True)
+    information = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('introduction'),
+        InlinePanel('related_content', max_num=3, label='Promoted pages'),
+        FieldPanel('information'),
+    ]
