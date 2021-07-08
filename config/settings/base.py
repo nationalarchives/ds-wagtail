7a153851922b4889afe60ad71a48e3027afd0df2
Craig Wood <craigwood@numiko.com>
2021-07-08 15:04:40 +0100

    Create the InsightsPage framework

--- config/settings/base.py
+++ config/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'etna.collections',
     'etna.ciim',
     'etna.home',
+    'etna.insights',
     'etna.records',
     'etna.search',
     'etna.teasers',
---
+++ etna/insights/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+
+
+from .blocks import InsightsPageStreamBlock
+
+
+class InsightsIndexPage(Page):
+    """InsightsIndexPage
+
+    This page lists the InsightsPage objects that are children of this page.
+    """
+    introduction = models.CharField(max_length=200, blank=False)
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        insights_pages = self.get_children().live()
+        context['insights_pages'] = insights_pages
+        return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('introduction'),
+    ]
+
+    subpage_types = ['insights.InsightsPage']
+
+
+class InsightsPage(Page):
+    """InsightsPage
+
+    The InsightsPage model.
+    """
+    introduction = models.CharField(max_length=200, blank=False)
+    content = StreamField(InsightsPageStreamBlock, blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('introduction'),
+        StreamFieldPanel('content'),
+    ]
+
+    parent_page_types = ['insights.InsightsIndexPage']
+    subpage_types = []
