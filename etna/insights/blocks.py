8a2016711d1cc62adc1fde17cb95baa0e99dedc6
Craig Wood <craigwood@numiko.com>
2021-07-15 16:33:57 +0100

    Create Section heading block

--- etna/insights/blocks.py
+++ etna/insights/blocks.py
@@ -3,9 +3,11 @@ from wagtail.core import blocks
 from ..quotes.blocks import QuoteBlock
 from ..paragraphs.blocks import ParagraphWithHeading
 from ..media_embeds.blocks import MediaEmbedAudioBlock, MediaEmbedVideoBlock
+from ..sections.blocks import SectionHeadingBlock
 
 class InsightsPageStreamBlock(blocks.StreamBlock):
     quote = QuoteBlock()
     paragraph_with_heading = ParagraphWithHeading()
     media_embed_audio = MediaEmbedAudioBlock()
     media_embed_video = MediaEmbedVideoBlock()
+    section_heading = SectionHeadingBlock()
---
+++ etna/sections/blocks.py
@@ -0,0 +1,11 @@
+from wagtail.core import blocks
+
+
+class SectionHeadingBlock(blocks.StructBlock):
+    heading = blocks.CharBlock(required=True, max_length=100)
+    section_id = blocks.CharBlock(required=True, max_length=50)
+
+    class Meta:
+        icon = 'fa-header'
+        label = 'Section heading'
+        template = 'sections/blocks/section-heading.html'
