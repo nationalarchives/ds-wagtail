a3926335c511589133a969bf144d65750c54e0aa
Craig Wood <craigwood@numiko.com>
2021-07-15 15:56:17 +0100

    Make media embed heading field consistent with other blocks

--- etna/media_embeds/blocks.py
+++ etna/media_embeds/blocks.py
@@ -3,7 +3,7 @@ from wagtail.core import blocks
 
 
 class MediaEmbedAudioBlock(blocks.StructBlock):
-    heading = blocks.TextBlock(max_length=100)
+    heading = blocks.CharBlock(required=True, max_length=100)
     datetime = blocks.DateTimeBlock(required=False)
     description = blocks.RichTextBlock(required=False, features=settings.INLINE_RICH_TEXT_FEATURES)
     transcript = blocks.RichTextBlock(required=False, features=settings.INLINE_RICH_TEXT_FEATURES)
@@ -16,7 +16,7 @@ class MediaEmbedAudioBlock(blocks.StructBlock):
 
 
 class MediaEmbedVideoBlock(blocks.StructBlock):
-    heading = blocks.TextBlock(max_length=100)
+    heading = blocks.CharBlock(required=True, max_length=100)
     datetime = blocks.DateTimeBlock(required=False)
     description = blocks.RichTextBlock(required=False, features=settings.INLINE_RICH_TEXT_FEATURES)
     transcript = blocks.RichTextBlock(required=False, features=settings.INLINE_RICH_TEXT_FEATURES)
