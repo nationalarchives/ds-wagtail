fe714b68aeccdfbd896a8b2cd99982152040e45e
Craig Wood <craigwood@numiko.com>
2021-07-19 13:13:13 +0100

    Add media_file_url to audio and video embed blocks
    [DS-235]

--- etna/media_embeds/blocks.py
+++ etna/media_embeds/blocks.py
@@ -1,13 +1,31 @@
 from django.conf import settings
+from django.forms.utils import ErrorList
+from django.core.exceptions import ValidationError
+
 from wagtail.core import blocks
+
+import re
 
 
 class MediaEmbedAudioBlock(blocks.StructBlock):
     heading = blocks.CharBlock(required=True, max_length=100)
     date = blocks.DateBlock(required=False)
     description = blocks.RichTextBlock(required=False, features=settings.INLINE_RICH_TEXT_FEATURES)
+    media_file_url = blocks.URLBlock(required=True, label='Audio file URL')
     transcript = blocks.RichTextBlock(required=False, features=settings.INLINE_RICH_TEXT_FEATURES)
-    # ToDo: Add media file link field
+
+    def clean(self, value):
+        errors = {}
+
+        # Perform a rudimentary check for appropriate file extension in media_file_url field.
+        if value.get('media_file_url'):
+            if not re.search('\.mp3$', value.get('media_file_url')):
+                errors['media_file_url'] = ErrorList(['Media file url field must contain a link to an audio file (.mp3)'])
+
+            if errors:
+                raise ValidationError('Validation error in Media Embed Audio Block', params=errors)
+
+        return super().clean(value)
 
     class Meta:
         icon = 'fa-headphones'
@@ -19,8 +37,21 @@ class MediaEmbedVideoBlock(blocks.StructBlock):
     heading = blocks.CharBlock(required=True, max_length=100)
     date = blocks.DateBlock(required=False)
     description = blocks.RichTextBlock(required=False, features=settings.INLINE_RICH_TEXT_FEATURES)
+    media_file_url = blocks.URLBlock(required=True, label='Video file URL')
     transcript = blocks.RichTextBlock(required=False, features=settings.INLINE_RICH_TEXT_FEATURES)
-    # ToDo: Add media file link field
+
+    def clean(self, value):
+        errors = {}
+
+        # Perform a rudimentary check for appropriate file extension in media_file_url field.
+        if value.get('media_file_url'):
+            if not re.search('\.mp4$', value.get('media_file_url')):
+                errors['media_file_url'] = ErrorList(['Media file url field must contain a link to a video file (.mp4)'])
+
+            if errors:
+                raise ValidationError('Validation error in Media Embed Video Block', params=errors)
+
+        return super().clean(value)
 
     class Meta:
         icon = 'fa-video-camera'
