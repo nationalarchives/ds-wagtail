5794dc5f788735407f838836d85fe555cfbe628b
Dan Bentley <dan@numiko.com>
2021-07-23 11:57:18 +0100

    Implement jumplinks

---
+++ etna/insights/tests/test_blocks.py
@@ -0,0 +1,56 @@
+import json
+
+from django.test import TestCase
+
+from wagtail.core.models import Site
+
+from ..models import InsightsPage
+
+
+class TestInsightPageSectionBlockIntegration(TestCase):
+    def setUp(self):
+        root = Site.objects.get().root_page
+
+        self.insights_page = InsightsPage(
+            title="Insights page", introduction="Introduction"
+        )
+        self.insights_page.body = json.dumps(
+            [
+                {
+                    "type": "section",
+                    "value": {"heading": "Section One"},
+                },
+                {
+                    "type": "section",
+                    "value": {"heading": "Section Two"},
+                },
+            ]
+        )
+        root.add_child(instance=self.insights_page)
+
+    def test_jumplinks_rendered(self):
+        response = self.client.get(self.insights_page.get_url())
+        self.assertContains(response, "jumplinks")
+
+    def test_section_one_id(self):
+        response = self.client.get(self.insights_page.get_url())
+        self.assertContains(response, 'id="section-one"')
+
+    def test_section_one_url(self):
+        response = self.client.get(self.insights_page.get_url())
+        self.assertContains(response, 'href="#section-one"')
+
+    def test_section_two_id(self):
+        response = self.client.get(self.insights_page.get_url())
+        self.assertContains(response, 'id="section-two"')
+
+    def test_section_two_url(self):
+        response = self.client.get(self.insights_page.get_url())
+        self.assertContains(response, 'href="#section-two"')
+
+    def test_jumplinks_not_rendered_if_page_has_no_sections(self):
+        self.insights_page.body = json.dumps([])
+        self.insights_page.save()
+
+        response = self.client.get(self.insights_page.get_url())
+        self.assertNotContains(response, "jumplinks")
