c98b076f9774fd16be467e99036c4a9b0781d31a
Craig Wood <craigwood@numiko.com>
2021-07-23 11:11:25 +0100

    Move PromotedItemBlock to insights app
    [DS-277]

--- etna/collections/blocks.py
+++ etna/collections/blocks.py
@@ -2,7 +2,6 @@ from django.conf import settings
 
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
-from wagtail.snippets.blocks import SnippetChooserBlock
 
 
 class CollectionHighlightsBlock(blocks.StructBlock):
@@ -22,27 +21,6 @@ class FeaturedPageBlock(blocks.StructBlock):
         template = "collections/blocks/featured_page.html"
         help_text = "Block used feature a page from within Wagtail"
         icon = "fa-arrow-up"
-
-
-class PromotedSingleItemBlock(blocks.StructBlock):
-    title = blocks.CharBlock(max_length=100, help_text="Title of the promoted page")
-    category = SnippetChooserBlock("categories.Category")
-    publication_date = blocks.DateBlock()
-    url = blocks.URLBlock(label="external URL", help_text="URL for the external page")
-    cta_label = blocks.CharBlock(label="CTA label", max_length=50, help_text="The button label")
-    teaser_image = ImageChooserBlock(
-        help_text="An image used to create a teaser for the promoted page"
-    )
-    teaser_alt_text = blocks.CharBlock(max_length=100, help_text="Alt text of the teaser image")
-    description = blocks.RichTextBlock(
-        features=settings.INLINE_RICH_TEXT_FEATURES,
-        help_text="A description of the promoted page"
-    )
-
-    class Meta:
-        template = "collections/blocks/promoted_single_item.html"
-        help_text = "Block used promote an external page"
-        icon = "fa-star"
 
 
 class PromotedItemBlock(blocks.StructBlock):
--- etna/insights/blocks.py
+++ etna/insights/blocks.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.snippets.blocks import SnippetChooserBlock
 
 from ..authors.blocks import AuthorBlock
-from ..collections.blocks import PromotedSingleItemBlock
 from ..paragraphs.blocks import ParagraphWithHeading
 from ..quotes.blocks import QuoteBlock
 from ..records.blocks import RecordChooserBlock
@@ -30,10 +31,31 @@ class FeaturedRecordsBlock(blocks.StructBlock):
         template = "insights/blocks/featured_records.html"
 
 
+class PromotedItemBlock(blocks.StructBlock):
+    title = blocks.CharBlock(max_length=100, help_text="Title of the promoted page")
+    category = SnippetChooserBlock("categories.Category")
+    publication_date = blocks.DateBlock()
+    url = blocks.URLBlock(label="external URL", help_text="URL for the external page")
+    cta_label = blocks.CharBlock(label="CTA label", max_length=50, help_text="The button label")
+    teaser_image = ImageChooserBlock(
+        help_text="An image used to create a teaser for the promoted page"
+    )
+    teaser_alt_text = blocks.CharBlock(max_length=100, help_text="Alt text of the teaser image")
+    description = blocks.RichTextBlock(
+        features=settings.INLINE_RICH_TEXT_FEATURES,
+        help_text="A description of the promoted page"
+    )
+
+    class Meta:
+        template = "insights/blocks/promoted_item.html"
+        help_text = "Block used promote an external page"
+        icon = "fa-star"
+
+
 class InsightsPageStreamBlock(blocks.StreamBlock):
     author = AuthorBlock()
     paragraph_with_heading = ParagraphWithHeading()
-    promoted_item = PromotedSingleItemBlock()
+    promoted_item = PromotedItemBlock()
     featured_record = FeaturedRecordBlock()
     featured_records = FeaturedRecordsBlock()
     section = SectionBlock()
