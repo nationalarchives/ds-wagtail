9a426f82c5af5b859e70aba2d05f9c74f0d1cc33
Craig Wood <craigwood@numiko.com>
2021-07-26 17:18:35 +0100

    Create model for authors
    Replace block content with chooser field
    
    [DS-283]

--- etna/authors/blocks.py
+++ etna/authors/blocks.py
@@ -1,17 +1,12 @@
 from django.conf import settings
 
 from wagtail.core import blocks
-from wagtail.images.blocks import ImageChooserBlock
+from wagtail.snippets.blocks import SnippetChooserBlock
 
 
 class AuthorBlock(blocks.StructBlock):
-    name = blocks.CharBlock(required=True, max_length=100)
-    role = blocks.CharBlock(required=False, max_length=100)
-    summary = blocks.RichTextBlock(required=False, features=settings.INLINE_RICH_TEXT_FEATURES)
-    image = ImageChooserBlock(required=False)
-    bio_link = blocks.URLBlock(required=True, help_text="Link to external bio page")
-    bio_link_label = blocks.CharBlock(required=True, help_text="Button text for bio link", max_length=50)
+    author = SnippetChooserBlock("authors.Author")
 
     class Meta:
         icon = "fa-user-circle "
-        template = "insights/blocks/author.html"
+        template = "authors/blocks/author.html"
