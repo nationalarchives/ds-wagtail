00a0533d63fe52005e187f58de12686508625dcc
Dan Bentley <dan@numiko.com>
2021-07-27 12:44:29 +0100

    Refactor SearchManager down into smaller classes

--- etna/ciim/models.py
+++ etna/ciim/models.py
@@ -1,3 +1,5 @@
+from abc import ABC
+
 from django.apps import apps
 from django.conf import settings
 
@@ -11,33 +13,79 @@ from .exceptions import (
 from .utils import translate_result
 
 
-class ResultsIterator:
-    def __init__(self, model, results):
-        self.model = model
-        self.results = results
-        self.index = 0
-
-    def __next__(self):
-        try:
-            result = self.results[self.index]
-        except IndexError:
-            raise StopIteration
-
-        self.index += 1
-
-        translated_result = translate_result(result)
-        return apps.get_model(self.model)(**translated_result)
-
-
 class SearchManager:
     """A model.Manager/QuerySet-like object to query and fetch KongModel instances."""
 
     def __init__(self, model):
         self.model = model
-        self._response = {}
-        self._results = []
-        self._query = {}
-        self._cache = {}
+
+    def filter(self, **kwargs):
+        query = kwargs
+        return SearchQuery(self.model, query)
+
+    def get(self, iaid=None, reference_number=None):
+        return FetchQuery(self.model).get(iaid=iaid, reference_number=reference_number)
+
+    def get_multiple(self, iaid=None):
+        return FetchQuery(self.model).get_multiple(iaid=iaid)
+
+
+class ResultPending(Exception):
+    """Raised if result being requested that hasn't yet been fetched."""
+
+
+class ResultSet:
+    """Wrapper for a list of ES search results.
+
+    Contains a list the size of the ES results set with each item being None or
+    a dict.
+
+    If None is accessed from ResultSet, client is informed that the result
+    hasn't yet been fetched from ES.
+    """
+
+    def __init__(self, total_count=0):
+        self.results = [None] * total_count
+
+    def __getitem__(self, index):
+        """Support subscripting.
+
+        Raise IndexError if requested item is not in list
+        or ResultPending if item hasn't been fetched yet.
+        """
+        result = self.results[index]
+        if not result:
+            raise ResultPending
+
+        return result
+
+    def add(self, results, start=0):
+        """Add a list of results at a specified point."""
+        for index, result in zip(range(start, start + len(results)), results):
+            self.results[index] = result
+
+
+class SearchQueryIterator:
+    """Support iterating over a SearchQuery's results."""
+
+    def __init__(self, search_query):
+        self.search_query = search_query
+        self.index = 0
+
+    def __next__(self):
+
+        try:
+            result = self.search_query[self.index]
+        except IndexError:
+            raise StopIteration
+
+        self.index += 1
+
+        return result
+
+
+class Query(ABC):
+    """Abstract base class to inherit from to create a Query"""
 
     @property
     def client(self):
@@ -49,49 +97,33 @@ class SearchManager:
             test_file_path=settings.KONG_CLIENT_TEST_FILENAME,
         )
 
-    def filter(self, **kwargs):
-        self._query = kwargs
 
-        return self
+class SearchQuery(Query):
+    """Support queries to Kong's /search method.
 
-    def get(self, iaid=None, reference_number=None):
-        response = self.client.fetch(iaid=iaid, reference_number=reference_number)
+    Once configured via SearchManager.filter, this class is responsible for
+    fetching and transforming responses into model instances.
+    """
 
-        results = response["hits"]["hits"]
-        results_count = response["hits"]["total"]["value"]
-        if results_count == 0:
-            raise DoesNotExist
-        if results_count > 1:
-            raise MultipleObjectsReturned
+    BATCH_SIZE = 10
 
-        result = results[0]
+    def __init__(self, model, query):
+        self.model = model
+        self._response = {}
+        self._results = []
+        self._query = query
 
-        data = translate_result(result)
-        data['_debug_kong_result'] = result
+    def _fetch(self, start=0, size=1):
+        """Fetch and update ResultSet"""
+        self._response = self.client.search(start=start, size=size, **self._query)
 
-        return apps.get_model(self.model)(**data)
+        if not self._results:
+            self._results = ResultSet(
+                total_count=self._response["hits"]["total"]["value"]
+            )
 
-    def get_multiple(self, iaid=None):
-        """Temporary method created to fetch multiple items.
-
-        Currently uses multiple calls to SearchManager.get. Not suitable
-        for production code"""
-        ids = iaid or []
-
-        records = []
-
-        for iaid in ids:
-            try:
-                instance = self.get(iaid=iaid)
-            except (KongError, DoesNotExist):
-                continue
-            records.append(instance)
-
-        return records
-
-    def _fetch(self):
-        self._response = self.client.search(**self._query)
-        self._results = self._response["hits"]["hits"]
+        if hits := self._response["hits"]["hits"]:
+            self._results.add(hits, start)
 
     def __getitem__(self, key):
         """Support subscripts to allow results set to be paginated.
@@ -124,30 +156,76 @@ class SearchManager:
 
         start = key.start
         stop = key.stop or 0
-
         count = max(1, stop - start)
+        is_single_item_fetch = key.start is not None and not key.stop
 
-        self._query["start"] = start
-        self._query["size"] = count
+        if is_single_item_fetch:
+            try:
+                result = self._results[key.start]
+            except (ResultPending, IndexError):
+                self._fetch(start=key.start, size=self.BATCH_SIZE)
+                result = self._results[key.start]
 
-        self._fetch()
-
-        # If positional fetch, return single item, otherwise return all fetched results.
-        if key.start is not None and not key.stop:
-            translated_result = translate_result(self._results[key.start])
+            translated_result = translate_result(result)
             return apps.get_model(self.model)(**translated_result)
-        else:
-            translated_results = [translate_result(r) for r in self._results]
-            return [apps.get_model(self.model)(**r) for r in translated_results]
+
+        self._fetch(start=key.start, size=count)
+        translated_results = [translate_result(r) for r in self._results[key]]
+        return [apps.get_model(self.model)(**r) for r in translated_results]
 
     def __len__(self):
         """Support len()"""
         return self.count()
 
     def count(self):
-        self._fetch()
+        self._fetch(start=0, size=0)
         return self._response["hits"]["total"]["value"]
 
     def __iter__(self):
-        self._fetch()
-        return ResultsIterator(self.model, self._results)
+        return SearchQueryIterator(self)
+
+
+class FetchQuery(Query):
+    """Support queries to Kong's /fetch method.
+
+    Once configured via SearchManager.get, this class is responsible for
+    fetching and transforming responses into model instances.
+    """
+
+    def __init__(self, model):
+        self.model = model
+
+    def get(self, iaid=None, reference_number=None):
+        response = self.client.fetch(iaid=iaid, reference_number=reference_number)
+
+        results = response["hits"]["hits"]
+        results_count = response["hits"]["total"]["value"]
+        if results_count == 0:
+            raise DoesNotExist
+        if results_count > 1:
+            raise MultipleObjectsReturned
+
+        result = results[0]
+
+        data = translate_result(result)
+        data["_debug_kong_result"] = result
+
+        return apps.get_model(self.model)(**data)
+
+    def get_multiple(self, iaid=None):
+        """Temporary method created to fetch multiple items.
+
+        Currently uses multiple calls to SearchManager.get. Not suitable
+        for production code"""
+        ids = iaid or []
+
+        records = []
+
+        for iaid in ids:
+            try:
+                instance = self.get(iaid=iaid)
+            except (KongError, DoesNotExist):
+                continue
+            records.append(instance)
+
+        return records
