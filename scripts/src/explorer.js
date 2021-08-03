3143f8f14f601c9e4ff85ca22c57038401ec4724
Gwyn Jones <gtvj@users.noreply.github.com>
2021-08-03 16:41:38 +0100

    Introduce analytics front end
    This commit:
    
    1. Introduces the GTM data-* attributes
    2. Introduces JavaScript to manage specific GTM
       attributes which could not be achieved on the back end
    3. Removes some redundant includes (for alerts)

---
+++ scripts/src/explorer.js
@@ -0,0 +1,9 @@
+import {add_analytics_data_card_position} from './modules/analytics_card_position'
+
+add_analytics_data_card_position('.card-group-promo--green');
+add_analytics_data_card_position('.card-group-secondary-nav__link');
+add_analytics_data_card_position('.card-group-record-summary');
+add_analytics_data_card_position('.card-group--list-style-none');
+add_analytics_data_card_position('.card-group--no-flex');
+add_analytics_data_card_position('.card-group-promo__card');
+add_analytics_data_card_position('.card-group-promo__card-heading a');
