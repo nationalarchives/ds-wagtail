e6fd90dd87472c014f702105e06c808cbfb94d6d
Craig Wood <craigwood@numiko.com>
2021-08-12 14:23:48 +0100

    Define allowed audio/video media types in the module
    [DS-318]

--- etna/media/forms.py
+++ etna/media/forms.py
@@ -11,6 +11,11 @@ from wagtail.admin.forms.collections import BaseCollectionMemberForm
 from django.forms.models import modelform_factory
 
 
+# Valid audio/video media types - https://www.iana.org/assignments/media-types/media-types.xhtml
+ALLOWED_AUDIO_MIME_TYPES = ["audio/mpeg",]
+ALLOWED_VIDEO_MIME_TYPES = ["video/mp4",]
+
+
 class BaseMediaForm(BaseCollectionMemberForm):
     class Meta:
         widgets = {
@@ -24,7 +29,8 @@ class BaseMediaForm(BaseCollectionMemberForm):
     def __init__(self, *args, **kwargs):
         super(BaseMediaForm, self).__init__(*args, **kwargs)
 
-        # Remove unused fields.
+        # Remove unused fields - as is done for audio only in wagtailmedia BaseMediaForm.__init__
+        # https://github.com/torchbox/wagtailmedia/blob/28f0df495661d8fc9f4e573daf3b446a55bbbfab/wagtailmedia/forms.py#L38
         for name in ("width", "height", "thumbnail"):
             if name in self.fields:
                 del self.fields[name]
@@ -33,14 +39,6 @@ class BaseMediaForm(BaseCollectionMemberForm):
         error_messages = {
             "mime_type": "%(filename)s (%(mime_type)s) is not an allowed %(type)s file.",
         }
-
-        audio_types = [
-            "audio/mpeg",
-        ]
-
-        video_types = [
-            "video/mp4",
-        ]
 
         data = self.cleaned_data['file']
         mime_type = magic.from_buffer(data.read(), mime=True)
@@ -54,10 +52,10 @@ class BaseMediaForm(BaseCollectionMemberForm):
 
         # Ensure file is a valid type for the current instance.
         if self.instance.type == "audio":
-            if mime_type not in audio_types:
+            if mime_type not in ALLOWED_AUDIO_MIME_TYPES:
                 raise ValidationError(error_messages["mime_type"], params=params)
         else:
-            if mime_type not in video_types:
+            if mime_type not in ALLOWED_VIDEO_MIME_TYPES:
                 raise ValidationError(error_messages["mime_type"], params=params)
 
         return data
