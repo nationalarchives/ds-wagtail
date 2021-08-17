e3cec3ba774259e7871d0ac9b954fb5c554489e7
Dan Bentley <dan@numiko.com>
2021-08-17 11:38:03 +0100

    Add tests for fallback image thumbnail

---
+++ etna/records/tests/test_models.py
@@ -0,0 +1,53 @@
+from django.test import TestCase, override_settings
+
+import responses
+
+from ...ciim.tests.factories import create_response, create_media
+from ..models import Image
+
+
+@override_settings(
+    KONG_CLIENT_BASE_URL="https://kong.test",
+    KONG_IMAGE_PREVIEW_BASE_URL="https://media.preview/",
+    KONG_CLIENT_TEST_MODE=False,
+)
+class ImageTestCase(TestCase):
+    @responses.activate
+    def test_thumbnail_url(self):
+        responses.add(
+            responses.GET,
+            "https://kong.test/search",
+            json=create_response(
+                records=[
+                    create_media(
+                        thumbnail_location="path/to/thumbnail.jpeg",
+                        location="path/to/image.jpeg",
+                    ),
+                ]
+            ),
+        )
+
+        images = Image.search.filter(rid="")
+        image = images[0]
+
+        self.assertEquals(image.thumbnail_url, "https://media.preview/path/to/thumbnail.jpeg")
+
+    @responses.activate
+    def test_thumbnail_url_fallback(self):
+        responses.add(
+            responses.GET,
+            "https://kong.test/search",
+            json=create_response(
+                records=[
+                    create_media(
+                        thumbnail_location=None, location="path/to/image.jpeg"
+                    ),
+                ]
+            ),
+        )
+
+        images = Image.search.filter(rid="")
+        image = images[0]
+
+        # Fallback serves image through Wagtail instead of from kong
+        self.assertEquals(image.thumbnail_url, "/records/image/path/to/image.jpeg")
