88f1b82d60a9619c6b0f48a2537c2684a53f340a
Dan Bentley <dan@numiko.com>
2021-08-25 10:06:56 +0100

    Update import script to import latest users

--- etna/users/management/commands/import_users.py
+++ etna/users/management/commands/import_users.py
@@ -9,8 +9,8 @@ User = get_user_model()
 def get_users(filename):
     with open(filename, "r") as f:
         for email in f.read().splitlines():
-            username, domain = email.split("@")
-            first_name, last_name = username.split(".")
+            first_name, domain = email.split("@")
+            last_name = "Beta tester"
 
             yield first_name, last_name, email
 
