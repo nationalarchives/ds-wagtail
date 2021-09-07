9c29b44599c6e32b27a356b271b2f6ccd79316da
JamesWChan <jwchan1993@gmail.com>
2021-09-07 13:34:27 +0100

    Fix focus style issue on image viewer canvas

--- scripts/src/image-viewer.js
+++ scripts/src/image-viewer.js
@@ -36,4 +36,18 @@ if (image_source) {
             full_screen_button.focus();
         }
     });
+
+    const canvas = document.getElementsByClassName("openseadragon-canvas")[0];
+    const header = document.getElementsByClassName("image-viewer__header")[0];
+    const footer = document.getElementsByClassName("image-viewer__footer")[0];
+
+    canvas.addEventListener("focus", () => {
+        header.style.borderBottom = "solid 0.3125rem #8EC0EC";
+        footer.style.borderTop = "solid 0.3125rem #8EC0EC";
+    });
+
+    canvas.addEventListener("blur", () => {
+        header.style.borderBottom = "none";
+        footer.style.borderTop = "none";
+    });
 }
