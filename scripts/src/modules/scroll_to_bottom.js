156840582bc74781bd337bc6c4b19a00e4282dda
JamesWChan <jwchan1993@gmail.com>
2021-09-17 14:50:19 +0100

    Refactor scroll_to_bottom so that it works in IE11

--- scripts/src/modules/scroll_to_bottom.js
+++ scripts/src/modules/scroll_to_bottom.js
@@ -1,7 +1,13 @@
-const scroll_to_bottom = (element) => {
-	if(element){
-		element.scrollTo(0, element.scrollHeight)
+const scroll_to_bottom = (element, container) => {
+	try{
+		const elementRect = element.getBoundingClientRect();
+		const scrollY = elementRect.top;
+		container.scrollLeft = 0;
+		container.scrollTop = scrollY;
 	}
-};
+	catch(e){
+		console.error(e)
+	}
+}
 
 export default scroll_to_bottom;
