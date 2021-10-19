1fff929905545e6018878c5c932e32595d341c4a
AshTNA <ashleyaburnett@gmail.com>
2021-10-19 11:21:52 +0100

    Add mark_safe to block help text

--- etna/media/blocks.py
+++ etna/media/blocks.py
@@ -3,6 +3,7 @@ from django.utils.html import format_html, format_html_join
 from django.template.loader import render_to_string
 from django.core.exceptions import ValidationError
 from django.forms.utils import ErrorList
+from django.utils.html import mark_safe, escape
 
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
@@ -41,8 +42,20 @@ class ImageBlock(blocks.StructBlock):
     An image block which encourages accessibility-first design.
     """
     image = ImageChooserBlock(required=True)
-    decorative = blocks.BooleanBlock(label="Is this image decorative?", help_text="Decorative images are used for visual enhancement, and do not add context to the page. Guidance for determining wether your image is decorative: https://www.w3.org/WAI/tutorials/images/decorative/", required=False, default=False)
-    alt_text = blocks.CharBlock(max_length=100, label="Image alternative text", help_text="Used to describe images when they fail to load, or for users of assistive technologies. Enter text between 10-100 characters. Required if your image is not decorative. Guidance for writing alt text: https://html.spec.whatwg.org/multipage/images.html#alt", required=False)
+    decorative = blocks.BooleanBlock(label="Is this image decorative?", help_text=
+    mark_safe("%s <a href=%s target=%s>%s</a>" % (
+            escape("Decorative images are used for visual enhancement, and do not add context to the page. Guidance for determining wether your image is decorative: "),
+            escape("https://www.w3.org/WAI/tutorials/images/decorative/"),
+            escape("_blank"),
+            escape("https://www.w3.org/WAI/tutorials/images/decorative/")
+        )), required=False, default=False)
+    alt_text = blocks.CharBlock(max_length=100, label="Image alternative text", help_text=
+    mark_safe("%s <a href=%s target=%s>%s</a>" % (
+            escape("Used to describe images when they fail to load, or for users of assistive technologies. Enter text between 10-100 characters. Required if your image is not decorative. Guidance for writing alt text: "),
+            escape("https://html.spec.whatwg.org/multipage/images.html#alt"),
+            escape("_blank"),
+            escape("https://html.spec.whatwg.org/multipage/images.html#alt")
+    )), required=False)
     caption = blocks.RichTextBlock(features=['link'], help_text="An optional caption for non-decorative images, which will be displayed directly below the image. This could be used for image sources or for other useful metadata.", label="Caption (optional)", required=False)
 
     def clean(self, value):
