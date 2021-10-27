556879d762f228abd3a8af055ee2955fd670aa01
AshTNA <ashleyaburnett@gmail.com>
2021-10-27 11:42:30 +0100

    Replace mark_safe/escape with format_html

--- etna/media/blocks.py
+++ etna/media/blocks.py
@@ -3,7 +3,7 @@ from django.utils.html import format_html, format_html_join
 from django.template.loader import render_to_string
 from django.core.exceptions import ValidationError
 from django.forms.utils import ErrorList
-from django.utils.html import mark_safe, escape
+from django.utils.html import format_html
 
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
@@ -41,28 +41,27 @@ class ImageBlock(blocks.StructBlock):
     An image block which allows editors to ensure accessibility is reflected on the page.
     """
     image = ImageChooserBlock(required=True)
-    decorative = blocks.BooleanBlock(
-        
-    label=mark_safe("%s <p style='font-size: 11px;'>%s</p>" % (
-    
-    escape("Is this image decorative?"),
-    escape("Tick the box if 'yes'")
-    
+    decorative = blocks.BooleanBlock( 
+    label=format_html("%s <p style='font-size: 11px;'>%s</p>" % (
+    "Is this image decorative?",
+    "Tick the box if 'yes'"   
     )), 
     help_text=
-    mark_safe("%s <a href=%s target=%s>%s</a>." % (
-            escape("Decorative images are used for visual effect and do not add information to the content of a page."),
-            escape("https://www.w3.org/WAI/tutorials/images/decorative/"),
-            escape("_blank"),
-            escape("Check the guidance to see if your image is decorative")
+    format_html("%s <a href=%s target=%s>%s</a>." % (
+            "Decorative images are used for visual effect and do not add information to the content of a page.",
+            "https://www.w3.org/WAI/tutorials/images/decorative/",
+            "_blank",
+            "Check the guidance to see if your image is decorative"
         )), required=False, default=False)
+
     alt_text = blocks.CharBlock(max_length=100, label="Image alternative text", help_text=
-    mark_safe("%s <a href=%s target=%s>%s</a>." % (
-            escape("Alternative (alt) text describes images when they fail to load, and is read aloud by assistive technologies. Use a maximum of 100 characters to describe your image. Decorative images do not require alt text."),
-            escape("https://html.spec.whatwg.org/multipage/images.html#alt"),
-            escape("_blank"),
-            escape("Check the guidance for tips on writing alt text")
+    format_html("%s <a href=%s target=%s>%s</a>." % (
+            "Alternative (alt) text describes images when they fail to load, and is read aloud by assistive technologies. Use a maximum of 100 characters to describe your image. Decorative images do not require alt text.",
+            "https://html.spec.whatwg.org/multipage/images.html#alt",
+            "_blank",
+            "Check the guidance for tips on writing alt text"
     )), required=False)
+
     caption = blocks.RichTextBlock(features=['link'], help_text="An optional caption for non-decorative images, which will be displayed directly below the image. This could be used for image sources or for other useful metadata.", label="Caption (optional)", required=False)
 
     def clean(self, value):
