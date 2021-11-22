d058d844a269631e881641cbdfa1c51a40e670df
Andy Babic <andy.babic@torchbox.com>
2021-11-22 15:59:44 +0000

    Add AppConfig classes for each app with 'default_auto_field' specified

---
+++ etna/categories/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CategoriesAppConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
+    name = "etna.categories"
+    verbose_name = "Categories"
---
+++ etna/ciim/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CIIMAppConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
+    name = "etna.ciim"
+    verbose_name = "CIIM"
