708b1424a93635eb17286cf925ed9d048f6813fa
Andy Babic <andy.babic@torchbox.com>
2021-12-14 10:29:09 +0000

    Use the decorate_urls helper from Wagtail to apply default caching headers to all non page URLs

--- config/urls.py
+++ config/urls.py
@@ -2,24 +2,34 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import include, path, register_converter
+from django.views.decorators.cache import never_cache
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.utils.urlpatterns import decorate_urlpatterns
 
+from etna.core.cache_control import (
+    apply_default_cache_control,
+    apply_default_vary_headers,
+)
 from etna.records import converters
 from etna.records import views as records_views
 
 register_converter(converters.ReferenceNumberConverter, "reference_number")
 register_converter(converters.IAIDConverter, "iaid")
 
-# fmt: off
-urlpatterns = [
+
+# Private URLs that are not meant to be cached.
+private_urls = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
-
+    path('accounts/', include('allauth.urls')),
     path("documents/", include(wagtaildocs_urls)),
+]
 
+# Public URLs that are meant to be cached.
+public_urls = [
     path(
         r"catalogue/<iaid:iaid>/",
         login_required(records_views.record_page_view),
@@ -36,38 +46,43 @@ urlpatterns = [
         records_views.image_serve,
         name="image-serve",
     ),
-
     path(
         r"records/images/<iaid:iaid>/<str:sort>/",
         login_required(records_views.image_viewer),
         name="image-viewer",
     ),
-
     path(
         r"records/images/<iaid:iaid>/",
         login_required(records_views.image_browse),
         name="image-browse",
     ),
 ]
-# fmt: on
-
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    public_urls += staticfiles_urlpatterns()
+    public_urls += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = urlpatterns + [
-    path('accounts/', include('allauth.urls')),
 
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
-]
+# Update public URLs to use the "default" cache settings.
+public_urls = decorate_urlpatterns(public_urls, apply_default_cache_control)
+
+# Set vary headers for public URLS to instruct cache to serve different version on
+# different cookies, different request method (e.g. AJAX) and different protocol
+# (http vs https).
+public_urls = decorate_urlpatterns(public_urls, apply_default_vary_headers)
+
+# Update private URLs to use the "never cache" cache settings.
+private_urls = decorate_urlpatterns(private_urls, never_cache)
+
+# Join private and public URLs.
+urlpatterns = (
+    private_urls + public_urls + [
+        # Wagtail URLs are added at the end.
+        # cache-control is applied to the page models's serve methods
+        path("", include(wagtail_urls)),
+    ]
+)
