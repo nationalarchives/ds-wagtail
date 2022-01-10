a691f4c5cf7443edec217be2d906e912211d7829
Andy Babic <andy.babic@torchbox.com>
2022-01-10 11:50:33 +0000

    Improvements to enable_platform_shh:
    * Use os.path.expanduser to get a platform-specific known_hosts path
    
    * Use ssh-keygen to remove existing keys before adding a new one

--- fabfile.py
+++ fabfile.py
@@ -240,8 +240,12 @@ def pull_staging_images(c):
 
 
 def enable_platform_ssh(c):
-    local("ssh-keyscan 'ssh.uk-1.platform.sh' >> $HOME/.ssh/known_hosts")
-    local("ssh-keyscan 'git.uk-1.platform.sh' >> $HOME/.ssh/known_hosts")
+    known_hosts = os.path.expanduser("~/.ssh/known_hosts")
+    for hostname in ('ssh.uk-1.platform.sh', 'git.uk-1.platform.sh'):
+        # Remove existing keys for this hostname
+        local(f"ssh-keygen -q -R {hostname} >> {known_hosts}")
+        # Add new ones
+        local(f"ssh-keyscan {hostname} >> {known_hosts}")
 
 
 def pull_database_from_platform(c, environment_name):
