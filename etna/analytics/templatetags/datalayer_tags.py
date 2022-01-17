26e8a3ac440208137e0de12f23f40dda47a72fc3
Dan Bentley <dan@numiko.com>
2022-01-17 15:48:35 +0000

    Add RecordPage-specific template tag for datalayer data.
    Now that RecordPage is no longer a wagtail.core.Page or models.Model
    instance, it's no longer compatible with the standard datalayer tag.
    
    Created separate template tag and removed RecordPage-specific properties
    from datalayer tag.

--- etna/analytics/templatetags/datalayer_tags.py
+++ etna/analytics/templatetags/datalayer_tags.py
@@ -22,24 +22,12 @@ def get_content_group(page):
     return groups.get(appname, page.title)
 
 
-def get_availability_condition(page):
-    """
-    Return the page's availablility condition value (if applicable).
-    """
-    try:
-        return page.availablility_delivery_condition
-    except AttributeError:
-        return ""
-
-
-def get_availability_condition_category(page):
+def get_availability_condition_category(availability_condition):
     """
     Return a category for the availability condition of the page.
 
     If an availability condition is not present, return an empty string.
     """
-    availability_condition = get_availability_condition(page)
-
     return settings.AVAILABILITY_CONDITION_CATEGORIES.get(availability_condition, "")
 
 
@@ -86,9 +74,9 @@ def datalayer(page, request) -> dict:
         # CatalogueDataSource where applicable. Empty string if not applicable.
         "customDimension15": "",
         # Availability condition category where applicable.  Empty string if not applicable.
-        "customDimension16": get_availability_condition_category(page),
+        "customDimension16": "",
         # Availability condition where applicable.  Empty string if not applicable.
-        "customDimension17": get_availability_condition(page),
+        "customDimension17": "",
     }
 
 
@@ -103,6 +91,61 @@ def image_browse_datalayer(context) -> dict:
         "contentGroup1": "TNA catalogue",
         "customDimension1": "offsite",
         # The user type and is private beta specific - the user ID for participants.
-        "customDimension2": context['request'].user.id,
-        "customDimension18": context.get("images_count", ''),
+        "customDimension2": context["request"].user.id,
+        "customDimension18": context.get("images_count", ""),
+    }
+
+
+@register.simple_tag(takes_context=True)
+def record_page_datalayer(context) -> dict:
+    """Custom tag for image browse datalayer.
+
+    Image browse isn't a page served by Wagtail, therefore we're unable to
+    query the page object to populate the datalayer.
+    """
+    user = context["request"].user
+    page = context["page"]
+    availability_condition = page.availability_delivery_condition
+    availability_condition_category = get_availability_condition_category(
+        availability_condition
+    )
+
+    return {
+        # The name of the content group - [Always has a value]
+        "contentGroup1": get_content_group(page),
+        # The reader type (options are "offsite",
+        # "onsite_public", "onsite_staff", "subscription")
+        "customDimension1": "offsite",
+        # ser ID for participants.
+        "customDimension2": user.id,
+        # The page type - [Always has a value]
+        "customDimension3": "Record page",
+        # Taxonomy topics for the page, delineated by semi-colons. Empty string if no value.
+        "customDimension4": "",
+        # Taxonomy sub topic where applicable. Empty string if not applicable.
+        "customDimension5": "",
+        # Taxonomy term where applicable. Empty string if not applicable.
+        "customDimension6": "",
+        # Time period where applicable. Empty string if not applicable.
+        "customDimension7": "",
+        # Sub time period where applicable. Empty string if not applicable.
+        "customDimension8": "",
+        # Entity type where applicable. Empty string if not applicable.
+        "customDimension9": "",
+        # Entity label where applicable. Empty string if not applicable.
+        "customDimension10": "",
+        # Catalogue repository where applicable. Empty string if not applicable.
+        "customDimension11": "",
+        # Catalogue level where applicable. Empty string if not applicable.
+        "customDimension12": "",
+        # Catalogue series where applicable. Empty string if not applicable.
+        "customDimension13": "",
+        # Catalogue reference where applicable. Empty string if not applicable.
+        "customDimension14": "",
+        # CatalogueDataSource where applicable. Empty string if not applicable.
+        "customDimension15": "",
+        # Availability condition category where applicable.  Empty string if not applicable.
+        "customDimension16": availability_condition_category,
+        # Availability condition where applicable.  Empty string if not applicable.
+        "customDimension17": availability_condition,
     }
