14440e6736db35f208298ce5d5b6bd23120f41bc
Dan Bentley <dan@numiko.com>
2022-01-21 12:16:24 +0000

    Fix issue outputting fallback in chooser Record if request fails

--- etna/records/widgets.py
+++ etna/records/widgets.py
@@ -8,25 +8,29 @@ class RecordChooser(AdminChooser):
     choose_one_text = "Choose a record"
     choose_another_text = "Choose another record"
     link_to_chosen_text = "Edit this record"
-    model = Record
     choose_modal_url_name = "record_chooser:choose"
 
     def get_value_data(self, value):
-        if self.model and isinstance(value, self.model):
-            instance = value
-            value = value.iaid
+        if isinstance(value, Record):
             return {
-                'value': value,
-                'title': self.get_title(instance),
-                'edit_item_url': self.get_edit_item_url(instance),
+                "value": value.iaid,
+                "title": self.get_title(value),
+                "edit_item_url": self.get_edit_item_url(value),
             }
-        return super().get_value_data(value)
+
+        # If value isn't a Record instance, it's an iaid that can be directly
+        # output in the form field.
+        return {
+            "value": value,
+            "title": "",
+            "edit_item_url": "",
+        }
 
     def get_instance(self, pk):
         """Fetch related instance on edit form."""
         try:
-            return self.model.search.get(iaid=pk)
+            return Record.search.get(iaid=pk)
         except KongException:
             # If there's a connection issue with Kong, return a stub Record
             # so we have something to render on the ResultsPage edit form.
-            return Record(iaid=pk)
+            return Record(iaid=pk, title="", reference_number="")
