693fcb6fc7e9887e43333b1751094fdba025fb19
Dan Bentley <dan@numiko.com>
2022-02-15 12:13:25 +0000

    Add pagination

--- etna/search/views.py
+++ etna/search/views.py
@@ -1,9 +1,9 @@
-from django.conf import settings
+from django.core.paginator import Page
 from django.shortcuts import render
 
-from .forms import SearchForm
+from ..ciim.paginator import APIPaginator
 from ..records.models import Record
-from ..ciim.client import KongClient
+from .forms import SearchForm
 
 
 def search_view(request):
@@ -11,21 +11,28 @@ def search_view(request):
 
     records = []
 
+    per_page = 20
+    page_number = int(request.GET.get("page", 1))
+    offset = (page_number - 1) * per_page
+    search_text = ''
+    count = 0
+
     if form.is_valid():
-        client = KongClient(
-            settings.KONG_CLIENT_BASE_URL,
-            api_key=settings.KONG_CLIENT_KEY,
-            verify_certificates=settings.KONG_CLIENT_VERIFY_CERTIFICATES,
-        )
         search_text = form.cleaned_data["search_text"]
-        # records = Record.search.filter(keyword=search_text)
-        records = client.search(keyword=search_text, size=10)["hits"]["hits"]
+        count, records = Record.api.search(
+            keyword=search_text, offset=offset, size=per_page
+        )
+
+    paginator = APIPaginator(count, per_page=per_page)
+    page = Page(records, number=page_number, paginator=paginator)
 
     return render(
         request,
         "search/search.html",
         {
+            "search_text": search_text,
+            "count": count,
             "form": form,
-            "records": records,
+            "page": page,
         },
     )
