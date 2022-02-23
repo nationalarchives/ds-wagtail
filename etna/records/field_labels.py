67d98a2fe19da21292f24a9d906219e49d8018f6
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-02-23 07:26:53 +0000

    Changes to replace fixed field labels with configured dict human readable values in record-details template

---
+++ etna/records/field_labels.py
@@ -0,0 +1,9 @@
+# format <record_field_name> : <human readable label name>
+human_readable_labels = {
+    "reference_number": "Reference",
+    "description": "Description",
+    "origination_date": "Record created",
+    "held_by": "Held by",
+    "legal_status": "Legal status",
+    "closure_status": "Closure status",
+}
--- etna/records/templatetags/records_tags.py
+++ etna/records/templatetags/records_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from ..models import Record
+from ..field_labels import human_readable_labels
 
 register = template.Library()
 
@@ -9,3 +10,9 @@ register = template.Library()
 def is_page_current_item_in_hierarchy(page: Record, hierarchy_item: dict):
     """Checks whether given page matches item from a record's hierarchy"""
     return page.reference_number == hierarchy_item["reference_number"]
+
+
+@register.filter
+def as_label(record_field_name: str) -> str:
+    """returns human readable pre configured record/api field name, otherwise blank"""
+    return human_readable_labels.get(record_field_name, "")
