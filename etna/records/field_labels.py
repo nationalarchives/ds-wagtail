cbd27289077f4d8ee8cd9e408ca03991e35b7b36
TNA-Allan <96120886+TNA-Allan@users.noreply.github.com>
2022-03-04 10:52:04 +0000

    DF-85:Implement Wagtail config for template handling (#235)

---
+++ etna/records/field_labels.py
@@ -0,0 +1,17 @@
+# format <record_field_name> : <human readable label name>
+FIELD_LABELS = {
+    "reference_number": "Reference",
+    "legal_status": "Legal status",
+    "created_by": "Record created by",
+    "description": "Description",
+    "origination_date": "Record created at",
+    "closure_status": "Closure status",
+    "held_by": "Record now held at",
+    "parent": "This record is in",
+    "hierarchy": "Where am I in the catalogue?",
+    "topics": "Topics covered in this record",
+    "next_record": "Next",
+    "previous_record": "Previous",
+    "related_records": "Related records",
+    "related_articles": "Related content",
+}
--- etna/records/templatetags/records_tags.py
+++ etna/records/templatetags/records_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+from ..field_labels import FIELD_LABELS
 from ..models import Record
 
 register = template.Library()
@@ -9,3 +10,9 @@ register = template.Library()
 def is_page_current_item_in_hierarchy(page: Record, hierarchy_item: dict):
     """Checks whether given page matches item from a record's hierarchy"""
     return page.reference_number == hierarchy_item["reference_number"]
+
+
+@register.filter
+def as_label(record_field_name: str) -> str:
+    """returns human readable label for pre configured record field name, otherwise Invalid name"""
+    return FIELD_LABELS.get(record_field_name, "UNRECOGNISED FIELD NAME")
