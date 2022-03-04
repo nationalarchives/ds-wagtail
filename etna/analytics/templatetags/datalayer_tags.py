d96e654ec6af39630685b8062a5152ca85cca151
Andy Babic <andy.babic@torchbox.com>
2022-03-04 12:10:03 +0000

    CHORE: Prep for datalayer tickets DF-74 + UN-141 (#237)
    * Add DataLayerMixin
    
    * Apply DataLayerMixin to BasePage
    
    * Apply DataLayerMixin to Record dataclass
    
    * Ensure collections page types inherit from BasePage
    
    * Simply datalayer template tag
    
    * Update base.html to make it easier to extend and use {% wagtail_site %} to identify the Site
    
    * Add base_page.html template for page types to use
    
    * Update page templates to extend "base_page.html" and remove duplicated/outdated datalayer code

--- etna/analytics/templatetags/datalayer_tags.py
+++ etna/analytics/templatetags/datalayer_tags.py
@@ -1,83 +1,21 @@
 from django import template
-from django.conf import settings
 
 register = template.Library()
 
 
-def get_content_group(page):
+@register.inclusion_tag("includes/gtm-datalayer.html", takes_context=True)
+def render_gtm_datalayer(context, obj) -> dict:
     """
-    Get the content group name for a specified page.
-
-    The group name is derived from the page model's app name.
-
-    If the model has no defined group, the page title is returned by default.
-    """
-    appname = page.__module__.split(".")[1]
-    groups = {
-        "home": "Homepage",
-        "insights": "Insights",
-        "collections": "Explorer",
-        "records": "TNA catalogue",
-    }
-    return groups.get(appname, page.title)
-
-
-def get_availability_condition_category(availability_condition):
-    """
-    Return a category for the availability condition of the page.
-
-    If an availability condition is not present, return an empty string.
-    """
-    return settings.AVAILABILITY_CONDITION_CATEGORIES.get(availability_condition, "")
-
-
-@register.filter
-def datalayer(page, request) -> dict:
-    """
-    Return Datalayer data for a Page object.
-
+    Render the datalayer for an instance of a ``DataLayerMixin`` subclass.
     https://developers.google.com/tag-manager/devguide
     """
+    try:
+        data = obj.get_datalayer_data(context["request"])
+    except AttributeError:
+        data = {}
 
-    return {
-        # Name of the content group - [Always has a value]
-        "contentGroup1": get_content_group(page),
-        # The reader type (options are "offsite",
-        # "onsite_public", "onsite_staff", "subscription")
-        "customDimension1": "offsite",
-        # - User ID for participants.
-        "customDimension2": request.user.id,
-        # Page type - [Always has a value]
-        "customDimension3": page.get_verbose_name(),
-        # Taxonomy topics for the page, delineated by semi-colons. Empty string if no value.
-        "customDimension4": "",
-        # Taxonomy sub topic where applicable. Empty string if not applicable.
-        "customDimension5": "",
-        # Taxonomy term where applicable. Empty string if not applicable.
-        "customDimension6": "",
-        # Time period where applicable. Empty string if not applicable.
-        "customDimension7": "",
-        # Sub time period where applicable. Empty string if not applicable.
-        "customDimension8": "",
-        # Entity type where applicable. Empty string if not applicable.
-        "customDimension9": "",
-        # Entity label where applicable. Empty string if not applicable.
-        "customDimension10": "",
-        # Catalogue repository where applicable. Empty string if not applicable.
-        "customDimension11": "",
-        # Catalogue level where applicable. Empty string if not applicable.
-        "customDimension12": "",
-        # Catalogue series where applicable. Empty string if not applicable.
-        "customDimension13": "",
-        # Catalogue reference where applicable. Empty string if not applicable.
-        "customDimension14": "",
-        # CatalogueDataSource where applicable. Empty string if not applicable.
-        "customDimension15": "",
-        # Availability condition category where applicable.  Empty string if not applicable.
-        "customDimension16": "",
-        # Availability condition where applicable.  Empty string if not applicable.
-        "customDimension17": "",
-    }
+    # Add a json serialized version of the data for output
+    return {"data": data}
 
 
 @register.simple_tag(takes_context=True)
@@ -93,59 +31,4 @@ def image_browse_datalayer(context) -> dict:
         # The user type and is private beta specific - the user ID for participants.
         "customDimension2": context["request"].user.id,
         "customDimension18": context.get("images_count", ""),
-    }
-
-
-@register.simple_tag(takes_context=True)
-def record_datalayer(context) -> dict:
-    """Custom tag for image browse datalayer.
-
-    Image browse isn't a page served by Wagtail, therefore we're unable to
-    query the page object to populate the datalayer.
-    """
-    user = context["request"].user
-    page = context["page"]
-    availability_condition = page.availability_delivery_condition
-    availability_condition_category = get_availability_condition_category(
-        availability_condition
-    )
-
-    return {
-        # The name of the content group - [Always has a value]
-        "contentGroup1": get_content_group(page),
-        # The reader type (options are "offsite",
-        # "onsite_public", "onsite_staff", "subscription")
-        "customDimension1": "offsite",
-        # ser ID for participants.
-        "customDimension2": user.id,
-        # The page type - [Always has a value]
-        "customDimension3": "Record page",
-        # Taxonomy topics for the page, delineated by semi-colons. Empty string if no value.
-        "customDimension4": "",
-        # Taxonomy sub topic where applicable. Empty string if not applicable.
-        "customDimension5": "",
-        # Taxonomy term where applicable. Empty string if not applicable.
-        "customDimension6": "",
-        # Time period where applicable. Empty string if not applicable.
-        "customDimension7": "",
-        # Sub time period where applicable. Empty string if not applicable.
-        "customDimension8": "",
-        # Entity type where applicable. Empty string if not applicable.
-        "customDimension9": "",
-        # Entity label where applicable. Empty string if not applicable.
-        "customDimension10": "",
-        # Catalogue repository where applicable. Empty string if not applicable.
-        "customDimension11": "",
-        # Catalogue level where applicable. Empty string if not applicable.
-        "customDimension12": "",
-        # Catalogue series where applicable. Empty string if not applicable.
-        "customDimension13": "",
-        # Catalogue reference where applicable. Empty string if not applicable.
-        "customDimension14": "",
-        # CatalogueDataSource where applicable. Empty string if not applicable.
-        "customDimension15": "",
-        # Availability condition category where applicable.  Empty string if not applicable.
-        "customDimension16": availability_condition_category,
-        # Availability condition where applicable.  Empty string if not applicable.
-        "customDimension17": availability_condition,
     }
