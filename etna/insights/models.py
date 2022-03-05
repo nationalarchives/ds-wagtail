d7480802adec1263ca2d61a2e88a88ffeda90d42
mo zaki <mozaki1@hotmail.co.uk>
2022-03-05 02:52:11 +0000

    Resolve conflicts

--- etna/insights/models.py
+++ etna/insights/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.fields import StreamField
 from wagtail.snippets.models import register_snippet
