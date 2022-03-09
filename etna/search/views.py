335ce0a93f9d902cd495591f1d3bb32f82ba0271
Dan Bentley <dan@numiko.com>
2022-03-09 12:32:03 +0000

    Paginate catalogue search results DF-174

--- etna/search/views.py
+++ etna/search/views.py
@@ -1,11 +1,17 @@
+from django.core.paginator import Page
 from django.shortcuts import render
 
+from ..ciim.paginator import APIPaginator
 from ..records.models import Record
 from .forms import SearchForm
 
 
 def catalogue_search(request):
     form = SearchForm(request.GET)
+
+    per_page = int(request.GET.get("per_page", 20))
+    page_number = int(request.GET.get("page", 1))
+    offset = (page_number - 1) * per_page
 
     records = []
     count = 0
@@ -14,16 +20,23 @@ def catalogue_search(request):
         # If no keyword is provided, pass None to client to fetch all results.
         keyword = form.cleaned_data.get("keyword") or None
 
-        response = Record.api.client.search(keyword=keyword, size=20,)
+        response = Record.api.client.search(
+            keyword=keyword,
+            offset=offset,
+            size=per_page,
+        )
         _, result_response = response["responses"]
         records = result_response["hits"]["hits"]
         count = result_response["hits"]["total"]["value"]
 
+    paginator = APIPaginator(count, per_page=per_page)
+    page = Page(records, number=page_number, paginator=paginator)
+
     return render(
         request,
-        "search/search.html",
+        "search/catalogue_search.html",
         {
-            "count": count,
+            "page": page,
             "form": form,
             "records": records,
         },
