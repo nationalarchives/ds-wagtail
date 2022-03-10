e3c38381e142911b51228f90f356758ff317923c
Dan Bentley <dan@numiko.com>
2022-03-10 10:09:07 +0000

    Tidy up result output

--- etna/search/templatetags/search_tags.py
+++ etna/search/templatetags/search_tags.py
@@ -12,3 +12,42 @@ def bucket_count(api_response, bucket_name: str) -> int:
         return 0
 
     return next(iter(a["doc_count"] for a in buckets if a["key"] == bucket_name), 0)
+
+
+@register.filter
+def record_title(record) -> str:
+    """Output a record's title.
+
+    Django templates don't allow access to keys or attributes prefixed with _
+
+    https://docs.djangoproject.com/en/4.0/ref/templates/language/#variables
+    """
+    try:
+        return record["_source"]["@template"]["details"]["summaryTitle"]
+    except KeyError:
+        return 'No Title Found'
+
+
+@register.filter
+def record_iaid(record) -> str:
+    """Output a record's iaid.
+
+    Django templates don't allow access to keys or attributes prefixed with _
+
+    https://docs.djangoproject.com/en/4.0/ref/templates/language/#variables
+    """
+    try:
+        return record["_source"]["@template"]["details"]["iaid"]
+    except KeyError:
+        return 'No IAID Found'
+
+
+@register.filter
+def record_score(record) -> str:
+    """Output a record's score.
+
+    Django templates don't allow access to keys or attributes prefixed with _
+
+    https://docs.djangoproject.com/en/4.0/ref/templates/language/#variables
+    """
+    return record.get("_score")
