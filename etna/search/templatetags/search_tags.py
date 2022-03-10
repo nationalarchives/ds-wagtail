5ddb0323e4fd275ffbad542251f4c0015978952f
Dan Bentley <535833+danbentley@users.noreply.github.com>
2022-03-10 14:55:10 +0000

    Feature/filtered catalogue search (#247)
    * Populate filters using aggregation data DF-179
    
    * Filter search results using selected filters DF-180
    
    * Tidy up result output
    
    * Tidy up catalogue_search template
    
    * Configure empty search params in SearchForm

--- etna/search/templatetags/search_tags.py
+++ etna/search/templatetags/search_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.http import QueryDict
 
 register = template.Library()
 
@@ -12,3 +13,57 @@ def bucket_count(api_response, bucket_name: str) -> int:
         return 0
 
     return next(iter(a["doc_count"] for a in buckets if a["key"] == bucket_name), 0)
+
+
+@register.filter
+def record_title(record) -> str:
+    """Output a record's title.
+
+    Django templates don't allow access to keys or attributes prefixed with _
+
+    https://docs.djangoproject.com/en/4.0/ref/templates/language/#variables
+    """
+    try:
+        return record["_source"]["@template"]["details"]["summaryTitle"]
+    except KeyError:
+        return "No Title Found"
+
+
+@register.filter
+def record_iaid(record) -> str:
+    """Output a record's iaid.
+
+    Django templates don't allow access to keys or attributes prefixed with _
+
+    https://docs.djangoproject.com/en/4.0/ref/templates/language/#variables
+    """
+    try:
+        return record["_source"]["@template"]["details"]["iaid"]
+    except KeyError:
+        return "No IAID Found"
+
+
+@register.filter
+def record_score(record) -> str:
+    """Output a record's score.
+
+    Django templates don't allow access to keys or attributes prefixed with _
+
+    https://docs.djangoproject.com/en/4.0/ref/templates/language/#variables
+    """
+    return record.get("_score")
+
+
+@register.simple_tag(takes_context=True)
+def query_string(context, **kwargs) -> str:
+    """Update  query string for context's request with passed kwargs."""
+
+    request = context["request"]
+
+    query_dict = QueryDict(mutable=True)
+    query_dict.update(request.GET)
+
+    for k, v in kwargs.items():
+        query_dict[k] = v
+
+    return query_dict.urlencode()
--- etna/search/views.py
+++ etna/search/views.py
@@ -1,14 +1,13 @@
 from django.core.paginator import Page
 from django.shortcuts import render
 
-from ..ciim.client import Aggregation
+from ..ciim.client import Aggregation, Template
 from ..ciim.paginator import APIPaginator
 from ..records.models import Record
 from .forms import SearchForm
 
 
 def catalogue_search(request):
-    form = SearchForm(request.GET)
 
     per_page = int(request.GET.get("per_page", 20))
     page_number = int(request.GET.get("page", 1))
@@ -18,13 +17,19 @@ def catalogue_search(request):
     records = []
     count = 0
 
+    data = request.GET.copy()
+    data.setdefault("group", "group:tna")
+    form = SearchForm(data)
+
     if form.is_valid():
         # If no keyword is provided, pass None to client to fetch all results.
-        keyword = form.cleaned_data.get("keyword") or None
+        keyword = form.cleaned_data.get("keyword")
+        # If no filter_keyword is provided, pass None to client bypass search within
         filter_keyword = form.cleaned_data.get("filter_keyword")
         filter_aggregations = form.cleaned_data.get("filter_aggregations")
 
         response = Record.api.client.search(
+            template=Template.DETAILS,
             keyword=keyword,
             filter_keyword=filter_keyword,
             filter_aggregations=filter_aggregations,
@@ -42,6 +47,8 @@ def catalogue_search(request):
         bucket_count_response, result_response = response["responses"]
         records = result_response["hits"]["hits"]
         count = result_response["hits"]["total"]["value"]
+
+        form.update_from_response(response=result_response)
 
     paginator = APIPaginator(count, per_page=per_page)
     page = Page(records, number=page_number, paginator=paginator)
