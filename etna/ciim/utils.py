cd5d99f9addb1cb1425969988ed58e14a7ffd967
Andy Babic <andy.babic@torchbox.com>
2022-03-16 09:16:56 +0000

    Add MinimalResultParser and LazyResultList to ciim.utils

--- etna/ciim/utils.py
+++ etna/ciim/utils.py
@@ -1,8 +1,78 @@
 import re
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, List, Optional
 
 from django.urls import reverse
 
 from pyquery import PyQuery as pq
+
+
+class MinimalResultParser:
+    """
+    A parser that applies a mimimal amount of tranformation to the
+    dict representations from ElasticSearch, to make them a little
+    more user-friendly.
+    """
+    def __call__(self, value):
+        return_value = {}
+        # Lose top-level '_source' key from ES and strip leading/trailing
+        # underscores from top-level keys
+        for key, v in value.get("_source", {}).items():
+            return_value[key.strip('_')] = v
+        # Add search match score if present
+        return_value["score"] = value.get("_score")
+        # Add details template values to root level for easier access
+        return_value.update(return_value["template"]["details"])
+        return return_value
+
+
+@dataclass
+class LazyResultList:
+    """
+    A simple wrapper-class used to represent a list of results.
+
+    The total number of availabe results is available as `total_count`.
+
+    The object is 'lazy', because results are stored in their original
+    JSON-parsed dict representation until the object is iterated. Only then are
+    results parsed using the relevant `result_parser` to make them more
+    user-friendly.
+
+    Supports slicing, len() and bool() evaluation - much like a list or tuple.
+    Slicing returns a new `LazyResultList` object with an updated `results`
+    value, but with all other attribute values preserved.
+    """
+    total_count: int
+    results: List[Dict[str, Any]]
+    result_parser: Callable
+    aggregations: Optional[Dict[str, Any]] = None
+    bucket_counts: Optional[Dict[str, int]] = None
+    is_lazy: bool = True
+
+    def __len__(self) -> int:
+        return len(self.results)
+
+    def __bool__(self):
+        return bool(self.results)
+
+    def __iter__(self):
+        # Parse results if not already parsed
+        self._parse_results()
+        # Return parsed results one-by-one
+        yield from self.results
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            result = self.results[key]
+            return self.result_parser(result) if self.is_lazy else result
+        sliced_results = self.results[key]
+        return LazyResultList(total_count=self.total_count, results=sliced_results, result_parser=self.result_parser, aggregations=self.aggregations, bucket_counts=self.bucket_counts, is_lazy=self.is_lazy)
+
+    def _parse_results(self):
+        if not self.is_lazy:
+            return
+        self.results = [self.result_parser(r) for r in self.results]
+        self.is_lazy = False
 
 
 def pluck(collection, accessor=lambda: False, default=None):
