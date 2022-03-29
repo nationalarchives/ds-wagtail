ba7bbc269bb97ffcfc87e3fd724a8eb26278c5d3
Andy Babic <andy.babic@torchbox.com>
2022-03-29 16:51:51 +0100

    Use PageListBlock to define a FeaturedCollectionBlock

--- etna/insights/blocks.py
+++ etna/insights/blocks.py
@@ -5,7 +5,13 @@ from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.snippets.blocks import SnippetChooserBlock
 
-from etna.core.blocks import ImageBlock, ParagraphWithHeading, QuoteBlock, SectionBlock
+from etna.core.blocks import (
+    ImageBlock,
+    PageListBlock,
+    ParagraphWithHeading,
+    QuoteBlock,
+    SectionBlock,
+)
 
 from ..media.blocks import MediaBlock
 from ..records.blocks import RecordChooserBlock
@@ -170,6 +176,24 @@ class RelatedItemsBlock(blocks.StructBlock):
         block_counts = {
             "related_items": {"min_num": 1, "max_num": 1},
         }
+
+
+class FeaturedCollectionBlock(blocks.StructBlock):
+    heading = blocks.CharBlock(max_length=100)
+    description = blocks.TextBlock(max_length=200)
+    items = PageListBlock(
+        "insights.InsightsPage",
+        exclude_drafts=True,
+        exclude_private=False,
+        select_related=["teaser_image"],
+        min_num=3,
+        max_num=9,
+    )
+
+    class Meta:
+        icon = "ul"
+        label = "Featured collection"
+        template = "insights/blocks/featured_collection.html"
 
 
 class InsightsIndexPageStreamBlock(blocks.StreamBlock):
