adeaa1dc096e5ccff01bd44497b4920f75b4cfcc
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-04-01 17:46:19 +0100

    DF-74:Update post testing comments for datalayer for digitised record, marked TODO for unsupported media in API

--- etna/records/views/records.py
+++ etna/records/views/records.py
@@ -63,8 +63,9 @@ def record_detail_view(request, iaid):
 
     image = None
 
-    if page.is_digitised:
-        image = Image.search.filter(rid=page.media_reference_id).first()
+    # TODO: Kong open beta API does not support media. Re-enable/update once media is available.
+    # if page.is_digitised:
+    #     image = Image.search.filter(rid=page.media_reference_id).first()
 
     return render(
         request,
