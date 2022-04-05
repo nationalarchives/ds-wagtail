ae1191a36eb15b1ab4ac8181e2823980aed7a016
Andy Babic <andy.babic@torchbox.com>
2022-04-05 17:41:18 +0100

    UN-144: Add 'featured collections' to Insights landing page (#276)
    * Add a generic PageListBlock that can be reused for this kind of thing
    
    * Use PageListBlock to define a FeaturedCollectionBlock
    
    * Add field to InsightsIndexPage
    
    * Add placeholder template output for FE to refine

--- etna/core/blocks/__init__.py
+++ etna/core/blocks/__init__.py
@@ -1,10 +1,12 @@
 from .image import ImageBlock
+from .page_list import PageListBlock
 from .paragraph import ParagraphWithHeading
 from .quote import QuoteBlock
 from .section import SectionBlock
 
 __all__ = [
     "ImageBlock",
+    "PageListBlock",
     "ParagraphWithHeading",
     "QuoteBlock",
     "SectionBlock",
