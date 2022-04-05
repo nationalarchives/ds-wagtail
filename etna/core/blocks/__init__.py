2757836f1493bf826068835680aabad658b8138f
Andy Babic <andy.babic@torchbox.com>
2022-04-05 15:04:57 +0100

    Copy paragraphs, quotes and sections blocks to core

---
+++ etna/core/blocks/__init__.py
@@ -0,0 +1,9 @@
+from .paragraph import ParagraphWithHeading
+from .quote import QuoteBlock
+from .section import SectionBlock
+
+__all__ = [
+    "ParagraphWithHeading",
+    "QuoteBlock",
+    "SectionBlock",
+]
---
+++ etna/core/blocks/section.py
@@ -0,0 +1,12 @@
+from wagtail.core import blocks
+
+
+class SectionBlock(blocks.StructBlock):
+    heading = blocks.CharBlock(
+        required=True, max_length=100, label="Section heading (heading level 2)"
+    )
+
+    class Meta:
+        icon = "heading"
+        label = "Section heading"
+        template = "sections/blocks/section.html"
