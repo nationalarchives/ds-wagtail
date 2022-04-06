bc318ad5689b5eb1fed679a2453b7e32f160b139
Andy Babic <andy.babic@torchbox.com>
2022-04-06 20:37:07 +0100

    Remove more tests that don't tell us anything we don't already know, and consolidate others (all we're really testing here is the RecordChooserBlock behaviour... which is the same for both blocks... no need to repeat everything for both custom block types

--- etna/records/tests/test_blocks.py
+++ etna/records/tests/test_blocks.py
@@ -55,11 +55,12 @@ class TestFeaturedRecordBlockIntegration(WagtailPageTests):
                 "body": streamfield(
                     [
                         (
-                            "content_section", {
+                            "content_section",
+                            {
                                 "heading": "Heading",
                                 "content": streamfield(
                                     [("featured_record", {"record": "C123456"})]
-                                )
+                                ),
                             },
                         ),
                     ]
@@ -98,26 +99,42 @@ class TestFeaturedRecordBlockIntegration(WagtailPageTests):
         )
 
     @responses.activate
-    def test_view_edit_page_with_featured_record(self):
+    def test_page_with_featured_record(self):
         self.insights_page.body = json.dumps(
             [
                 {
-                    "type": "featured_record",
-                    "value": {"record": "C123456"},
+                    "type": "content_section",
+                    "value": {
+                        "heading": "Heading",
+                        "content": [
+                            {
+                                "type": "featured_record",
+                                "value": {"record": "C123456"},
+                            }
+                        ],
+                    },
                 }
             ]
         )
         self.insights_page.save()
 
+        # Check the edit view first
         response = self.client.get(
             reverse("wagtailadmin_pages:edit", args=(self.insights_page.id,))
         )
-
         self.assertContains(response, "Test record")
-
         self.assertEqual(len(responses.calls), 1)
         self.assertEqual(
             responses.calls[0].request.url,
+            "https://kong.test/data/fetchAll?iaids=C123456",
+        )
+
+        # View the page to check rendering also
+        response = self.client.get(self.insights_page.get_url())
+        self.assertContains(response, "Test record")
+        self.assertEqual(len(responses.calls), 2)
+        self.assertEqual(
+            responses.calls[1].request.url,
             "https://kong.test/data/fetchAll?iaids=C123456",
         )
 
@@ -126,218 +143,24 @@ class TestFeaturedRecordBlockIntegration(WagtailPageTests):
         """Ensure that even if a record associated with this page doesn't
         exist, we're still able to render its edit page."""
 
-        responses.add(responses.GET, "https://kong.test/data/fetch", status=500)
+        responses.replace(responses.GET, "https://kong.test/data/fetch", status=500)
 
         self.insights_page.body = json.dumps(
             [
                 {
-                    "type": "featured_records",
+                    "type": "content_section",
                     "value": {
-                        "heading": "This is a heading",
-                        "introduction": "This is some text",
-                        "records": ["C123456"],
-                    },
-                }
-            ]
-        )
-        self.insights_page.save()
-
-        response = self.client.get(
-            reverse("wagtailadmin_pages:edit", args=(self.insights_page.id,))
-        )
-
-        self.assertEquals(response.status_code, 200)
-
-    @responses.activate
-    def test_view_page_with_featured_record(self):
-        self.insights_page.body = json.dumps(
-            [
-                {
-                    "type": "featured_record",
-                    "value": {"record": "C123456"},
-                }
-            ]
-        )
-        self.insights_page.save()
-
-        response = self.client.get(self.insights_page.get_url())
-
-        self.assertContains(response, "Test record")
-
-        self.assertEqual(len(responses.calls), 1)
-        self.assertEqual(
-            responses.calls[0].request.url,
-            "https://kong.test/data/fetchAll?iaids=C123456",
-        )
-
-
-@override_settings(
-    KONG_CLIENT_BASE_URL="https://kong.test",
-)
-class TestFeaturedRecordsBlockIntegration(WagtailPageTests):
-    def setUp(self):
-        super().setUp()
-
-        response = create_response(
-            records=[
-                create_record(
-                    iaid="C123456",
-                    title="Test record",
-                ),
-            ]
-        )
-        responses.add(responses.GET, "https://kong.test/data/fetch", json=response)
-        responses.add(responses.GET, "https://kong.test/data/fetchAll", json=response)
-
-        root = Site.objects.get().root_page
-
-        self.insights_index_page = InsightsIndexPage(
-            title="Insights Index Page",
-            sub_heading="Introduction",
-        )
-        root.add_child(instance=self.insights_index_page)
-
-        self.insights_page = InsightsPage(
-            title="Insights page",
-            sub_heading="Introduction",
-        )
-        self.insights_index_page.add_child(instance=self.insights_page)
-
-    @responses.activate
-    def test_add_featured_records(self):
-        data = nested_form_data(
-            {
-                "title": "Insights page changed",
-                "slug": "insights-page",
-                "sub_heading": "Introduction",
-                "body": streamfield(
-                    [
-                        (
-                            "featured_records",
+                        "heading": "Heading",
+                        "content": [
                             {
-                                "heading": "This is a heading",
-                                "introduction": "This is some text",
-                                "records": streamfield([("value", "C123456")]),
-                            },
-                        ),
-                    ]
-                ),
-                "action-publish": "Publish",
-            }
-        )
-
-        response = self.client.post(
-            reverse("wagtailadmin_pages:edit", args=(self.insights_page.id,)), data
-        )
-        self.insights_page.refresh_from_db()
-
-        self.assertRedirects(
-            response,
-            reverse("wagtailadmin_explore", args=(self.insights_index_page.id,)),
-        )
-        self.assertEqual(self.insights_page.body[0].block_type, "featured_records")
-
-        self.assertEqual(
-            self.insights_page.body[0].value["heading"], "This is a heading"
-        )
-        self.assertEqual(
-            self.insights_page.body[0].value["introduction"], "This is some text"
-        )
-        self.assertEqual(self.insights_page.body[0].value["records"][0].iaid, "C123456")
-        self.assertEqual(len(responses.calls), 3)
-        self.assertEqual(
-            responses.calls[0].request.url,
-            "https://kong.test/data/fetch?iaid=C123456",
-        )
-        self.assertEqual(
-            responses.calls[1].request.url,
-            "https://kong.test/data/fetch?iaid=C123456",
-        )
-
-    @responses.activate
-    def test_remove_records(self):
-        self.insights_page.body = json.dumps(
-            [
-                {
-                    "type": "featured_records",
-                    "value": {
-                        "heading": "This is a heading",
-                        "introduction": "This is some text",
-                        "records": ["C123456"],
-                    },
-                }
-            ]
-        )
-        self.insights_page.save()
-
-        data = nested_form_data(
-            {
-                "title": "Insights page changed",
-                "slug": "insights-page",
-                "sub_heading": "Introduction",
-                "body": streamfield([]),
-                "action-publish": "Publish",
-            }
-        )
-
-        response = self.client.post(
-            reverse("wagtailadmin_pages:edit", args=(self.insights_page.id,)), data
-        )
-        self.insights_page.refresh_from_db()
-
-        self.assertRedirects(
-            response,
-            reverse("wagtailadmin_explore", args=(self.insights_index_page.id,)),
-        )
-        self.assertEqual(len(self.insights_page.body), 0)
-
-        self.assertEqual(len(responses.calls), 0)
-
-    @responses.activate
-    def test_view_edit_page_with_featured_record(self):
-        self.insights_page.body = json.dumps(
-            [
-                {
-                    "type": "featured_records",
-                    "value": {
-                        "heading": "This is a heading",
-                        "introduction": "This is some text",
-                        "records": ["C123456"],
-                    },
-                }
-            ]
-        )
-        self.insights_page.save()
-
-        response = self.client.get(
-            reverse("wagtailadmin_pages:edit", args=(self.insights_page.id,))
-        )
-
-        self.assertContains(response, "Test record")
-
-        self.assertEqual(len(responses.calls), 1)
-        self.assertEqual(
-            responses.calls[0].request.url,
-            "https://kong.test/data/fetchAll?iaids=C123456",
-        )
-
-    @responses.activate
-    def test_view_edit_page_with_featured_record_not_in_kong(self):
-        """Ensure that even if a record associated with this page doesn't
-        exist, we're still able to render its edit page."""
-
-        responses.add(
-            responses.GET, "https://kong.test/data/fetch", json=create_response()
-        )
-
-        self.insights_page.body = json.dumps(
-            [
-                {
-                    "type": "featured_records",
-                    "value": {
-                        "heading": "This is a heading",
-                        "introduction": "This is some text",
-                        "records": ["C123456"],
+                                "type": "featured_records",
+                                "value": {
+                                    "heading": "This is a heading",
+                                    "introduction": "This is some text",
+                                    "records": ["C123456"],
+                                },
+                            }
+                        ],
                     },
                 }
             ]
@@ -349,29 +172,3 @@ class TestFeaturedRecordsBlockIntegration(WagtailPageTests):
         )
 
         self.assertEquals(response.status_code, 200)
-
-    @responses.activate
-    def test_view_page_with_featured_record(self):
-        self.insights_page.body = json.dumps(
-            [
-                {
-                    "type": "featured_records",
-                    "value": {
-                        "heading": "This is a heading",
-                        "introduction": "This is some text",
-                        "records": ["C123456"],
-                    },
-                }
-            ]
-        )
-        self.insights_page.save()
-
-        response = self.client.get(self.insights_page.get_url())
-
-        self.assertContains(response, "Test record")
-
-        self.assertEqual(len(responses.calls), 1)
-        self.assertEqual(
-            responses.calls[0].request.url,
-            "https://kong.test/data/fetchAll?iaids=C123456",
-        )
