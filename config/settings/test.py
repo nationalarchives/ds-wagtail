6c43305f1d636c93c0b1627659f6b122fe112320
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-04-19 17:46:21 +0100

    DF-261:Fix test - make seperate from dev

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,4 +1,20 @@
-from .dev import *  # noqa
+from .base import *  # noqa: F401
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "@6gce61jt^(pyj5+l**&*_#zyxfj5v1*71cs5yoetg-!fsz826"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+try:
+    from .local import *  # noqa: F401
+except ImportError:
+    pass
 
 BIRDBATH_REQUIRED = False
 
