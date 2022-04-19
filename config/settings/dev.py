a66047f5eae89723c78e0359eda90347f68a7b1d
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-04-19 15:21:30 +0100

    DF-261:Add debug toolbar for debugging

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -17,7 +17,11 @@ except ImportError:
     pass
 
 if DEBUG:
+<<<<<<< HEAD
     from .base import INSTALLED_APPS, LOGGING, MIDDLEWARE
+=======
+    from .base import INSTALLED_APPS, MIDDLEWARE, LOGGING
+>>>>>>> 004ee59 (DF-261:Add debug toolbar for debugging)
 
     INSTALLED_APPS += [
         "debug_toolbar",
