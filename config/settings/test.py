3c8a8daeb748e8599d5fddbcebce60e2e7fdaff0
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-04-20 08:39:44 +0100

    DF-261:Format fix

--- config/settings/test.py
+++ config/settings/test.py
@@ -18,7 +18,7 @@ PASSWORD_HASHERS = [
 ]
 
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.MD5PasswordHasher',
+    "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
 
 BIRDBATH_REQUIRED = False
