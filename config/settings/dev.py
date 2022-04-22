31c66cd3fa89b74a7ee3f8164a04ec4ae31db0e9
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-04-22 07:58:16 +0100

    DF-261:Added django debug toolbar to display various debug information in debug mode

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -15,3 +15,23 @@ try:
     from .local import *  # noqa: F401
 except ImportError:
     pass
+
+if DEBUG:
+    from .base import INSTALLED_APPS, LOGGING, MIDDLEWARE
+
+    INSTALLED_APPS += [
+        "debug_toolbar",
+    ]
+
+    MIDDLEWARE += [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
+
+    def show_toolbar(request):
+        return True
+
+    DEBUG_TOOLBAR_CONFIG = {
+        "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+    }
+
+    LOGGING["root"]["level"] = "DEBUG"
