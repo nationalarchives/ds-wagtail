799ba09f9ce25a7888f49e18294ec0df6142fb53
Andy Babic <andy.babic@torchbox.com>
2022-04-27 13:18:45 +0100

    Amend FeaturedRecordsBlock as per discussion on ticket (#315)

--- etna/records/templatetags/records_tags.py
+++ etna/records/templatetags/records_tags.py
@@ -1,9 +1,32 @@
+from typing import Any, Dict
+
 from django import template
+from django.urls import reverse
 
 from ..field_labels import FIELD_LABELS
 from ..models import Record
 
 register = template.Library()
+
+
+@register.simple_tag
+def record_url(record: Dict[str, Any]) -> str:
+    """
+    Return the URL for the provided `record` dict; which could either be a
+    full/transformed result from the fetch() endpoint, OR a raw result from
+    the search() endpoint.
+    """
+    if ref := record.get("reference_number", record.get("referenceNumber")):
+        return reverse("details-page-human-readable", kwargs={"record_id": ref})
+    if iaid := record.get("iaid"):
+        return reverse("details-page-machine-readable", kwargs={"record_id": iaid})
+    if url := record.get("sourceUrl"):
+        return url
+    try:
+        # Assume `record` is an un-transformed search result
+        return record_url(record["_source"]["@template"]["details"])
+    except KeyError:
+        return ""
 
 
 @register.simple_tag
