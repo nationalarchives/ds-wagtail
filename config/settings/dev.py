387783ad79cf67941b2827e5a3429271cd0faaa4
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-04-28 15:58:17 +0100

    DF-261:Fix flake8

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,8 +1,8 @@
 from .base import *  # noqa: F401
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = strtobool(os.getenv("DEBUG", "True"))
-DEBUG_TOOLBAR_ENABLED = strtobool(os.getenv("DEBUG_TOOLBAR_ENABLED", "True"))
+DEBUG = strtobool(os.getenv("DEBUG", "True"))  # noqa: F405
+DEBUG_TOOLBAR_ENABLED = strtobool(os.getenv("DEBUG_TOOLBAR_ENABLED", "True"))  # noqa: F405
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "@6gce61jt^(pyj5+l**&*_#zyxfj5v1*71cs5yoetg-!fsz826"
@@ -18,10 +18,11 @@ except ImportError:
     pass
 
 if DEBUG:
+    from .base import LOGGING
     LOGGING["root"]["level"] = "DEBUG"
 
 if DEBUG and DEBUG_TOOLBAR_ENABLED:
-    from .base import INSTALLED_APPS, LOGGING, MIDDLEWARE
+    from .base import INSTALLED_APPS, MIDDLEWARE
 
     INSTALLED_APPS += [
         "debug_toolbar",
