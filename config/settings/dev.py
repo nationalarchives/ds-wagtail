be1fce5d6f529745811608b6ce0f493a889f5481
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-04-28 15:29:19 +0100

    DF-261:Make alternative to use env var, restore local setting location

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,7 +1,8 @@
 from .base import *  # noqa: F401
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = strtobool(os.getenv("DEBUG", "True"))
+DEBUG_TOOLBAR_ENABLED = strtobool(os.getenv("DEBUG_TOOLBAR_ENABLED", "True"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "@6gce61jt^(pyj5+l**&*_#zyxfj5v1*71cs5yoetg-!fsz826"
@@ -11,28 +12,28 @@ ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-if DEBUG:
-    if strtobool(os.getenv("DEBUG_TOOLBAR", "False")):  # noqa: F405
-        from .base import INSTALLED_APPS, LOGGING, MIDDLEWARE
-
-        INSTALLED_APPS += [
-            "debug_toolbar",
-        ]
-
-        MIDDLEWARE += [
-            "debug_toolbar.middleware.DebugToolbarMiddleware",
-        ]
-
-        def show_toolbar(request):
-            return True
-
-        DEBUG_TOOLBAR_CONFIG = {
-            "SHOW_TOOLBAR_CALLBACK": show_toolbar,
-        }
-
-    LOGGING["root"]["level"] = "DEBUG"
-
 try:
     from .local import *  # noqa: F401
 except ImportError:
     pass
+
+if DEBUG:
+    LOGGING["root"]["level"] = "DEBUG"
+
+if DEBUG and DEBUG_TOOLBAR_ENABLED:
+    from .base import INSTALLED_APPS, LOGGING, MIDDLEWARE
+
+    INSTALLED_APPS += [
+        "debug_toolbar",
+    ]
+
+    MIDDLEWARE += [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
+
+    def show_toolbar(request):
+        return True
+
+    DEBUG_TOOLBAR_CONFIG = {
+        "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+    }
