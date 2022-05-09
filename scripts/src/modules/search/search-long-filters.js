2e260c740d373ebbeab05a08cc8f0ebb00238638
AshTNA <ashleyaburnett@gmail.com>
2022-05-09 11:10:08 +0100

    Fix IE11 compatibility issues
    This also renames let to const, which helps developers understand the variable will never change.

--- scripts/src/modules/search/search-long-filters.js
+++ scripts/src/modules/search/search-long-filters.js
@@ -1,29 +1,29 @@
 export default function () {
-    let $parentForm = document.querySelector('[data-id="long-filter-form"]');
+    const $parentForm = document.querySelector('[data-id="long-filter-form"]');
 
-    let $longFiltersContainer = document.querySelector('[data-id="long-filters-container"]');
+    const $longFiltersContainer = document.querySelector('[data-id="long-filters-container"]');
 
-    let $longFiltersList = document.querySelector('[data-id="long-filters-list"]');
-    let $longFiltersListItems = document.querySelectorAll('[data-class="long-filters-list-item"]');
+    const $longFiltersList = document.querySelector('[data-id="long-filters-list"]');
+    const $longFiltersListItems = document.querySelectorAll('[data-class="long-filters-list-item"]');
 
     if(!$longFiltersContainer || !$longFiltersListItems || !$longFiltersList) {
         return;
     }
 
-    let longFiltersArray = [...$longFiltersListItems];
+    const longFiltersArray = [].slice.call($longFiltersListItems); // IE11 equivalent of doing [...$longFiltersListItems]
 
-    let $searchDiv = document.createElement('div');
+    const $searchDiv = document.createElement('div');
     $searchDiv.setAttribute('class', 'long-filters__search');
-    let $searchBox = document.createElement('input');
+    const $searchBox = document.createElement('input');
     $searchBox.setAttribute('class', 'long-filters__search-box');
-    let searchId = 'long-filters-search-box';
+    const searchId = 'long-filters-search-box';
     $searchBox.id = searchId;
-    let $searchLabel = document.createElement('label');
+    const $searchLabel = document.createElement('label');
     $searchLabel.innerText = "Narrow down filters";
     $searchLabel.setAttribute('for', searchId);
     $searchLabel.setAttribute('class', 'long-filters__search-label');
 
-    let $searchButton = document.createElement('button');
+    const $searchButton = document.createElement('button');
     $searchButton.innerText = 'Search';
     $searchButton.setAttribute('class', 'long-filters__search-button');
 
@@ -35,22 +35,37 @@ export default function () {
     $longFiltersContainer.insertBefore($searchDiv, $longFiltersContainer.childNodes[0]);
 
 
-    let handleSearch = function(e) {
+    const handleSearch = function(e) {
         e.preventDefault();
-        let keyword = $searchBox.value.toLowerCase();
+        const keyword = $searchBox.value.toLowerCase();
 
-        let narrowedDownFilters = longFiltersArray.filter(filter => {
-            let filterLabel = filter.childNodes[0];
-            let filterInput = filterLabel.childNodes[0];
+        const narrowedDownFilters = longFiltersArray.filter(filter => {
+
+            if(filter.childNodes.length === 0) {
+                return false;
+            }
+
+            const filterLabel = filter.childNodes[0];
+            const filterInput = filterLabel.childNodes[0];
 
             // Keep the filter if it matches the users keyword, or if the filter is selected.
-            return filter.innerText.toLowerCase().includes(keyword) || filterInput.checked;
+            return filterLabel.innerText.toLowerCase().indexOf(keyword) != -1 || filterInput.checked;
         });
 
-        $longFiltersList.replaceChildren(...narrowedDownFilters);
+        /* IE11 compatible method to remove children without affecting the JS APIs. 
+           Using $longFiltersList.innerHTML = ''; causes issues on IE11.
+        */
+        while ($longFiltersList.firstChild) {
+            $longFiltersList.removeChild($longFiltersList.firstChild);
+        }
+
+        for(const filter of narrowedDownFilters) {
+            $longFiltersList.appendChild(filter);
+        }
+
     }
 
-    let disableSearchSubmit = function(e) {
+    const disableSearchSubmit = function(e) {
         if(e.key === "Enter") {
             handleSearch(e);
         }
