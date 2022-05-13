60e6e6c1a6e5897b6463909130c7695e96b0cbf5
James <jwchan1993@gmail.com>
2022-05-13 12:27:52 +0100

    UN-191: Javascript enhancements for Insights (#330)
    * Add throttle module
    
    * Add jquery
    
    * Add accordion modules
    
    * Add navigation modules
    
    * Add add/remove event modules
    
    * Export throttle module
    
    * Implement insights JS enhancements
    
    * Formatting
    
    * Add styling for mobile/smaller screens
    
    * Wrap js reliant styles in js class
    
    * Fix grid in IE11
    
    * Use e.key
    
    * Remove  variable
    
    * Fix jumplinks behaviour following resize
    
    This was caused by not removing the jumpmlinks event listener when reducing the screen size below 768px and so multiple listeners were being applied
    
    * Formatting
    
    * Add desktop enhancements boolean
    
    This prevents event listeners from applied moree than once when the screen is resized (but not below 768px i.e. the desktop view is retained but gets smaller or larger)
    
    * Use local jquery
    
    * Toggle expand/collapse icon
    
    * Use e.key
    
    * Wrap toggle in if condition to prevent excessive clicks
    
    * Wrap jumplinks animation in if condition to prevent excesssive clicking

---
+++ scripts/src/modules/insights/accordion_functionality/slide_toggle.js
@@ -0,0 +1,8 @@
+export default function slide_toggle(sectionContent) {
+    if($(sectionContent).css("display") === "none") {
+        $(sectionContent).slideDown();
+    }
+    else {
+        $(sectionContent).slideUp();
+    }
+}
---
+++ scripts/src/modules/insights/remove_event.js
@@ -0,0 +1,3 @@
+export default function remove_event(element, event) {
+    $(element).off(event);
+}
