3f98a0bc9c833f1d309ed2d9acfaddcc270ddc56
James <jwchan1993@gmail.com>
2022-05-13 12:27:52 +0100

    UN-191: Javascript enhancements for Insights (#330)
    * Add throttle module
    
    * Add jquery
    
    * Add accordion modules
    
    * Add navigation modules
    
    * Add add/remove event modules
    
    * Export throttle module
    
    * Implement insights JS enhancements
    
    * Formatting
    
    * Add styling for mobile/smaller screens
    
    * Wrap js reliant styles in js class
    
    * Fix grid in IE11
    
    * Use e.key
    
    * Remove  variable
    
    * Fix jumplinks behaviour following resize
    
    This was caused by not removing the jumpmlinks event listener when reducing the screen size below 768px and so multiple listeners were being applied
    
    * Formatting
    
    * Add desktop enhancements boolean
    
    This prevents event listeners from applied moree than once when the screen is resized (but not below 768px i.e. the desktop view is retained but gets smaller or larger)
    
    * Use local jquery
    
    * Toggle expand/collapse icon
    
    * Use e.key
    
    * Wrap toggle in if condition to prevent excessive clicks
    
    * Wrap jumplinks animation in if condition to prevent excesssive clicking

--- scripts/src/insights.js
+++ scripts/src/insights.js
@@ -1,13 +1,115 @@
+import $ from 'jquery';
+
 import add_analytics_data_card_position from './modules/analytics/card_position'
 import audio_tracking from "./modules/analytics/audio_tracking"
 import video_tracking from "./modules/analytics/video_tracking"
 import add_unique_ids from "./modules/analytics/add_unique_ids";
 
+import accordion_functionality from "./modules/insights/accordion_functionality/accordion_functionality";
+import add_event from "./modules/insights/add_event";
+import add_section_ids from "./modules/insights/accordion_functionality/add_section_ids";
+import apply_aria_roles from "./modules/insights/accordion_functionality/apply_aria_roles";
+import debounce from "./modules/debounce";
+import jumplinks_smooth_scroll from './modules/insights/navigation/jumplinks_smooth_scroll';
+import open_first_section from "./modules/insights/accordion_functionality/open_first_section";
+import remove_aria_roles from "./modules/insights/accordion_functionality/remove_aria_roles";
+import remove_event from "./modules/insights/remove_event";
+import set_active from "./modules/insights/navigation/set_active";
+import throttle from "./modules/throttle";
+
 document.addEventListener('DOMContentLoaded', () => {
+    // Initialise jquery
+    window.$ = $;
+    
+    const $sectionHeadings = $(".section-separator__heading");
+    const $sectionContents = $(".section-content");
+    const $jumplinks = $(".jumplink");
+
+    // These booleans are used to detect if certain enhancements (e.g. click event listeners) have been applied in order to 
+    // prevent events from being added more than once.
+    let mobileEnhancementsApplied = false;
+    let desktopEnhancementsApplied = false;
+
     add_analytics_data_card_position('.record-embed-no-image');
     add_analytics_data_card_position('.card-group-secondary-nav');
-
     audio_tracking();
     video_tracking();
     add_unique_ids();
+
+    // Ids are added to sections for ARIA purposes.
+    add_section_ids($sectionHeadings, $sectionContents);
+
+    if($(window).width() < 768 && !mobileEnhancementsApplied) {
+        apply_aria_roles($sectionHeadings, $sectionContents);
+
+        // Detect if there are any expanded sections. If not, expand the first section.
+        open_first_section($sectionHeadings, $sectionContents);
+
+        // Add click and enter listeners for expanding/collapsing sections on small screens.
+        add_event($sectionHeadings, "click", function() {
+            accordion_functionality(this, $sectionHeadings, $sectionContents);
+        });
+        add_event($sectionHeadings, "keypress", function(e) {
+            if(e.key === "Enter") {
+                accordion_functionality(this, $sectionHeadings, $sectionContents);
+            }
+        });
+        mobileEnhancementsApplied = true;
+    }
+    else if(!mobileEnhancementsApplied) {
+        // Add click and scroll listeners for smooth scrolling when using jumpinks to navigate the page and 
+        // progress indication in the navigation on desktop.
+        add_event($(window), "scroll", throttle(function() {
+            set_active($sectionHeadings);
+        }, 300));
+        add_event($jumplinks, "click", function() {
+            jumplinks_smooth_scroll(this);
+        });
+        desktopEnhancementsApplied = true;
+    }
+
+    $(window).on('resize', debounce(() => {
+        if($(window).width() < 768 && !mobileEnhancementsApplied){
+            apply_aria_roles($sectionHeadings, $sectionContents);
+
+            // Detect if there are any expanded sections. If not, expand the first section.
+            open_first_section($sectionHeadings, $sectionContents);
+
+            // Add click and enter listeners for expanding/collapsing sections when the screen size is reduced.
+            add_event($sectionHeadings, "click", function() {
+                accordion_functionality(this, $sectionHeadings, $sectionContents);
+            });
+            add_event($sectionHeadings, "keypress", function(e) {
+                if(e.key === "Enter") {
+                    accordion_functionality(this, $sectionHeadings, $sectionContents);
+                }
+            });
+
+            // Remove scroll and jumplink click listeners
+            remove_event($(window), "scroll");
+            remove_event($jumplinks, "click");
+
+            desktopEnhancementsApplied = false;
+            mobileEnhancementsApplied = true;
+        }
+        else if ($(window).width() >= 768 && !desktopEnhancementsApplied) {
+            remove_aria_roles($sectionHeadings);
+
+            // Remove click and enter listeners because sections are fully expanded on desktop 
+            // i.e. no accordion functionality required.
+            remove_event($sectionHeadings, "click");
+            remove_event($sectionHeadings, "keypress");
+
+            // Add click and scroll listeners for smooth scrolling when using jumpinks to navigate the page and 
+            // progress indication in the navigation when the screen size is increased.
+            add_event($(window), "scroll", throttle(function() {
+                set_active($sectionHeadings);
+            }, 300));
+            add_event($jumplinks, "click", function() {
+                jumplinks_smooth_scroll(this);
+            });
+            desktopEnhancementsApplied = true;
+            mobileEnhancementsApplied = false;
+        }
+    }, 200));
 });
