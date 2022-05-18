6fa842cc4d8f7f2d2838a004f1b78463c28befbb
JamesWChan <jwchan1993@gmail.com>
2022-05-18 19:45:49 +0100

    Refactoring

--- scripts/src/modules/insights/accordion_functionality/slide_toggle.js
+++ scripts/src/modules/insights/accordion_functionality/slide_toggle.js
@@ -14,14 +14,17 @@ export default function slide_toggle(sectionHeadings, currentHeading, sectionCon
         position = $(currentHeading).offset().top;
     }
     else {
+        // The previous heading is used to determine the scrollTop position.
+        const prevHeading = sectionHeadings[sectionHeadings.index(currentHeading) - 1];
+
         // When using the css() method to get margin and padding properties, a string is returned e.g. 32px. Therefore, the
         // numeric value needs to be extracted for use in the calculations below.
-        const sectionHeadingPadding = convert_px_property_to_number($(sectionHeadings[sectionHeadings.index(currentHeading) - 1]).css("padding-top")) + convert_px_property_to_number($(sectionHeadings[sectionHeadings.index(currentHeading) - 1]).css("padding-bottom"));
-        const sectionHeadingMargin = convert_px_property_to_number($(sectionHeadings[sectionHeadings.index(currentHeading) - 1]).css("margin-top")) + convert_px_property_to_number($(sectionHeadings[sectionHeadings.index(currentHeading) - 1]).css("margin-bottom"));
-        const totalSectionHeadingHeight = $(sectionHeadings[sectionHeadings.index(currentHeading) - 1]).height() + sectionHeadingPadding + sectionHeadingMargin;
+        const sectionHeadingPadding = convert_px_property_to_number($(prevHeading).css("padding-top")) + convert_px_property_to_number($(prevHeading).css("padding-bottom"));
+        const sectionHeadingMargin = convert_px_property_to_number($(prevHeading).css("margin-top")) + convert_px_property_to_number($(prevHeading).css("margin-bottom"));
+        const totalSectionHeadingHeight = $(prevHeading).height() + sectionHeadingPadding + sectionHeadingMargin;
         
         // This accounts for the height of the element and also any padding/margin that has been applied.
-        position = $(sectionHeadings[sectionHeadings.index(currentHeading) - 1]).offset().top + totalSectionHeadingHeight;
+        position = $(prevHeading).offset().top + totalSectionHeadingHeight;
     }
 
     // Scroll to the expanded section's heading when clicked, keeping it at the top of the viewport.
