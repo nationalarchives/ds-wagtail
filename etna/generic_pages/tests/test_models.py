b59f1983994abde8a4ff5f4779dfbecc04f311d4
mo zaki <mozaki1@hotmail.co.uk>
2022-05-23 23:00:21 +0100

    Removing test_view_url_accessible_by_name and removing unused import

--- etna/generic_pages/tests/test_models.py
+++ etna/generic_pages/tests/test_models.py
@@ -1,5 +1,4 @@
 import json
-import re
 
 from django.test import TestCase
 
@@ -22,28 +21,8 @@ class TestGeneral(TestCase):
         )
         root.add_child(instance=self.general_page)
 
-    def assertContainsParagraph(self, subject: str, paragraph_text: str):
-        regex = re.compile(
-            "<div" + r"[\s\w'\=\-\"\.]*>\s*(.*)\s*<\/div" + ">",
-            re.MULTILINE,
-        )
-        paragraph_texts = []
-        for match in regex.finditer(subject):
-            paragraph_texts.append(match.groups()[0].strip())
-
-        self.assertIn(paragraph_text, paragraph_texts)
-
     def test_view_uses_correct_template(self):
         url = self.general_page.get_url()
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "generic_pages/general_page.html")
-
-    def test_paragraph_rendered_as_div(self):
-        url = self.general_page.get_url()
-        response = self.client.get(url)
-        response.render()
-        content = response.content.decode()
-        for paragraph_text in ("This is a paragraph",):
-            with self.subTest(paragraph_text):
-                self.assertContainsParagraph(content, paragraph_text)
