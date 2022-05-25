cc54cc8789c8af42a1fb43ada03b54f65da6fc93
JamesWChan <jwchan1993@gmail.com>
2022-05-25 12:53:58 +0100

    Refactor to use outerHeight()

--- scripts/src/modules/insights/accordion_functionality/set_heading_positions.js
+++ scripts/src/modules/insights/accordion_functionality/set_heading_positions.js
@@ -15,12 +15,7 @@ export default function set_heading_positions(sectionHeadings) {
 
             // This accounts for any padding, margin, and borders that have been applied to the heading in order to correctly
             // calculate its height.
-            const sectionHeadingPadding = convert_px_property_to_number($(prevHeading).css("padding-top")) + convert_px_property_to_number($(prevHeading).css("padding-bottom"));
-            const sectionHeadingMargin = convert_px_property_to_number($(prevHeading).css("margin-top")) + convert_px_property_to_number($(prevHeading).css("margin-bottom"));
-            const sectionHeadingBorder = convert_px_property_to_number($(prevHeading).css("border-top-width")) + convert_px_property_to_number($(prevHeading).css("border-bottom-width"));
-            const totalSectionHeadingHeight = $(prevHeading).height() + sectionHeadingPadding + sectionHeadingMargin + sectionHeadingBorder;
-
-            const position = positions[index - 1] + totalSectionHeadingHeight;
+            const position = positions[index - 1] + $(prevHeading).outerHeight(true);
             positions.push(position);
         }
     });
