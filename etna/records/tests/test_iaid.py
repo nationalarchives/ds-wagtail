d94d7b9e9118254e410903614492dd8f40bdf25c
mo zaki <mozaki1@hotmail.co.uk>
2022-05-30 13:55:57 +0100

    Add further test cases and correct class name typo.

--- etna/records/tests/test_iaid.py
+++ etna/records/tests/test_iaid.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 
 
-class TestiaidFromats(TestCase):
+class TestIaidFormats(TestCase):
     def test_iaid_long_mixed_format(self):
         longformat = "3717ee38900740728076a61a398fcb84"
         resolver = resolve("/catalogue/id/" + longformat + "/")
@@ -14,3 +14,33 @@ class TestiaidFromats(TestCase):
         resolver = resolve("/catalogue/id/" + guid + "/")
         self.assertEqual(resolver.view_name, "details-page-machine-readable")
         self.assertEqual(resolver.kwargs["iaid"], guid)
+
+    def test_iaid_A13530124_format(self):
+        iaid = "A13530124"
+        resolver = resolve("/catalogue/id/" + iaid + "/")
+        self.assertEqual(resolver.view_name, "details-page-machine-readable")
+        self.assertEqual(resolver.kwargs["iaid"], iaid)
+
+    def test_iaid_C2341693_format(self):
+        iaid = "C2341693"
+        resolver = resolve("/catalogue/id/" + iaid + "/")
+        self.assertEqual(resolver.view_name, "details-page-machine-readable")
+        self.assertEqual(resolver.kwargs["iaid"], iaid)
+
+    def test_iaid_D431198_format(self):
+        iaid = "D431198"
+        resolver = resolve("/catalogue/id/" + iaid + "/")
+        self.assertEqual(resolver.view_name, "details-page-machine-readable")
+        self.assertEqual(resolver.kwargs["iaid"], iaid)
+
+    def test_iaid_F257629_format(self):
+        iaid = "F257629"
+        resolver = resolve("/catalogue/id/" + iaid + "/")
+        self.assertEqual(resolver.view_name, "details-page-machine-readable")
+        self.assertEqual(resolver.kwargs["iaid"], iaid)
+
+    def test_iaid_N14562581_format(self):
+        iaid = "N14562581"
+        resolver = resolve("/catalogue/id/" + iaid + "/")
+        self.assertEqual(resolver.view_name, "details-page-machine-readable")
+        self.assertEqual(resolver.kwargs["iaid"], iaid)
