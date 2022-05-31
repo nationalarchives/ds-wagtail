765d8ec23485105000078c9db4329de7365644af
mo zaki <mozaki1@hotmail.co.uk>
2022-05-31 23:34:04 +0100

    Change regex to accept iaid Guid format with _1 ending

--- etna/records/tests/test_iaid.py
+++ etna/records/tests/test_iaid.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 
 
-class TestIaidFromats(TestCase):
+class TestIaidFormats(TestCase):
     def test_iaid_long_mixed_format(self):
         longformat = "3717ee38900740728076a61a398fcb84"
         resolver = resolve("/catalogue/id/" + longformat + "/")
@@ -11,6 +11,12 @@ class TestIaidFromats(TestCase):
 
     def test_iaid_guid_format(self):
         guid = "4d8dae2c-b417-4614-8ed8-924b9b4beeac"
+        resolver = resolve("/catalogue/id/" + guid + "/")
+        self.assertEqual(resolver.view_name, "details-page-machine-readable")
+        self.assertEqual(resolver.kwargs["iaid"], guid)
+
+    def test_iaid_guid_ending_with_1(self):
+        guid = "4d8dae2c-b417-4614-8ed8-924b9b4beeac_1"
         resolver = resolve("/catalogue/id/" + guid + "/")
         self.assertEqual(resolver.view_name, "details-page-machine-readable")
         self.assertEqual(resolver.kwargs["iaid"], guid)
