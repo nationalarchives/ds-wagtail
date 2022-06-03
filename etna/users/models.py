884eebe5eff2eaff5f77e9d455ee67acb353b300
Andy Babic <andy.babic@torchbox.com>
2022-06-03 15:10:16 +0100

    Add model to allow linking of users to IDP records

---
+++ etna/users/models.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+
+
+class IDPProfile(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="idp_profiles", on_delete=models.CASCADE
+    )
+    provider_name = models.CharField(max_length=100, db_index=True)
+    provider_user_id = models.CharField(max_length=36, db_index=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["provider_name", "provider_user_id"], name="idp_unique"
+            ),
+        ]
