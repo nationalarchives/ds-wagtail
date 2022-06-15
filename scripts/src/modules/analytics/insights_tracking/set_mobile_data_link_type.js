e89367054b26bd465b9b35c6250f4bb2f7d3f3fb
JamesWChan <jwchan1993@gmail.com>
2022-06-15 11:10:25 +0100

    Refactor to use ternary operator

--- scripts/src/modules/analytics/insights_tracking/set_mobile_data_link_type.js
+++ scripts/src/modules/analytics/insights_tracking/set_mobile_data_link_type.js
@@ -1,10 +1,7 @@
 export default function toggle_data_link_type(e, section_headings) {
-    if(e.target.getAttribute("aria-expanded") === "false") {
-        e.target.setAttribute("data_link_type", `Collapse header no. ${Array.prototype.indexOf.call(section_headings, e.target) + 1}`);
-    }
-    else {
-        e.target.setAttribute("data_link_type", `Expand header no. ${Array.prototype.indexOf.call(section_headings, e.target) + 1}`);
-    }
+    const isExpanded = e.target.getAttribute("aria-expanded") === "true" ? true : false;
+
+    e.target.setAttribute("data_link_type", `${isExpanded ? "Expand" : "Collapse"} header no. ${Array.prototype.indexOf.call(section_headings, e.target) + 1}`);
 
     return e.target.getAttribute("data_link_type");
 }
