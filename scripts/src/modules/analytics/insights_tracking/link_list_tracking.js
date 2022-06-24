ac465cadee185c50a45be251a6c16ecd65780e47
James <jwchan1993@gmail.com>
2022-06-24 14:50:00 +0100

    UN-195: Tagging requirements for HTML (#375)
    * Reduce font size for consistency
    
    * Modify analytics implementation for linked lists
    
    If several linked list components are present, the position attribute will reset to 0 each time a new linked list component is encountered. This refactor prevents the reset and continues to increment the position when a new linked list component is encountered.

--- scripts/src/modules/analytics/insights_tracking/link_list_tracking.js
+++ scripts/src/modules/analytics/insights_tracking/link_list_tracking.js
@@ -4,9 +4,10 @@ export default function link_list_tracking() {
         const link_list_links = document.querySelectorAll(".related-resources__link");
         const section_name = link_list_grandparent.querySelector(`h2`).textContent.trim();
 
-        Array.prototype.forEach.call(link_list_links, item => {
+        Array.prototype.forEach.call(link_list_links, (item, index) => {
             const component_name = item.getAttribute("data-component-name");
             item.setAttribute("data-component-name", component_name + section_name);
+            item.setAttribute("position", index);
         });
     }
 }
