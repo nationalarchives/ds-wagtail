150f277de14153a0ab9ebced279b840ca9d636b9
Andy Babic <andy.babic@torchbox.com>
2022-06-29 14:52:46 +0100

    register -> signup

--- etna/auth0/urls.py
+++ etna/auth0/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("login/", views.login, name="account_login"),
     path("logout/", views.logout, name="account_logout"),
-    path("register/", views.register, name="account_register"),
+    path("signup/", views.register, name="account_signup"),
     path("authorize/", views.authorize, name="account_authorize"),
 ]
