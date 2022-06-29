2decfab32c498111aba5315e709ef49ab4990621
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-06-29 17:24:55 +0100

    DF-345:Added changes for MAINTENENCE_MODE_ALLOW_IPS

--- etna/core/middleware.py
+++ etna/core/middleware.py
@@ -1,5 +1,14 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.template.response import SimpleTemplateResponse
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
 
 
 class MaintenanceModeMiddleware:
@@ -7,9 +16,9 @@ class MaintenanceModeMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-
         if settings.MAINTENANCE_MODE:
-            return render(request, "503.html")
+            if get_client_ip(request) not in settings.MAINTENENCE_MODE_ALLOW_IPS:
+                return SimpleTemplateResponse(template="503.html", status=503).render()
 
         response = self.get_response(request)
 
