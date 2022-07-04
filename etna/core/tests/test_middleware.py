774749ee85ed73230ba7f73d0420d0f464253d55
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-07-04 14:24:01 +0100

    DF-345:Amends - updated test case for home page

--- etna/core/tests/test_middleware.py
+++ etna/core/tests/test_middleware.py
@@ -30,28 +30,11 @@ class TestMaintenanceMode(TestCase):
     MAINTENENCE_MODE_ALLOW_IPS="123.4.5.6",
 )
 class TestMaintenanceModeOverrrice(TestCase):
-
     def _mocked_ip(request):
         return "123.4.5.6"
 
     @patch("etna.core.middleware.get_client_ip", _mocked_ip)
-    @responses.activate
-    def test_503_maintenance_override_for_record_detail(self):
-
-        responses.add(
-            responses.GET,
-            "https://kong.test/data/fetch",
-            json=create_response(
-                records=[
-                    create_record(iaid="C123456"),
-                ]
-            ),
-        )
-
-        response = self.client.get("/catalogue/C123456/")
+    def test_503_maintenance_mode_override_for_home_page(self):
+        response = self.client.get("/")
 
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(
-            response.resolver_match.view_name, "details-page-machine-readable"
-        )
-        self.assertTemplateUsed(response, "records/record_page.html")
