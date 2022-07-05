81b0ccdc9e0ffee257c1e94fd66326350751b035
Andy Babic <andy.babic@torchbox.com>
2022-07-05 18:19:13 +0100

    Tweak tests

--- etna/core/tests/test_middleware.py
+++ etna/core/tests/test_middleware.py
@@ -5,43 +5,43 @@ from django.test import TestCase, override_settings
 from ...ciim.utils import prevent_request_warnings
 
 
-@override_settings(
-    MAINTENANCE_MODE=True,
-    MAINTENENCE_MODE_ENDS="2011-11-04T00:05:23+04:00",
-)
+@override_settings(MAINTENANCE_MODE=True)
 class TestMaintenanceMode(TestCase):
+    def test_without_maintenance_mode_ends(self):
+        response = self.client.get("/")
+        self.assertEquals(response.status_code, 503)
+        self.assertNotIn("Retry-After", response.headers)
+
     @prevent_request_warnings
-    def test_503_maintenance_mode_for_home_page(self):
+    @override_settings(MAINTENENCE_MODE_ENDS="2011-11-04T00:05:23+04:00")
+    def test_maintenance_mode_ends_with_timezone_info(self):
         response = self.client.get("/")
         self.assertEquals(response.status_code, 503)
         self.assertEquals(
-            response.headers["Retry-After"], "Fri, 04 Nov 2011 00:05:23 UTC+04:00"
+            response.headers["Retry-After"], "Thu, 03 Nov 2011 20:05:23 GMT"
         )
 
+    @prevent_request_warnings
+    @override_settings(MAINTENENCE_MODE_ENDS="2011-11-04T00:05:23")
+    def test_maintenance_mode_ends_without_timezone_info(self):
+        response = self.client.get("/")
+        self.assertEquals(response.status_code, 503)
+        self.assertEquals(
+            response.headers["Retry-After"], "Fri, 04 Nov 2011 00:05:23 GMT"
+        )
 
-@override_settings(
-    KONG_CLIENT_BASE_URL="https://kong.test",
-    MAINTENANCE_MODE=True,
-    MAINTENENCE_MODE_ALLOW_IPS=["123.4.5.6", "123.3.2.1"],
-)
-class TestMaintenanceModeOverride(TestCase):
-    @patch("etna.core.middleware.get_client_ip")
-    def test_maintenance_mode_bypassed_when_ip_in_allowed_list(
-        self, mock_get_client_ip
-    ):
-        # set ip value that is in allowed list
-        mock_get_client_ip.return_value = "123.4.5.6"
+    @override_settings(MAINTENENCE_MODE_ALLOW_IPS=["123.4.5.6"])
+    @patch("etna.core.middleware.get_client_ip", return_value="123.4.5.6")
+    def test_maintenance_mode_bypassed_when_ip_in_allow_list(self, *args):
         response = self.client.get("/")
 
         self.assertEquals(response.status_code, 200)
 
-    @patch("etna.core.middleware.get_client_ip")
     @prevent_request_warnings
+    @override_settings(MAINTENENCE_MODE_ALLOW_IPS=["123.4.5.6"])
+    @patch("etna.core.middleware.get_client_ip", return_value="789.0.1.2")
     def test_maintenance_mode_enforced_when_ip_not_in_allowed_list(
         self, mock_get_client_ip
     ):
-        # set ip value that is not in allowed list
-        mock_get_client_ip.return_value = "789.0.1.2"
         response = self.client.get("/")
-
         self.assertEquals(response.status_code, 503)
