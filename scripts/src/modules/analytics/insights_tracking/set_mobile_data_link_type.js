1f38e12298f6443b503dcd0697b542bcef76f715
JamesWChan <jwchan1993@gmail.com>
2022-07-07 23:21:23 +0100

    Fix mobile tracking index bug

--- scripts/src/modules/analytics/insights_tracking/set_mobile_data_link_type.js
+++ scripts/src/modules/analytics/insights_tracking/set_mobile_data_link_type.js
@@ -1,7 +1,7 @@
 export default function toggle_data_link_type(e, section_headings) {
     const isExpanded = e.target.getAttribute("aria-expanded") === "true" ? true : false;
 
-    e.target.setAttribute("data-link-type", `${isExpanded ? "Expand" : "Collapse"} header no. ${Array.prototype.indexOf.call(section_headings, e.target) + 1}`);
+    e.target.setAttribute("data-link-type", `${isExpanded ? "Expand" : "Collapse"} header no. ${Array.prototype.indexOf.call(section_headings, e.target)}`);
 
     return e.target.getAttribute("data-link-type");
 }
