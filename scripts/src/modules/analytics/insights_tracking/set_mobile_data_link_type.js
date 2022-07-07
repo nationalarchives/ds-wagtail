f84f084c0ad75b5041708c9bf1bcdd65f9942ed6
JamesWChan <jwchan1993@gmail.com>
2022-07-07 23:40:01 +0100

    Change mobile nav data-link-type to button

--- scripts/src/modules/analytics/insights_tracking/set_mobile_data_link_type.js
+++ scripts/src/modules/analytics/insights_tracking/set_mobile_data_link_type.js
@@ -1,7 +1,7 @@
 export default function toggle_data_link_type(e) {
     const isExpanded = e.target.getAttribute("aria-expanded") === "true" ? true : false;
 
-    e.target.setAttribute("data-link-type", `${isExpanded ? "Expand" : "Collapse"} jump link`);
+    e.target.setAttribute("data-link-type", `${isExpanded ? "Expand" : "Collapse"} button`);
 
     return e.target.getAttribute("data-link-type");
 }
--- scripts/src/modules/analytics/insights_tracking/set_mobile_tracking_attributes.js
+++ scripts/src/modules/analytics/insights_tracking/set_mobile_tracking_attributes.js
@@ -3,7 +3,7 @@ export default function set_mobile_tracking_attributes(section_headings) {
         const isExpanded = item.getAttribute("aria-expanded") === "true" ? true : false;
 
         item.setAttribute("data-component-name", "Mobile in page navigation");
-        item.setAttribute("data-link-type", `${isExpanded ? "Expand" : "Collapse"} jump link`);
+        item.setAttribute("data-link-type", `${isExpanded ? "Expand" : "Collapse"} button`);
         item.setAttribute("data-position", index);
         item.setAttribute("data-link", item.textContent.trim());
     });
