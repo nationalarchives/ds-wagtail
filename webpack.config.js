cee2d63e51eaacfa203acd1d99ff4fced3921cc0
James <jwchan1993@gmail.com>
2022-07-13 13:07:06 +0100

    Refactor cookie consent JS implementation (#409)
    * Load correct gtm script
    
    * Add gtm script to webpack
    
    * Refactor cookie consent implementation
    
    Moved cookie consent modules into ds-wagtail for easier management

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
     mode: 'production',
     entry: {
         beta_banner: './scripts/src/beta-banner.js',
+        cookie_consent: './scripts/src/ds-cookie-consent.js',
+        gtm_script: './scripts/src/gtm-script.js',
         image_viewer: './scripts/src/image-viewer.js',
         home_page: './scripts/src/home-page.js',
         explorer: './scripts/src/explorer.js',
