daaac7685eb374e4dd97ae00596901f1b9a9fc3d
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-08-25 15:49:36 +0100

    DF-209:Amends-Removed input key attribute and arranged for form data input

--- etna/search/tests/test_forms.py
+++ etna/search/tests/test_forms.py
@@ -6,60 +6,52 @@ from ..forms import CatalogueSearchForm
 class CatalogueSearchFormTest(SimpleTestCase):
     def test_various_opening_date_inputs_is_valid(self):
         """tests inputs accross both date fields with various input formats for form validity"""
-        for label, value in (
+        for label, form_data in (
             (
                 "input end date with empty start date",
                 {
-                    "input": {
-                        "group": "tna",
-                        "opening_end_date_0": "01",
-                        "opening_end_date_1": "01",
-                        "opening_end_date_2": "2000",
-                    }
+                    "group": "tna",
+                    "opening_end_date_0": "01",
+                    "opening_end_date_1": "01",
+                    "opening_end_date_2": "2000",
                 },
             ),
             (
                 "input start date with empty end date",
                 {
-                    "input": {
-                        "group": "tna",
-                        "opening_start_date_0": "31",
-                        "opening_start_date_1": "12",
-                        "opening_start_date_2": "1999",
-                    }
+                    "group": "tna",
+                    "opening_start_date_0": "31",
+                    "opening_start_date_1": "12",
+                    "opening_start_date_2": "1999",
                 },
             ),
             (
                 "input start date before end date",
                 {
-                    "input": {
-                        "group": "tna",
-                        "opening_start_date_0": "31",
-                        "opening_start_date_1": "12",
-                        "opening_start_date_2": "1999",
-                        "opening_end_date_0": "01",
-                        "opening_end_date_1": "01",
-                        "opening_end_date_2": "2000",
-                    }
+                    "group": "tna",
+                    "opening_start_date_0": "31",
+                    "opening_start_date_1": "12",
+                    "opening_start_date_2": "1999",
+                    "opening_end_date_0": "01",
+                    "opening_end_date_1": "01",
+                    "opening_end_date_2": "2000",
                 },
             ),
             (
                 "input for day - start date and end date are same",
                 {
-                    "input": {
-                        "group": "tna",
-                        "opening_start_date_0": "31",
-                        "opening_start_date_1": "12",
-                        "opening_start_date_2": "1999",
-                        "opening_end_date_0": "31",
-                        "opening_end_date_1": "12",
-                        "opening_end_date_2": "1999",
-                    }
+                    "group": "tna",
+                    "opening_start_date_0": "31",
+                    "opening_start_date_1": "12",
+                    "opening_start_date_2": "1999",
+                    "opening_end_date_0": "31",
+                    "opening_end_date_1": "12",
+                    "opening_end_date_2": "1999",
                 },
             ),
         ):
             with self.subTest(label):
-                form = CatalogueSearchForm(value["input"])
+                form = CatalogueSearchForm(form_data)
                 self.assertTrue(form.is_valid(), label)
 
     def test_opening_start_date_before_opening_end_date_is_invalid(self):
