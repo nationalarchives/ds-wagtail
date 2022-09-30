f1631ae55a96523dc69205cbe4eee191fe9f296a
James Biggs <james.biggs@nationalarchives.gov.uk>
2022-09-30 20:46:08 +0000

    Added workaround for missing API data+applied HTML

--- etna/records/models.py
+++ etna/records/models.py
@@ -286,6 +286,23 @@ class Record(DataLayerMixin, APIModel):
 
     @cached_property
     def hierarchy(self) -> Tuple["Record"]:
+        for item in self.get("@hierarchy.0", default=()):
+            if item.get("identifier"):
+                pass
+            else:
+                level = item.get("level")
+                level_code = level.get("code")
+                hierarchy = self.get("@hierarchy.0")
+                previous_level_record = hierarchy[level_code-2]
+                previous_level_identifier = previous_level_record.get("identifier")[0]
+                previous_level_reference = previous_level_identifier.get("reference_number")
+                if level_code == 2:
+                    reference_number = "Division within " + previous_level_reference
+                elif level_code == 4:
+                    reference_number = "Sub-series within " + previous_level_reference
+                new_identifier = [{'primary': True, 'reference_number': reference_number, 'type': 'reference number', 'value': reference_number}]
+                item["identifier"] = new_identifier
+                print(new_identifier)
         return tuple(
             Record(item)
             for item in self.get("@hierarchy.0", default=())
