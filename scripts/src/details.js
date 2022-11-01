29dbb34e625e3efdb2785c457bb7aeead55b9699
Matt Blair <7143978+matt-blair@users.noreply.github.com>
2022-11-01 14:47:15 +0000

    Creating files and adding JS, CSS and HTML to show/hide hierarchy detailed view

--- scripts/src/details.js
+++ scripts/src/details.js
@@ -1,30 +1,16 @@
-import manage_details_element from './modules/manage_details_element';
 import scroll_to_bottom from "./modules/scroll_to_bottom";
 import push_reference_and_series from "./modules/analytics/push_reference_and_series";
 import add_unique_ids from "./modules/analytics/add_unique_ids";
+import toggle_detailed_view from "./modules/records/toggle_detailed_view";
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const hierarchy_list = document.querySelector('.hierarchy-global__list');
     const hierarchy_list_current_item = document.querySelector(".hierarchy-global__list-item--current-item");
 
-    manage_details_element();
     scroll_to_bottom(hierarchy_list_current_item, hierarchy_list);
     push_reference_and_series();
     add_unique_ids();
+    toggle_detailed_view();
 
-    const summary_element = document.querySelector('#js-hierarchy-global summary');
-
-    summary_element.addEventListener('click', e => {
-
-        if (window.innerWidth < 1200) {
-            return;
-        }
-        e.preventDefault();
-    });
-
-});
-
-window.addEventListener("resize", () => {
-    manage_details_element();
 });
