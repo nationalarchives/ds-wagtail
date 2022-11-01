83bf2d0e3900ec26790eb8edeb77c59079c5b0d5
Matt Blair <7143978+matt-blair@users.noreply.github.com>
2022-11-01 19:47:50 +0000

    Creating files and adding JS, CSS and HTML to show/hide cta panel

--- scripts/src/details.js
+++ scripts/src/details.js
@@ -2,6 +2,7 @@ import scroll_to_bottom from "./modules/scroll_to_bottom";
 import push_reference_and_series from "./modules/analytics/push_reference_and_series";
 import add_unique_ids from "./modules/analytics/add_unique_ids";
 import toggle_detailed_view from "./modules/records/toggle_detailed_view";
+import toggle_order_options from "./modules/records/toggle_order_options";
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -12,5 +13,5 @@ document.addEventListener("DOMContentLoaded", () => {
     push_reference_and_series();
     add_unique_ids();
     toggle_detailed_view();
-
+    toggle_order_options();
 });
