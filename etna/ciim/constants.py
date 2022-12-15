dd99ddff3b32ae20cde352db8ff2762fd848a5a4
TNA_Allan <allan.fernandes@nationalarchives.gov.uk>
2022-12-15 13:45:22 +0000

    UN-411:Amends-restore line to previous state

--- etna/ciim/constants.py
+++ etna/ciim/constants.py
@@ -115,7 +115,7 @@ FEATURED_BUCKETS = BucketList(
         Bucket(key="creator", label="Record creators"),
         Bucket(key="blog", label="Blogs"),
         Bucket(key="researchGuide", label="Research Guides"),
-        Bucket(key="insight", label="Insights from the collection"),
+        Bucket(key=BucketKeys.INSIGHT.value, label="Stories from the collection"),
     ]
 )
 
