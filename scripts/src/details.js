0219a458dba3445effbc20ffa1f8411ccdd53822
Matt Blair <7143978+matt-blair@users.noreply.github.com>
2022-12-19 11:19:59 +0000

    Removing redundant JS to fix scroll-to-bottom JS error

--- scripts/src/details.js
+++ scripts/src/details.js
@@ -1,4 +1,3 @@
-import scroll_to_bottom from "./modules/scroll_to_bottom";
 import push_reference_and_series from "./modules/analytics/push_reference_and_series";
 import add_unique_ids from "./modules/analytics/add_unique_ids";
 import toggle_detailed_view from "./modules/records/toggle_detailed_view";
@@ -6,10 +5,6 @@ import toggle_order_options from "./modules/records/toggle_order_options";
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const hierarchy_list = document.querySelector('.hierarchy-global__list');
-    const hierarchy_list_current_item = document.querySelector(".hierarchy-global__list-item--current-item");
-
-    scroll_to_bottom(hierarchy_list_current_item, hierarchy_list);
     push_reference_and_series();
     add_unique_ids();
     toggle_detailed_view();
