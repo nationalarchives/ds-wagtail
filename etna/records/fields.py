9b576358d10a7315426ecafdc0589f228066596c
Andy Babic <andy.babic@torchbox.com>
2023-01-20 22:31:28 +0000

    Modify RecordChooserField to lazily fetch record data when the value is interacted with in some way

--- etna/records/fields.py
+++ etna/records/fields.py
@@ -1,12 +1,51 @@
+from typing import Any
+
 from django.core.exceptions import ValidationError
 from django.db.models.fields import Field
+from django.utils.functional import SimpleLazyObject
 
 from ..ciim.exceptions import KongBadRequestError
 from .models import Record
 from .widgets import RecordChooser
 
 
+class LazyRecord(SimpleLazyObject):
+    """
+    The return type for ``RecordChooserField``, which lazily fetches
+    the full record details from CIIM when the value is interacted
+    with in some way. For example, when requesting a ``Record``
+    attribute value.
+    """
+
+    def __init__(self, iaid: str):
+        self.__dict__["iaid"] = iaid
+
+        def _do_fetch():
+            try:
+                return Record.api.fetch(iaid=iaid)
+            except KongBadRequestError as e:
+                return ValidationError(str(e))
+
+        super().__init__(_do_fetch)
+
+    def __getattribute__(self, name: str) -> Any:
+        """
+        Allow access to ``self.iaid`` without fetching the record details
+        from CIIM.
+        """
+        if name == "iaid" and "iaid" in self.__dict__:
+            return self.__dict__["iaid"]
+        return super().__getattribute__(name)
+
+
 class RecordChooserField(Field):
+    """
+    A model field that presents editors with a ``RecordChooser`` widget
+    to allow selection of a record from CIIM, stores the ``iaid`` of
+    that record in the database, then converts it into a ``LazyRecord``
+    when the model instance is retrieved again from the database.
+    """
+
     def __init__(self, *args, **kwargs):
         kwargs["max_length"] = 50
         super().__init__(*args, **kwargs)
@@ -27,19 +66,20 @@ class RecordChooserField(Field):
         return "CharField"
 
     def to_python(self, value):
-        if isinstance(value, Record) or value is None:
-            return value
-        try:
-            return Record.api.fetch(iaid=value)
-        except KongBadRequestError as e:
-            raise ValidationError(e)
+        if isinstance(value, str):
+            if not value:
+                return None
+            return LazyRecord(value)
+        return value
 
     def from_db_value(self, value, expression, connection):
-        if value is None:
-            return value
-        return Record.api.fetch(iaid=value)
+        if isinstance(value, str):
+            if not value:
+                return None
+            return LazyRecord(value)
+        return value
 
     def get_prep_value(self, value):
-        if isinstance(value, Record):
-            return value.iaid
-        return self.to_python(value)
+        if isinstance(value, str) or value is None:
+            return value
+        return value.iaid
