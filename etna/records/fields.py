de71a450370484025bc4771cfc31e5082b8d7fa7
Andy Babic <andy.babic@torchbox.com>
2023-01-21 10:59:52 +0000

    Force is_null=True and perform an immediate lookup in to_python() as a form of validation

--- etna/records/fields.py
+++ etna/records/fields.py
@@ -4,39 +4,36 @@ from django.core.exceptions import ValidationError
 from django.db.models.fields import Field
 from django.utils.functional import SimpleLazyObject
 
-from ..ciim.exceptions import KongBadRequestError
+from requests import HTTPError
+
 from .models import Record
 from .widgets import RecordChooser
 
 
 class LazyRecord(SimpleLazyObject):
     """
-    The return type for ``RecordChooserField``, which lazily fetches
-    the full record details from CIIM when the value is interacted
-    with in some way. For example, when requesting a ``Record``
-    attribute value.
+    The return type for ``RecordChooserField.from_db_value()``, which lazily
+    fetches the record details from CIIM when the value is interacted
+    with in some way. For example, when requesting a ``Record`` attribute
+    value, like: ``obj.record.title``
     """
 
     def __init__(self, iaid: str):
         self.__dict__["iaid"] = iaid
 
-        def _do_fetch():
-            try:
-                return Record.api.fetch(iaid=iaid)
-            except KongBadRequestError as e:
-                return ValidationError(str(e))
+        def _fetch_record():
+            return Record.api.fetch(iaid=iaid)
 
-        super().__init__(_do_fetch)
+        super().__init__(_fetch_record)
 
     def __getattribute__(self, name: str) -> Any:
         """
-        Allow access to ``iaid`` without fetching the full record details
-        from CIIM.
-
-        # The local ``iaid`` value is not preserved when copying or pickling,
-        # the value (hence the extra check here).
+        Overrides ``SimpleLazyObject.__getattribute__()`` to allow access to
+        ``iaid`` without fetching the full record details from CIIM.
         """
         if name == "iaid" and "iaid" in self.__dict__:
+            # The stored ``iaid`` value is not preserved when copying or
+            # pickling; hence the extra conditional.
             return self.__dict__["iaid"]
         return super().__getattribute__(name)
 
@@ -50,16 +47,17 @@ class RecordChooserField(Field):
     """
 
     def __init__(self, *args, **kwargs):
-        kwargs["max_length"] = 50
+        kwargs.update(max_length=50, null=True)
         super().__init__(*args, **kwargs)
 
     def deconstruct(self):
         """
-        RecordChooserFields have a fixed max_length of 50, so it doesn't
-        need to be included in field breakdowns in migrations.
+        RecordChooserFields have fixed "max_length" and "null" options,
+        so we can ignore them in field breakdowns (in migrations).
         """
         name, path, args, kwargs = super().deconstruct()
         del kwargs["max_length"]
+        del kwargs["null"]
         return name, path, args, kwargs
 
     def formfield(self, **kwargs):
@@ -77,25 +75,28 @@ class RecordChooserField(Field):
 
     def to_python(self, value):
         """
-        Return ``None`` if the value is null or an empty string.
-        Otherwise, return create a ``LazyRecord`` from the stored
-        "iaid" value.
+        Return ``None`` if the value is null or an empty string. Otherwise, use
+        the "iaid" string to fetch and return a ``Record`` instance from CIIM.
+
+        NOTE: We do not use ``LazyRecord`` here, because we want to attempt a
+        record fetch as a means of validating the "iaid" value (``to_python()``
+        is used in model/form validation).
         """
         if isinstance(value, str):
-            if not value:
-                return None
-            return LazyRecord(value)
+            try:
+                return Record.api.fetch(iaid=value)
+            except HTTPError:
+                return ValidationError(
+                    "CIIM could not return a record with iaid '{value}'. "
+                )
         return value
 
     def from_db_value(self, value, expression, connection):
         """
-        Return ``None`` if the value is null or an empty string.
-        Otherwise, return create a ``LazyRecord`` from the stored
-        "iaid" value.
+        Return ``None`` if the value is null or an empty string. Otherwise,
+        create and return a ``LazyRecord`` from the stored "iaid" value.
         """
         if isinstance(value, str):
-            if not value:
-                return None
             return LazyRecord(value)
         return value
 
@@ -105,6 +106,6 @@ class RecordChooserField(Field):
         ``Record`` or ``LazyRecord`` instance, extract the "iaid" value to
         store in the DB.
         """
-        if isinstance(value, str) or value is None:
-            return value
-        return value.iaid
+        if isinstance(value, str):
+            return value or None
+        return getattr(value, "iaid", None)
