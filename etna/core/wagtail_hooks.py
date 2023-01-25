1003ca42ac8f9871754ccd7023296db9ea518dc1
James Biggs <james.biggs@nationalarchives.gov.uk>
2023-01-25 15:17:03 +0000

    Changed colour to meet accessibility requirements

--- etna/core/wagtail_hooks.py
+++ etna/core/wagtail_hooks.py
@@ -14,5 +14,5 @@ def editor_css():
 @hooks.register('insert_global_admin_css')
 def global_admin_css():
     if settings.FEATURE_PLATFORM_ENVIRONMENT_TYPE in ["development", "staging"]:
-        return '<style> :root {--w-color-primary: #104100; --w-color-primary-200: #103200;} </style>'
+        return '<style> :root {--w-color-primary: #00623B; --w-color-primary-200: #003c1e;} </style>'
     return ""
