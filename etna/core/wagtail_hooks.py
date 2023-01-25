752c9df4ef37d20d793fbddff22c0129a112ffd5
James Biggs <james.biggs@nationalarchives.gov.uk>
2023-01-25 15:20:01 +0000

    Formatting

--- etna/core/wagtail_hooks.py
+++ etna/core/wagtail_hooks.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.templatetags.static import static
 from django.utils.html import format_html
-from django.conf import settings
 
 from wagtail.core import hooks
 
@@ -11,8 +11,9 @@ def editor_css():
         '<link rel="stylesheet" href="{}">', static("css/dist/etna-wagtail-editor.css")
     )
 
-@hooks.register('insert_global_admin_css')
+
+@hooks.register("insert_global_admin_css")
 def global_admin_css():
     if settings.FEATURE_PLATFORM_ENVIRONMENT_TYPE in ["development", "staging"]:
-        return '<style> :root {--w-color-primary: #00623B; --w-color-primary-200: #003c1e;} </style>'
+        return "<style> :root {--w-color-primary: #00623B; --w-color-primary-200: #003c1e;} </style>"
     return ""
