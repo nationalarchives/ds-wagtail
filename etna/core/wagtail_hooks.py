31ddcd8bdf5b44a206e72655795174c8b1fa0622
James Biggs <62654785+jamesbiggs@users.noreply.github.com>
2023-02-07 10:50:13 +0000

    UN-324: Edit admin page to show the difference between environments (#600)
    * Created etna-wagtail-admin.scss for colour change
    
    * Testing .env variables to differentiate dev/main
    
    * Making it NOT develop to spoof "production"
    
    * Undoing last commit
    
    * Trying if statements in CSS
    
    * Creating variable for environment
    
    * Removed SASS as it can be applied via the hooks
    
    * Removed the commands for the wagtail-admin SASS
    
    * Updated logic handling and returning style tag instead of CSS file
    
    * Changing variable name to match env variable name
    
    * Changes to home.html to test the retrieval of PLATFORM_ENVIRONMENT_TYPE
    
    * Same as previous
    
    * Final check on P.SH
    
    * Returning to original logic after testing on P.SH
    
    * Fixing django template issue
    
    * Changed to a darker shade of green
    
    * Changed colour to meet accessibility requirements
    
    * Formatting
    
    * Returning FEATURE_RECORD_LINKS_GO_TO_DISCOVERY back to False
    
    * Changed default value to "production" instead of empty string

--- etna/core/wagtail_hooks.py
+++ etna/core/wagtail_hooks.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.templatetags.static import static
 from django.utils.html import format_html
 
@@ -9,3 +10,10 @@ def editor_css():
     return format_html(
         '<link rel="stylesheet" href="{}">', static("css/dist/etna-wagtail-editor.css")
     )
+
+
+@hooks.register("insert_global_admin_css")
+def global_admin_css():
+    if settings.FEATURE_PLATFORM_ENVIRONMENT_TYPE != "production":
+        return "<style> :root {--w-color-primary: #00623B; --w-color-primary-200: #003c1e;} </style>"
+    return ""
