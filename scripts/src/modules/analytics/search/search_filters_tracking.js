518fff5fc1346d52251895d98f7ed57f7d72a18d
Jane <jane.hughes@torchbox.com>
2023-02-15 21:49:32 +1100

    set up gtm tracking for search filters

---
+++ scripts/src/modules/analytics/search/search_filters_tracking.js
@@ -0,0 +1,35 @@
+import push_to_data_layer from "./../push_to_data_layer";
+
+const getFilters = () => {
+    // get filters after DOM has loaded and they have rendered on page
+    window.addEventListener('load', () => {
+        const filters = document.querySelectorAll('[data-filter]');
+
+        loopFilters(filters);
+    });
+}
+
+const loopFilters = (filters) => {
+    filters.forEach((filter) => {
+        const bucket = filter.getAttribute('data-search-bucket');
+        const value = filter.getAttribute('data-filter-value');
+        const name = filter.getAttribute('data-filter-name');
+
+        const filterData = {
+            'search-type':  dataLayer[0].customDimension3 || '',
+            'search-bucket': bucket || '',
+            'search-filter-value': value || '',
+            'search-filter-name': name || '',
+        }; 
+
+        push_to_data_layer(filterData);
+
+        console.log(filterData);
+    });
+}
+
+const searchFiltersTracking = () => {
+    getFilters();
+};
+
+export default searchFiltersTracking;
