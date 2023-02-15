73e96849f29f2cc7653d5be3efd8bc1ae89626f0
Andy Babic <andy.babic@torchbox.com>
2023-02-15 10:31:33 +0000

    Move ResultsPage and ResultsPageRecord models to bottom of models file

--- etna/collections/models.py
+++ etna/collections/models.py
@@ -265,75 +265,6 @@ class TimePeriodExplorerPage(AlertMixin, TeaserImageMixin, MetadataPageMixin, Ba
         )
 
 
-class ResultsPage(AlertMixin, TeaserImageMixin, MetadataPageMixin, BasePage):
-    """Results BasePage.
-
-    This page is a placeholder for the results page at the end of a user's
-    journey through the collection explorer.
-
-    Eventually this page will run an editor-defined query against the
-    collections API and display the results.
-    """
-
-    title_prefix = models.CharField(max_length=200, blank=True)
-    sub_heading = models.CharField(max_length=200, blank=False)
-    introduction = models.TextField(blank=False)
-
-    content_panels = BasePage.content_panels + [
-        FieldPanel("title_prefix"),
-        FieldPanel("sub_heading"),
-        FieldPanel("introduction"),
-        InlinePanel("records", heading="Records"),
-    ]
-    promote_panels = MetadataPageMixin.promote_panels + TeaserImageMixin.promote_panels
-    settings_panels = BasePage.settings_panels + AlertMixin.settings_panels
-
-    parent_page_types = [
-        "collections.TimePeriodExplorerPage",
-        "collections.TopicExplorerPage",
-    ]
-    subpage_types = []
-
-    # DataLayerMixin overrides
-    gtm_content_group = "Explorer"
-
-
-class ResultsPageRecord(Orderable, models.Model):
-    """Map orderable records data to ResultsPage"""
-
-    page = ParentalKey("ResultsPage", on_delete=models.CASCADE, related_name="records")
-    record_iaid = models.TextField(verbose_name="Record")
-    teaser_image = models.ForeignKey(
-        get_image_model_string(),
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name="+",
-    )
-    description = models.TextField(
-        help_text="Optional field to override the description for this record in the teaser.",
-        blank=True,
-    )
-
-    @cached_property
-    def record(self):
-        """Fetch associated record BasePage.
-
-        Capture any exception thrown by KongClient and return None so we can
-        skip this record on the results BasePage.
-        """
-        try:
-            return Record.api.fetch(iaid=self.record_iaid)
-        except (KongAPIError, APIManagerException):
-            return None
-
-    panels = [
-        FieldPanel("record_iaid", widget=RecordChooser),
-        FieldPanel("teaser_image"),
-        FieldPanel("description"),
-    ]
-
-
 class PageTopic(Orderable):
     """
     This model allows any page type to be associated with one or more topics
@@ -463,3 +394,72 @@ class TopicalPageMixin:
         one big comma-separated string. Ideal for indexing!
         """
         return ", ".join(item.title for item in self.time_periods)
+
+
+class ResultsPage(AlertMixin, TeaserImageMixin, MetadataPageMixin, BasePage):
+    """Results BasePage.
+
+    This page is a placeholder for the results page at the end of a user's
+    journey through the collection explorer.
+
+    Eventually this page will run an editor-defined query against the
+    collections API and display the results.
+    """
+
+    title_prefix = models.CharField(max_length=200, blank=True)
+    sub_heading = models.CharField(max_length=200, blank=False)
+    introduction = models.TextField(blank=False)
+
+    content_panels = BasePage.content_panels + [
+        FieldPanel("title_prefix"),
+        FieldPanel("sub_heading"),
+        FieldPanel("introduction"),
+        InlinePanel("records", heading="Records"),
+    ]
+    promote_panels = MetadataPageMixin.promote_panels + TeaserImageMixin.promote_panels
+    settings_panels = BasePage.settings_panels + AlertMixin.settings_panels
+
+    parent_page_types = [
+        "collections.TimePeriodExplorerPage",
+        "collections.TopicExplorerPage",
+    ]
+    subpage_types = []
+
+    # DataLayerMixin overrides
+    gtm_content_group = "Explorer"
+
+
+class ResultsPageRecord(Orderable, models.Model):
+    """Map orderable records data to ResultsPage"""
+
+    page = ParentalKey("ResultsPage", on_delete=models.CASCADE, related_name="records")
+    record_iaid = models.TextField(verbose_name="Record")
+    teaser_image = models.ForeignKey(
+        get_image_model_string(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    description = models.TextField(
+        help_text="Optional field to override the description for this record in the teaser.",
+        blank=True,
+    )
+
+    @cached_property
+    def record(self):
+        """Fetch associated record BasePage.
+
+        Capture any exception thrown by KongClient and return None so we can
+        skip this record on the results BasePage.
+        """
+        try:
+            return Record.api.fetch(iaid=self.record_iaid)
+        except (KongAPIError, APIManagerException):
+            return None
+
+    panels = [
+        FieldPanel("record_iaid", widget=RecordChooser),
+        FieldPanel("teaser_image"),
+        FieldPanel("description"),
+    ]
