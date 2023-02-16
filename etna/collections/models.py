a036b4be2ca9885ac137daf3518d4a059ab839ad
Andy Babic <andy.babic@torchbox.com>
2023-02-16 14:08:12 +0000

    UN-328: Highlights gallery page BE (#650)
    * Move ResultsPage and ResultsPageRecord models to bottom of models file
    
    * Add page type and supporting highlight model
    
    * Allow highlight galleries to live below either topics or time periods
    
    * Complain when a highlight image doesn't have a record specified
    
    * Add template with 'starter output'
    
    * Add help text to related topic / time period inline panels

--- etna/collections/models.py
+++ etna/collections/models.py
@@ -1,14 +1,17 @@
 from typing import List, Optional, Tuple, Union
 
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 
 from modelcluster.fields import ParentalKey
 from wagtail.admin.panels import FieldPanel, InlinePanel
-from wagtail.fields import StreamField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.images import get_image_model_string
 from wagtail.models import Orderable, Page
+from wagtail.search import index
 
 from wagtailmetadata.models import MetadataPageMixin
 
@@ -144,7 +147,11 @@ class TopicExplorerPage(AlertMixin, TeaserImageMixin, MetadataPageMixin, BasePag
         "collections.TopicExplorerIndexPage",
         "collections.TopicExplorerPage",
     ]
-    subpage_types = ["collections.TopicExplorerPage", "collections.ResultsPage"]
+    subpage_types = [
+        "collections.TopicExplorerPage",
+        "collections.HighlightGalleryPage",
+        "collections.ResultsPage",
+    ]
 
     @cached_property
     def related_articles(self):
@@ -251,7 +258,11 @@ class TimePeriodExplorerPage(AlertMixin, TeaserImageMixin, MetadataPageMixin, Ba
         "collections.TimePeriodExplorerIndexPage",
         "collections.TimePeriodExplorerPage",
     ]
-    subpage_types = ["collections.TimePeriodExplorerPage", "collections.ResultsPage"]
+    subpage_types = [
+        "collections.TimePeriodExplorerPage",
+        "collections.HighlightGalleryPage",
+        "collections.ResultsPage",
+    ]
 
     @cached_property
     def related_articles(self):
@@ -263,6 +274,277 @@ class TimePeriodExplorerPage(AlertMixin, TeaserImageMixin, MetadataPageMixin, Ba
             .select_related("teaser_image")
             .order_by("title")[:3]
         )
+
+
+class PageTopic(Orderable):
+    """
+    This model allows any page type to be associated with one or more topics
+    in a way that retains the order of topics selected.
+
+    The ``sort_order`` field value from ``Orderable`` can be used to pull out
+    the 'first' topic to treat as the 'primary topic' for a page, and can also
+    used to prioritise items for a list of 'pages related to a topic'.
+
+    Just add `InlinePanel("page_topics")` to a page type's panel
+    configuration to use it!
+    """
+
+    page = ParentalKey(Page, on_delete=models.CASCADE, related_name="page_topics")
+    topic = models.ForeignKey(
+        TopicExplorerPage,
+        verbose_name=_("topic"),
+        related_name="topic_pages",
+        on_delete=models.CASCADE,
+    )
+
+
+class PageTimePeriod(Orderable):
+    """
+    This model allows any page type to be associated with one or more topics
+    in a way that retains the order of topics selected.
+
+    The ``sort_order`` field value from ``Orderable`` can be used to pull out
+    the 'first' topic to treat as the 'primary topic' for a page, and can also
+    used to prioritise items for a list of 'pages related to a topic'.
+
+    Just add `InlinePanel("page_time_periods")` to a page type's panel
+    configuration to use it!
+    """
+
+    page = ParentalKey(Page, on_delete=models.CASCADE, related_name="page_time_periods")
+    time_period = models.ForeignKey(
+        TimePeriodExplorerPage,
+        verbose_name=_("time period"),
+        related_name="time_period_pages",
+        on_delete=models.CASCADE,
+    )
+
+
+class TopicalPageMixin:
+    """
+    A mixin for pages that use the ``PageTopic`` and ``PageTimePeriod`` models
+    in order to be associated with one or many topics/time periods. It simply
+    adds a few properies to support robust, efficient access the related topic
+    and time period pages.
+    """
+
+    @classmethod
+    def get_time_periods_inlinepanel(
+        cls, max_num: Optional[int] = 4, min_num: Optional[int] = None
+    ) -> InlinePanel:
+        return InlinePanel(
+            "page_time_periods",
+            heading=_("Related time periods"),
+            help_text=_(
+                "Where possible, specify these in relevancy order (most relevant first)."
+            ),
+            min_num=min_num,
+            max_num=max_num,
+        )
+
+    @classmethod
+    def get_topics_inlinepanel(
+        cls, max_num: Optional[int] = 4, min_num: Optional[int] = None
+    ) -> InlinePanel:
+        return InlinePanel(
+            "page_topics",
+            heading=_("Related topics"),
+            help_text=_(
+                "Where possible, specify these in relevancy order (most relevant first)."
+            ),
+            min_num=min_num,
+            max_num=max_num,
+        )
+
+    @cached_property
+    def primary_topic(self) -> Union[TopicExplorerPage, None]:
+        try:
+            return self.topics[0]
+        except IndexError:
+            return None
+
+    @cached_property
+    def topics(self) -> Tuple[TopicExplorerPage]:
+        return tuple(
+            item.topic
+            for item in self.page_topics.select_related("topic").filter(
+                topic__live=True
+            )
+        )
+
+    @cached_property
+    def topics_alphabetical(self) -> List[TopicExplorerPage]:
+        return sorted(self.topic, key=lambda item: item.title.lower())
+
+    @property
+    def topic_names(self) -> str:
+        """
+        Returns the titles of all related topics, joined together into one big
+        comma-separated string. Ideal for indexing!
+        """
+        return ", ".join(item.title for item in self.topics)
+
+    @cached_property
+    def primary_time_period(self) -> Union[TimePeriodExplorerPage, None]:
+        try:
+            return self.time_periods[0]
+        except IndexError:
+            return None
+
+    @cached_property
+    def time_periods(self) -> Tuple[TimePeriodExplorerPage]:
+        return tuple(
+            item.time_period
+            for item in self.page_time_periods.select_related("time_period").filter(
+                time_period__live=True
+            )
+        )
+
+    @cached_property
+    def time_periods_chronological(self) -> List[TimePeriodExplorerPage]:
+        return sorted(self.time_periods, key=lambda item: item.start_year)
+
+    @property
+    def time_period_names(self) -> str:
+        """
+        Returns the titles of all related time periods, joined together into
+        one big comma-separated string. Ideal for indexing!
+        """
+        return ", ".join(item.title for item in self.time_periods)
+
+
+class HighlightGalleryPage(
+    TopicalPageMixin, TeaserImageMixin, MetadataPageMixin, BasePage
+):
+    parent_page_types = [TimePeriodExplorerPage, TopicExplorerPage]
+    subpage_types = []
+
+    # NOTE: To be moved to mixin/base page as part of UN-471
+    intro = RichTextField(
+        verbose_name=_("introductory text"),
+        help_text=_(
+            "1-2 sentences introducing the subject of the article and explaining why a user should read on."
+        ),
+        features=settings.INLINE_RICH_TEXT_FEATURES,
+        max_length=300,
+    )
+
+    featured_record_article = models.ForeignKey(
+        "articles.RecordArticlePage",
+        verbose_name=_("featured record article"),
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    featured_article = models.ForeignKey(
+        "articles.ArticlePage",
+        verbose_name=_("featured article"),
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    # NOTE: To be moved to mixin/base page as part of UN-471
+    teaser_text = models.TextField(
+        verbose_name=_("teaser text"),
+        help_text=_(
+            "A short, enticing description of the article. This will appear in promos and under thumbnails around the site."
+        ),
+        max_length=160,
+    )
+
+    class Meta:
+        verbose_name = _("highlight gallery page")
+        verbose_name_plural = _("highlight gallery pages")
+
+    content_panels = BasePage.content_panels + [
+        FieldPanel("intro"),
+        InlinePanel(
+            "page_highlights",
+            heading=_("Highlights"),
+            label=_("Item"),
+            min_num=2,
+            max_num=15,
+        ),
+        FieldPanel("featured_record_article"),
+        FieldPanel("featured_article"),
+        TopicalPageMixin.get_topics_inlinepanel(),
+        TopicalPageMixin.get_time_periods_inlinepanel(),
+    ]
+
+    promote_panels = (
+        MetadataPageMixin.promote_panels
+        + TeaserImageMixin.promote_panels
+        + [
+            FieldPanel("teaser_text"),
+        ]
+    )
+
+    search_fields = BasePage.search_fields + [
+        index.SearchField("intro", boost=1),
+        index.SearchField("highlights_text", boost=1),
+        index.SearchField("topic_names"),
+        index.SearchField("time_period_names"),
+        index.SearchField("teaser_text"),
+    ]
+
+    @cached_property
+    def highlights(self):
+        """
+        Used to access the page's 'highlights' for output. Makes use of
+        Django's select_related() and prefetch_related() to efficiently
+        prefetch image and rendition data from the database.
+        """
+        return (
+            self.page_highlights.exclude(image__isnull=True)
+            .select_related("image")
+            .prefetch_related("image__renditions")
+        )
+
+    @property
+    def highlights_text(self) -> str:
+        """
+        Returns all of the relevant text defined for this page's highlights,
+        joined into one giant string to faciliate indexing.
+        """
+        strings = []
+        for item in self.highlights:
+            strings.extend([item.image.title, item.long_description])
+        return " | ".join(strings)
+
+
+class Highlight(Orderable):
+    page = ParentalKey(
+        "wagtailcore.Page", on_delete=models.CASCADE, related_name="page_highlights"
+    )
+    image = models.ForeignKey(
+        get_image_model_string(),
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name=_("image"),
+    )
+    long_description = RichTextField(
+        verbose_name=_("long description"),
+        features=settings.RESTRICTED_RICH_TEXT_FEATURES,
+        max_length=400,
+    )
+
+    panels = [
+        FieldPanel("image"),
+        FieldPanel("long_description"),
+    ]
+
+    def clean(self) -> None:
+        if self.image and self.image.record is None:
+            raise ValidationError(
+                {
+                    "image": [
+                        "Only images with a 'record' specified can be used for highlights."
+                    ]
+                }
+            )
+        return super().clean()
 
 
 class ResultsPage(AlertMixin, TeaserImageMixin, MetadataPageMixin, BasePage):
@@ -332,134 +614,3 @@ class ResultsPageRecord(Orderable, models.Model):
         FieldPanel("teaser_image"),
         FieldPanel("description"),
     ]
-
-
-class PageTopic(Orderable):
-    """
-    This model allows any page type to be associated with one or more topics
-    in a way that retains the order of topics selected.
-
-    The ``sort_order`` field value from ``Orderable`` can be used to pull out
-    the 'first' topic to treat as the 'primary topic' for a page, and can also
-    used to prioritise items for a list of 'pages related to a topic'.
-
-    Just add `InlinePanel("page_topics")` to a page type's panel
-    configuration to use it!
-    """
-
-    page = ParentalKey(Page, on_delete=models.CASCADE, related_name="page_topics")
-    topic = models.ForeignKey(
-        TopicExplorerPage,
-        verbose_name=_("topic"),
-        related_name="topic_pages",
-        on_delete=models.CASCADE,
-    )
-
-
-class PageTimePeriod(Orderable):
-    """
-    This model allows any page type to be associated with one or more topics
-    in a way that retains the order of topics selected.
-
-    The ``sort_order`` field value from ``Orderable`` can be used to pull out
-    the 'first' topic to treat as the 'primary topic' for a page, and can also
-    used to prioritise items for a list of 'pages related to a topic'.
-
-    Just add `InlinePanel("page_time_periods")` to a page type's panel
-    configuration to use it!
-    """
-
-    page = ParentalKey(Page, on_delete=models.CASCADE, related_name="page_time_periods")
-    time_period = models.ForeignKey(
-        TimePeriodExplorerPage,
-        verbose_name=_("time period"),
-        related_name="time_period_pages",
-        on_delete=models.CASCADE,
-    )
-
-
-class TopicalPageMixin:
-    """
-    A mixin for pages that use the ``PageTopic`` and ``PageTimePeriod`` models
-    in order to be associated with one or many topics/time periods. It simply
-    adds a few properies to support robust, efficient access the related topic
-    and time period pages.
-    """
-
-    @classmethod
-    def get_time_periods_inlinepanel(
-        cls, max_num: Optional[int] = 4, min_num: Optional[int] = None
-    ) -> InlinePanel:
-        return InlinePanel(
-            "page_time_periods",
-            heading=_("Related time periods"),
-            min_num=min_num,
-            max_num=max_num,
-        )
-
-    @classmethod
-    def get_topics_inlinepanel(
-        cls, max_num: Optional[int] = 4, min_num: Optional[int] = None
-    ) -> InlinePanel:
-        return InlinePanel(
-            "page_topics",
-            heading=_("Related topics"),
-            min_num=min_num,
-            max_num=max_num,
-        )
-
-    @cached_property
-    def primary_topic(self) -> Union[TopicExplorerPage, None]:
-        try:
-            return self.topics[0]
-        except IndexError:
-            return None
-
-    @cached_property
-    def topics(self) -> Tuple[TopicExplorerPage]:
-        return tuple(
-            item.topic
-            for item in self.page_topics.select_related("topic").filter(
-                topic__live=True
-            )
-        )
-
-    @cached_property
-    def topics_alphabetical(self) -> List[TopicExplorerPage]:
-        return sorted(self.topic, key=lambda item: item.title.lower())
-
-    @property
-    def topic_names(self) -> str:
-        """
-        Returns the titles of all related topics, joined together into one big
-        comma-separated string. Ideal for indexing!
-        """
-        return ", ".join(item.title for item in self.topics)
-
-    @cached_property
-    def primary_time_period(self) -> Union[TimePeriodExplorerPage, None]:
-        try:
-            return self.time_periods[0]
-        except IndexError:
-            return None
-
-    @cached_property
-    def time_periods(self) -> Tuple[TimePeriodExplorerPage]:
-        return tuple(
-            item.time_period
-            for item in self.page_time_periods.select_related("time_period").filter(
-                time_period__live=True
-            )
-        )
-
-    @cached_property
-    def time_periods_chronological(self) -> List[TimePeriodExplorerPage]:
-        return sorted(self.time_periods, key=lambda item: item.start_year)
-
-    @property
-    def time_period_names(self) -> str:
-        """
-        Returns the titles of all related time periods, joined together into
-        one big comma-separated string. Ideal for indexing!
-        """
-        return ", ".join(item.title for item in self.time_periods)
