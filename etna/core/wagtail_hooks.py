6882f6e779b285c6c70ec0922d021bf49c1acb10
James Biggs <62654785+jamesbiggs@users.noreply.github.com>
2023-03-02 14:55:18 +0000

    UN-483: Upgrade to Wagtail 4.2 (#648)
    * Update poetry files
    
    * Replace deprecated `wagtail.core` with `wagtail`
    
    * Replace deprecated `wagtail.tests` with `wagtail.test`
    
    * New migration for `CustomImage` model
    
    * Replace deprecated `class_name` with `classname`
    
    * Formatting
    
    * Updating `wagtailmedia` to 0.13 to be compatible with `wagtail` 4.2
    
    * Update poetry files
    
    * Replace deprecated `wagtail.core` with `wagtail`
    
    * Replace deprecated `wagtail.tests` with `wagtail.test`
    
    * New migration for `CustomImage` model
    
    * Replace deprecated `class_name` with `classname`
    
    * Formatting
    
    * Updating `wagtailmedia` to 0.13 to be compatible with `wagtail` 4.2
    
    * Remove `etna-wagtail-editor` SASS and move into static CSS file `wagtail-overrides`

--- etna/core/wagtail_hooks.py
+++ etna/core/wagtail_hooks.py
@@ -2,13 +2,13 @@ from django.conf import settings
 from django.templatetags.static import static
 from django.utils.html import format_html
 
-from wagtail.core import hooks
+from wagtail import hooks
 
 
 @hooks.register("insert_editor_css")
 def editor_css():
     return format_html(
-        '<link rel="stylesheet" href="{}">', static("css/dist/etna-wagtail-editor.css")
+        '<link rel="stylesheet" href="{}">', static("css/wagtail-overrides.css")
     )
 
 
