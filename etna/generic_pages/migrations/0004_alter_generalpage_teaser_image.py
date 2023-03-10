12b1b1dc986e76b0dedaac7b2a19358d9ccc5514
James Biggs <james.biggs@nationalarchives.gov.uk>
2023-03-10 12:05:40 +0000

    Migration formatting

--- etna/generic_pages/migrations/0004_alter_generalpage_teaser_image.py
+++ etna/generic_pages/migrations/0004_alter_generalpage_teaser_image.py
@@ -7,14 +7,21 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('images', '0005_alter_customimage_file_and_more'),
-        ('generic_pages', '0003_add_teaser_text'),
+        ("images", "0005_alter_customimage_file_and_more"),
+        ("generic_pages", "0003_add_teaser_text"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='generalpage',
-            name='teaser_image',
-            field=models.ForeignKey(blank=True, help_text='Image that will appear on thumbnails and promos around the site.', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='images.customimage'),
+            model_name="generalpage",
+            name="teaser_image",
+            field=models.ForeignKey(
+                blank=True,
+                help_text="Image that will appear on thumbnails and promos around the site.",
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="+",
+                to="images.customimage",
+            ),
         ),
     ]
--- etna/home/migrations/0020_alter_homepage_teaser_image.py
+++ etna/home/migrations/0020_alter_homepage_teaser_image.py
@@ -7,14 +7,21 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('images', '0005_alter_customimage_file_and_more'),
-        ('home', '0019_rename_sub_heading_fields'),
+        ("images", "0005_alter_customimage_file_and_more"),
+        ("home", "0019_rename_sub_heading_fields"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='homepage',
-            name='teaser_image',
-            field=models.ForeignKey(blank=True, help_text='Image that will appear on thumbnails and promos around the site.', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='images.customimage'),
+            model_name="homepage",
+            name="teaser_image",
+            field=models.ForeignKey(
+                blank=True,
+                help_text="Image that will appear on thumbnails and promos around the site.",
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="+",
+                to="images.customimage",
+            ),
         ),
     ]
