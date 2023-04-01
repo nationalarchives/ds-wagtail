d05f1fc72317abb07af36f0f2777e1010fdc6ddf
Andy Babic <andy.babic@torchbox.com>
2023-04-01 10:36:38 +0100

    Add new feedback mechanism to the website footer

---
+++ etna/feedback/templatetags/feedback_tags.py
@@ -0,0 +1,54 @@
+from django import template
+from django.conf import settings
+from django.template.loader import get_template
+from django.utils.safestring import mark_safe
+
+from wagtail.models import Page
+
+from etna.feedback.forms import FeedbackForm
+from etna.feedback.models import FeedbackPrompt
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def render_feedback_prompt(context, template_name="feedback/includes/prompt.html"):
+    request = context["request"]
+
+    # Avoid rendering if not enabled
+    if not settings.FEATURE_FEEDBACK_MECHANISM_ENABLED:
+        return ""
+
+    # Avoid rendering on feedback views
+    if getattr(request.resolver_match, "namespace", "") == "feedback":
+        return ""
+
+    # Only continue if a valid prompt is available
+    try:
+        prompt = FeedbackPrompt.objects.get_for_path(request.path)
+    except FeedbackPrompt.DoesNotExist:
+        return ""
+
+    initial_data = {
+        "url": request.build_absolute_uri(),
+    }
+    page = context.get("page")
+    if isinstance(page, Page):
+        initial_data["page"] = page.id
+        initial_data["page_revision"] = page.live_revision_id
+
+    form = FeedbackForm(
+        response_options=prompt.response_options,
+        initial=initial_data,
+    )
+
+    template = get_template(template_name)
+    return mark_safe(
+        template.render(
+            {
+                "request": request,
+                "prompt": prompt,
+                "form": form,
+            }
+        )
+    )
