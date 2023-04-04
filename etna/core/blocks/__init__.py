4648f725baafd6fab9915f4b4c3be300ffa42eab
James Biggs <james.biggs@nationalarchives.gov.uk>
2023-04-04 10:26:59 +0100

    `isort` formatting

--- etna/core/blocks/__init__.py
+++ etna/core/blocks/__init__.py
@@ -2,7 +2,7 @@ from .base import SectionDepthAwareStructBlock
 from .image import ContentImageBlock, ImageBlock, NoCaptionImageBlock
 from .page_list import PageListBlock
 from .paragraph import ParagraphBlock, ParagraphWithHeading
-from .promoted_links import PromotedLinkBlock, AuthorPromotedLinkBlock
+from .promoted_links import AuthorPromotedLinkBlock, PromotedLinkBlock
 from .quote import QuoteBlock
 from .section import SectionBlock
 
