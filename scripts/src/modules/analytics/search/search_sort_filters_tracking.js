b917cd5a737c368b277a538bfa272d0ccd2d5f68
Jane <jane.hughes@torchbox.com>
2023-04-05 15:31:10 +0100

    push active 'sort by' filter values to GTM

---
+++ scripts/src/modules/analytics/search/search_sort_filters_tracking.js
@@ -0,0 +1,27 @@
+import push_to_data_layer from "./../push_to_data_layer";
+
+const getSortBy = () => {
+    // get filters after DOM has loaded and they have rendered on page
+    window.addEventListener('load', () => {
+        const sortBy = document.querySelector('[data-id="sort-form"]');
+        const searchType = sortBy.getAttribute('data-search-type');
+        const searchBucket = sortBy.getAttribute('data-search-bucket');
+        const searchName = sortBy.getAttribute('data-search-filter-name');
+        const searchValue = sortBy.getAttribute('data-search-filter-value');
+        
+        let filterData = {
+            'search_type': searchType || '',
+            'search_bucket': searchBucket || '',
+            'search_filter_name': searchName || '',
+            'search_filter_value': searchValue || '',
+        };
+
+        push_to_data_layer(filterData);
+    });
+}
+
+const searchSortFiltersTracking = () => {
+    getSortBy();
+};
+
+export default searchSortFiltersTracking;
