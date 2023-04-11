5033d55705e22b6bae22d210e63873e6c8eb9913
Andy Babic <andy.babic@torchbox.com>
2023-04-11 22:10:26 +0100

    Add filters to report view

--- etna/feedback/views.py
+++ etna/feedback/views.py
@@ -16,13 +16,19 @@ from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.http import urlencode
+from django.utils.translation import gettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import FormView, TemplateView
 
 from wagtail.admin.auth import permission_denied
+from wagtail.admin.filters import WagtailFilterSet
 from wagtail.admin.views.reports import ReportView
+from wagtail.admin.widgets import AdminDateInput
 from wagtail.models import Revision
 
+import django_filters
+
+from etna.feedback.constants import SentimentChoices
 from etna.feedback.forms import FeedbackForm
 from etna.feedback.models import FeedbackPrompt, FeedbackSubmission
 
@@ -144,11 +150,36 @@ class FeedbackSuccessView(VersionedFeedbackViewMixin, TemplateView):
         )
 
 
+class FeedbackSubmissionFilterSet(WagtailFilterSet):
+    received_from = django_filters.DateFilter(
+        field_name="received_at",
+        lookup_expr="date__gte",
+        label=_("Date received (from)"),
+        widget=AdminDateInput,
+    )
+    received_to = django_filters.DateFilter(
+        field_name="received_at",
+        lookup_expr="date__lte",
+        label=_("Date received (to)"),
+        widget=AdminDateInput,
+    )
+    response_sentiment = django_filters.ChoiceFilter(
+        choices=SentimentChoices.choices, label=_("Sentiment")
+    )
+
+    class Meta:
+        model = FeedbackSubmission
+        fields = {
+            "path": ["iexact", "istartswith"],
+        }
+
+
 class FeedbackSubmissionReportView(ReportView):
     title = "Feedback submissions"
     header_icon = "form"
     model = FeedbackSubmission
     is_searchable = False
+    filterset_class = FeedbackSubmissionFilterSet
     template_name = "feedback/reports/submission_report.html"
 
     list_display = [
