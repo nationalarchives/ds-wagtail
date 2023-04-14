72cc39e1d370d69b5422dded13b979ce7e8f0575
Andy Babic <andy.babic@torchbox.com>
2023-04-14 19:37:05 +0100

    DF-607: Fix JS error on search results page (#785)

--- scripts/src/catalogue-search.js
+++ scripts/src/catalogue-search.js
@@ -2,10 +2,10 @@ import mobileFilterExpander from './modules/search/mobile-filter-expander-enhanc
 import searchBucketsExpander from './modules/search/search-buckets-expander.js';
 import searchLongFilters from './modules/search/search-long-filters';
 import intialiseSearchResultTracking from './modules/analytics/search/search_result_interaction';
-import searchFiltersTracking from './modules/analytics/search/search_filters_tracking';
+import pushActiveFilterDataOnLoad from './modules/analytics/search/search_filters_tracking';
 
 mobileFilterExpander();
 searchBucketsExpander();
 searchLongFilters();
 intialiseSearchResultTracking();
-searchFiltersTracking();
+pushActiveFilterDataOnLoad();
