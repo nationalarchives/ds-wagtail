5310f9312f299577d34279a36790f473499c0ad0
Andy Babic <andy.babic@torchbox.com>
2023-04-28 16:00:11 +0100

    DF-630: Add `skos_id` fields for entities that map to official categories (#822)

--- etna/collections/tests/test_models.py
+++ etna/collections/tests/test_models.py
@@ -76,6 +76,78 @@ class TestTopicExplorerIndexPages(TestCase):
         )
 
 
+class TestTopicExplorerPage(TestCase):
+    def setUp(self):
+        root_page = Site.objects.get().root_page
+        self.index_page = TopicExplorerIndexPage(
+            title="Topics", intro="topics", teaser_text="topics"
+        )
+        root_page.add_child(instance=self.index_page)
+        self.topic_page = TopicExplorerPage(
+            title="Test Topic",
+            skos_id="Test_Topic",
+            slug="topic",
+            teaser_text="test",
+            intro="test",
+        )
+        self.index_page.add_child(instance=self.topic_page)
+
+    def test_clean_preserves_existing_skos_ids(self):
+        self.topic_page.clean()
+        self.assertEqual(self.topic_page.skos_id, "Test_Topic")
+
+        self.topic_page.skos_id = "new-SKOS-id"
+        self.topic_page.clean()
+        self.assertEqual(self.topic_page.skos_id, "new-SKOS-id")
+
+    def test_clean_does_not_generate_skos_id_when_no_title_specified(self):
+        self.topic_page.title = ""
+        self.topic_page.skos_id = ""
+        self.topic_page.clean()
+        self.assertEqual(self.topic_page.skos_id, "")
+
+    def test_clean_generates_skos_id_from_title_with_no_conflicts(self):
+        self.topic_page.skos_id = ""
+        self.topic_page.clean()
+        self.assertEqual(self.topic_page.skos_id, "Test_Topic")
+
+    def test_clean_generates_skos_id_from_title_with_conflicts(self):
+        self.index_page.add_child(
+            instance=TopicExplorerPage(
+                title="Test Topic 2",
+                skos_id="Test_Topic_2",
+                slug="topic-2",
+                teaser_text="test",
+                intro="test",
+            )
+        )
+        self.index_page.add_child(
+            instance=TopicExplorerPage(
+                title="Test Topic 3",
+                skos_id="Test_Topic_3",
+                slug="topic-3",
+                teaser_text="test",
+                intro="test",
+            )
+        )
+
+        obj = TopicExplorerPage(title="Test Topic")
+        obj.clean()
+        self.assertEqual(obj.skos_id, "Test_Topic_4")
+
+    def test_page_skos_id_preserved_when_revision_value_differs(self):
+        self.topic_page.skos_id = "SomeOtherValue"
+        revision = self.topic_page.save_revision()
+        self.topic_page.refresh_from_db()
+
+        topic_page_recreated = revision.as_object()
+
+        # Thanks to with_content_json() overrides, the recreated topic page
+        # should retain the `skos_id` value saved for `self.topic_page`
+        # initally, and ignore the new value from the revision
+        self.assertEqual(topic_page_recreated.skos_id, "Test_Topic")
+
+
 class TestTimePeriodExplorerIndexPages(TestCase):
     def setUp(self):
         root_page = Site.objects.get().root_page
