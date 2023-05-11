7e08aed4de1c12725619df19af106b848550f336
James Biggs <james.biggs@nationalarchives.gov.uk>
2023-05-11 09:08:17 +0000

    Updated media_tags to take in title

--- etna/media/templatetags/media_tags.py
+++ etna/media/templatetags/media_tags.py
@@ -4,13 +4,14 @@ register = template.Library()
 
 
 @register.inclusion_tag("media/blocks/media-block--audio.html")
-def audio(media):
+def audio(title, media):
     """
     Inclusion tag to render a custom audio media block.
 
     Exposes "src" and "type" attributes for FE convenience.
     """
     return {
+        "title": title,
         "value": media,
         "src": media.sources[0]["src"],
         "type": media.sources[0]["type"],
@@ -18,13 +19,14 @@ def audio(media):
 
 
 @register.inclusion_tag("media/blocks/media-block--video.html")
-def video(media):
+def video(title, media):
     """
     Inclusion tag to render a custom video media block.
 
     Exposes "src" and "type" attributes for FE convenience.
     """
     return {
+        "title": title,
         "value": media,
         "src": media.sources[0]["src"],
         "type": media.sources[0]["type"],
