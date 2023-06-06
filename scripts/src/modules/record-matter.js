508c22f135e0a13d5650170321593928ad054a01
Jane <jane.hughes@torchbox.com>
2023-06-06 16:40:42 +0100

    add tracking for main body content, including expand/collapse button and yellow buttons

--- scripts/src/modules/record-matter.js
+++ scripts/src/modules/record-matter.js
@@ -1,4 +1,4 @@
-
+import push_to_data_layer from "./analytics/push_to_data_layer";
 class RecordMatters {
     constructor(node) {
         this.node = node;
@@ -20,12 +20,26 @@ class RecordMatters {
             e.preventDefault();
             this.show(this.readMoreContent);
             this.hide(this.readLessContent);
+
+            push_to_data_layer({
+                "event": "Expand accordion",
+                "data-component-name": e.target.getAttribute("data-component-name"),
+                "data-link-type": e.target.getAttribute("data-link-type"),
+                "data-link": e.target.getAttribute("data-link")
+            })
         })
 
         this.readLessButton.addEventListener("click", (e) => {
             e.preventDefault();
             this.show(this.readLessContent);
             this.hide(this.readMoreContent);
+
+            push_to_data_layer({
+                "event": "Collapse accordion",
+                "data-component-name": e.target.getAttribute("data-component-name"),
+                "data-link-type": e.target.getAttribute("data-link-type"),
+                "data-link": e.target.getAttribute("data-link")
+            })
         })
     }
 
