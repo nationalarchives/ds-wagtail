43baa2149c0adb91dfff3568bf2616a8b9b1338d
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2023-06-08 17:43:48 +0100

    Articles migrations

--- etna/articles/migrations/0090_publication_date_charblock.py
+++ etna/articles/migrations/0090_publication_date_charblock.py
@@ -9,12 +9,17 @@ def migrate_forwards(apps, schema_editor):
         for block in page.promoted_links:
             if block.value["promoted_items"]:
                 for item in block.value["promoted_items"]:
-                    if item["publication_date"]:
-                        date_obj = datetime.strptime(
-                            item["publication_date"], "%Y-%m-%d"
-                        ).date()
-                        formatted_date = date_obj.strftime("%d %B %Y")
-                        item["publication_date"] = formatted_date
+                    item["publication_date"] = datetime.strptime(item["publication_date"], "%Y-%m-%d").date().strftime("%d %B %Y")
+        page.save()
+
+    ArticlePage = apps.get_model("articles", "ArticlePage")
+
+    for page in ArticlePage.objects.all():
+        for section in page.body:
+            if section.block_type == "content_section":
+                for block in section.value["content"]:
+                    if block.block_type == "promoted_item" and block.value["publication_date"]:
+                        block.value["publication_date"] = datetime.strptime(str(block.value["publication_date"]), "%Y-%m-%d").date().strftime("%d %B %Y")
         page.save()
 
 
@@ -25,12 +30,22 @@ def migrate_backwards(apps, schema_editor):
         for block in page.promoted_links:
             if block.value["promoted_items"]:
                 for item in block.value["promoted_items"]:
-                    if isinstance(item["publication_date"], datetime):
-                        continue
-                    date_obj = datetime.strptime(item["publication_date"], "%d %B %Y")
-                    formatted_date = date_obj.strftime("%Y-%m-%d")
-                    print(formatted_date)
-                    item["publication_date"] = formatted_date
+                    item["publication_date"] = datetime.strptime(item["publication_date"], "%d %B %Y").strftime("%Y-%m-%d")
+        page.save()
+
+    # ArticlePage = apps.get_model("articles", "ArticlePage")
+
+    # for page in ArticlePage.objects.all():
+    #     for section in page.body:
+    #         if section.block_type == "content_section":
+    #             for block in section.value["content"]:
+    #                 if block.block_type == "promoted_item":
+    #                     for item in block.value:
+    #                         if item == "publication_date":
+    #                             # item["publication_date"] = datetime.strptime(item["publication_date"], "%d %B %Y").strftime("%Y-%m-%d")
+    #                             print("BEFORE FORMAT:", item.value)
+                        # block.value["publication_date"] = datetime.strptime(block.value["publication_date"], "%d %B %Y").strftime("%Y-%m-%d")
+                        # print("AFTER FORMAT:",block.value["publication_date"])
         page.save()
 
 
