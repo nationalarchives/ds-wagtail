43d4fdc66094ceacd7f842b6e472d4ccb21b0c55
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-07-27 13:03:01 +0100

    Add logic for adding tags to search and catalogue pages

---
+++ etna/core/templatetags/robots_meta.py
@@ -0,0 +1,10 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter()
+def is_search_or_catalogue(path) -> bool:
+    """Return True if path is contained in search or catalogue."""
+    return path.startswith('/search/') or path.startswith('/catalogue/')
