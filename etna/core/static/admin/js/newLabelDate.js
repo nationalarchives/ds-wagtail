eebe7f2e41ec455309cc9ace8935f9cc68413f6b
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2023-08-02 17:39:41 +0100

    Updated logic slightly and added "expired on"

--- etna/core/static/admin/js/newLabelDate.js
+++ etna/core/static/admin/js/newLabelDate.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     var label = document.getElementById("panel-child-promote-child-new_label-child-newly_published_at-label");
-    label.innerHTML = "New label current expiry date:";
+    if (label) {
+        label.innerHTML = "New label current expiry date:";
+    }
     var element = document.querySelector('[aria-labelledby="panel-child-promote-child-new_label-child-newly_published_at-label"]');
-    if (element.innerHTML.replace(/\s/g, '') != "None") {
-        var newDate = new Date(element.innerHTML);
-        newDate.setDate(newDate.getDate() + 21);
-        element.innerHTML = newDate.toLocaleDateString('en-GB');
-    } else {
-        element.innerHTML = "N/A";
+    if (element) {
+        if (element.innerHTML.replace(/\s/g, '') != "None") {
+            var newDate = new Date(element.innerHTML);
+            newDate.setDate(newDate.getDate() + 21);
+            if (newDate >= new Date().setHours(0, 0, 0, 0)) {
+                element.innerHTML = newDate.toLocaleDateString('en-GB');
+            } else {
+                element.innerHTML = "Expired on " + newDate.toLocaleDateString('en-GB');
+            }
+        } else {
+            element.innerHTML = "N/A";
+        }
     }
 });
