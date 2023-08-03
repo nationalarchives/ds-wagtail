7fb2e0f5dfeb60bc04004ce2bfba57a9bb50aa6a
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2023-08-03 11:48:11 +0100

    Added test for FA page

--- etna/articles/tests/test_pages.py
+++ etna/articles/tests/test_pages.py
@@ -1,7 +1,8 @@
 from wagtail.models import Site
 from wagtail.test.utils import WagtailPageTestCase
+from django.test import TestCase
 
-from ..models import ArticleIndexPage, ArticlePage
+from ..models import ArticleIndexPage, ArticlePage, FocusedArticlePage
 
 
 class TestPages(WagtailPageTestCase):
@@ -23,8 +24,18 @@ class TestPages(WagtailPageTestCase):
         )
         self.article_index_page.add_child(instance=self.article_page)
 
+        self.focused_article_page = FocusedArticlePage(
+            title="Focused Article page",
+            intro="test",
+            teaser_text="test",
+        )
+        self.article_index_page.add_child(instance=self.focused_article_page)
+
     def test_article_index_page(self):
         self.assertPageIsRenderable(self.article_index_page)
 
     def test_article_page(self):
         self.assertPageIsRenderable(self.article_page)
+
+    def test_focused_article_page(self):
+        self.assertPageIsRenderable(self.focused_article_page)
