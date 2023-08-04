1b0e357cc5aaed4e9cda6ed58313ee8c926f6ff8
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2023-08-04 11:48:30 +0100

    Added AuthorTag, PageMixin, other necessary bits and bobs

--- etna/authors/models.py
+++ etna/authors/models.py
@@ -2,10 +2,15 @@ from django.conf import settings
 from django.db import models
 from django.utils.functional import cached_property
 
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import (
+    FieldPanel,
+    InlinePanel,
+)
 from wagtail.fields import RichTextField
 from wagtail.images import get_image_model_string
 from etna.core.models import BasePage
+from wagtail.models import Page
+from modelcluster.fields import ParentalKey
 
 
 class AuthorIndexPage(BasePage):
@@ -70,3 +75,90 @@ class AuthorPage(BasePage):
         verbose_name_plural = "Authors"
 
     parent_page_types = ["authors.AuthorIndexPage"]
+
+    @cached_property
+    def authored_focused_articles(self):
+        from etna.articles.models import FocusedArticlePage
+
+        return (
+            FocusedArticlePage.objects.live()
+            .public()
+            .filter(pk__in=self.related_page_pks)
+            .order_by("-first_published_at")
+            .select_related("teaser_image")[:4]
+        )
+    
+    @cached_property
+    def related_page_pks(self):
+        """
+        Returns a list of ids of pages that have used the `PageTimePeriod` inline
+        to indicate a relationship with this time period. The values are ordered by:
+        - The order in which this time period was specified (more important time periods are specified first)
+        - When the page was first published ('more recently added' pages take presendence)
+        """
+        return tuple(
+            self.author_pages.values_list("page_id", flat=True).order_by(
+                "-page__first_published_at"
+            )
+        )
+
+
+class AuthorTag(models.Model):
+    """
+    This model allows any page type to be associated with one or more topics
+    in a way that retains the order of topics selected.
+
+    The ``sort_order`` field value from ``Orderable`` can be used to pull out
+    the 'first' topic to treat as the 'primary topic' for a page, and can also
+    used to prioritise items for a list of 'pages related to a topic'.
+
+    Just add `InlinePanel("page_topics")` to a page type's panel
+    configuration to use it!
+    """
+
+    page = ParentalKey(Page, on_delete=models.CASCADE, related_name="author_tags")
+    author = models.ForeignKey(
+        AuthorPage,
+        verbose_name="author",
+        related_name="author_pages",
+        on_delete=models.CASCADE,
+    )
+
+
+class AuthorPageMixin:
+    """
+    A mixin for pages that use the ``PageTopic`` and ``PageTimePeriod`` models
+    in order to be associated with one or many topics/time periods. It simply
+    adds a few properies to support robust, efficient access the related topic
+    and time period pages.
+    """
+
+    @classmethod
+    def get_authors_inlinepanel(cls, max_num = 1):
+        return InlinePanel(
+            "author_tags",
+            heading="Page author",
+            help_text="If the page relates to more than one topic, please add these in order of relevance from most to least.",
+            max_num=max_num,
+        )
+
+    @cached_property
+    def authors(self):
+        return tuple(
+            item.author
+            for item in self.author_tags.select_related("author").filter(
+                author__live=True
+            )
+        )
+    
+    @property
+    def author_name(self):
+        """
+        Returns the titles of all related topics, joined together into one big
+        comma-separated string. Ideal for indexing!
+        """
+        return "".join(item.title for item in self.authors)
+
+    @cached_property
+    def authors_alphabetical(self):
+        return sorted(self.author, key=lambda item: item.title.lower())
