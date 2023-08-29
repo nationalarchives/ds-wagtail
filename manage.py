0d0cfa94395ef869850a637210dd8ace36a4487f
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-08-29 15:52:38 +0100

    TNA Python Docker image (#1244)
    Co-authored-by: JohnHeeryTNA <110600556+JohnHeeryTNA@users.noreply.github.com>
    Co-authored-by: James Biggs <62654785+jamesbiggs@users.noreply.github.com>

--- manage.py
+++ manage.py
@@ -2,11 +2,16 @@
 import os
 import sys
 
+# THIS FILE IS NOT USED INSIDE DOCKER CONTAINERS - THIS IS USED INSTEAD:
+# https://github.com/nationalarchives/docker/blob/main/docker/tna-python-django/lib/manage.py
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
     try:
         if sys.argv[1] == "test":
+            # TEST CONFIG IS USED IN CI/CD
             os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.test"
     except IndexError:
         # No arguments passed to manage.py
