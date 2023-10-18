96745e4671d1b089c893f49573c94a61cd93ec17
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2023-10-18 14:54:08 +0100

    Un-used imports

--- etna/whatson/models.py
+++ etna/whatson/models.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils import timezone
 from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 
