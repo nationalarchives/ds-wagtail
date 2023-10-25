47b08cce3b120bf75915ea6c33d8006d43adcc6c
Helen C <helen.chapman@torchbox.com>
2023-10-25 09:57:21 +0100

    Manipulate urls better with URL() and add history management

--- scripts/src/modules/filters-submission.js
+++ scripts/src/modules/filters-submission.js
@@ -2,17 +2,20 @@ class FiltersSubmission {
     constructor(node) {
         this.node = node;
         this.dateField = this.node.querySelector("[data-js-date]");
-        this.dateValue = this.dateField.value;
+
         this.eventTypeFields = this.node.querySelectorAll(
             "[data-js-event-type]",
         );
         const eventTypeChecked = this.node.querySelector(
             "[data-js-event-type]:checked",
         );
-        this.eventTypeValue = eventTypeChecked ? eventTypeChecked.value : null;
         this.onlineField = this.node.querySelector("[data-js-online]");
-        this.onlineValue = this.onlineField.checked;
         this.familyField = this.node.querySelector("[data-js-family]");
+        // When the page loads normally, the initial values are based on
+        // the current field values
+        this.dateValue = this.dateField.value;
+        this.eventTypeValue = eventTypeChecked ? eventTypeChecked.value : null;
+        this.onlineValue = this.onlineField.checked;
         this.familyValue = this.familyField.checked;
         this.addEventListeners();
     }
@@ -39,36 +42,50 @@ class FiltersSubmission {
             this.familyValue = e.target.checked;
             this.reloadEvents();
         });
+
+        window.addEventListener("popstate", () => {
+            this.updateFields(document.location);
+            this.reloadEvents(false);
+        });
     }
 
     buildURL() {
-        let url = window.location.href.split("?")[0];
-        let first = true;
+        let url = new URL(window.location.href.split("?")[0]);
+
         if (this.dateValue) {
-            url += first ? `?` : `&`;
-            url += `date=${this.dateValue}`;
-            first = false;
+            url.searchParams.append("date", this.dateValue);
         }
         if (this.eventTypeValue) {
-            url += first ? `?` : `&`;
-            url += `event_type=${this.eventTypeValue}`;
-            first = false;
+            url.searchParams.append("event_type", this.eventTypeValue);
         }
         if (this.onlineValue) {
-            url += first ? `?` : `&`;
-            url += `is_online_event=on`;
-            first = false;
+            url.searchParams.append("is_online_event", "on");
         }
         if (this.familyValue) {
-            url += first ? `?` : `&`;
-            url += `family_friendly=on`;
-            first = false;
+            url.searchParams.append("family_friendly", "on");
         }
-        return url;
+        return url.toString();
     }
 
-    async reloadEvents() {
-        fetch(this.buildURL(), {
+    // when the back or forward buttons is used for a JavaScript change,
+    // we need to update the field values based on the URL
+    updateFields(url) {
+        let urlParams = new URLSearchParams(url.search);
+        this.dateValue = urlParams.get("date");
+        this.dateField.value = this.dateValue;
+        this.eventValue = urlParams.get("event_type");
+        this.eventTypeFields.forEach((field) => {
+            field.checked = field.value === this.eventValue;
+        });
+        this.onlineValue = urlParams.get("is_online_event");
+        this.onlineField.checked = this.onlineValue === "on";
+        this.familyValue = urlParams.get("family_friendly");
+        this.familyField.checked = this.familyValue === "on";
+    }
+
+    async reloadEvents(updateURL = true) {
+        let newURL = this.buildURL();
+        fetch(newURL, {
             method: "GET",
             headers: {
                 "JS-Request": true,
@@ -78,6 +95,9 @@ class FiltersSubmission {
             .then((value) => {
                 document.querySelector("[data-js-whatson-listing]").innerHTML =
                     value;
+                if (updateURL) {
+                    history.pushState({}, "", newURL);
+                }
             });
     }
 }
