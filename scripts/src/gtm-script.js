86a4a3e3d66c23eb994b53addfb4fb4434ecd92c
Gareth Mead <40268781+garethmead@users.noreply.github.com>
2023-11-22 14:36:04 +0000

    HERITAGE-79 Update tracking to use correct code (#18)

--- scripts/src/gtm-script.js
+++ scripts/src/gtm-script.js
@@ -10,4 +10,4 @@
     j.async = true;
     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
     f.parentNode.insertBefore(j, f);
-})(window, document, "script", "dataLayer", "GTM-T8DSWV");
+})(window, document, "script", "dataLayer", "GTM-NLWCSTF7");
