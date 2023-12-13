9cf92a5679a1720a1c6a44831e93bbe968d9dd95
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-12-13 16:16:37 +0000

    Add media endpoint

--- etna/api/urls.py
+++ etna/api/urls.py
@@ -5,6 +5,7 @@ from wagtail.api.v2.views import PagesAPIViewSet
 
 # from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from wagtail.images.api.v2.views import ImagesAPIViewSet
+from wagtailmedia.api.views import MediaAPIViewSet
 
 from rest_framework.response import Response
 from wagtail_headless_preview.models import PagePreview
@@ -67,5 +68,6 @@ api_router = WagtailAPIRouter("wagtailapi")
 
 api_router.register_endpoint("pages", PagesAPIViewSet)
 api_router.register_endpoint("images", CustomImagesAPIViewSet)
+api_router.register_endpoint("media", MediaAPIViewSet)
 # api_router.register_endpoint("documents", DocumentsAPIViewSet)
 api_router.register_endpoint("page_preview", PagePreviewAPIViewSet)
