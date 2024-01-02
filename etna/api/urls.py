20ab25bbd356d25ffe3ffbd74dd44da5089cdb5d
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-01-02 16:18:17 +0000

    Flake8 issues

--- etna/api/urls.py
+++ etna/api/urls.py
@@ -1,9 +1,9 @@
 from django.contrib.contenttypes.models import ContentType
 
-from wagtail.api import APIField
+# from wagtail.api import APIField
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.api.v2.views import PagesAPIViewSet
-from wagtail.images.api.fields import ImageRenditionField
+# from wagtail.images.api.fields import ImageRenditionField
 
 # from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from wagtail.images.api.v2.views import ImagesAPIViewSet
