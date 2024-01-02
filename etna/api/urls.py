e373ee3dc04d19ed7b187396db1d0398b357de6f
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-01-02 16:22:48 +0000

    Fix isort issue

--- etna/api/urls.py
+++ etna/api/urls.py
@@ -3,7 +3,6 @@ from django.contrib.contenttypes.models import ContentType
 # from wagtail.api import APIField
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.api.v2.views import PagesAPIViewSet
-# from wagtail.images.api.fields import ImageRenditionField
 
 # from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from wagtail.images.api.v2.views import ImagesAPIViewSet
@@ -11,6 +10,10 @@ from wagtail.images.api.v2.views import ImagesAPIViewSet
 from rest_framework.response import Response
 from wagtail_headless_preview.models import PagePreview
 from wagtailmedia.api.views import MediaAPIViewSet
+
+# from wagtail.images.api.fields import ImageRenditionField
+
+
 
 
 class PagePreviewAPIViewSet(PagesAPIViewSet):
