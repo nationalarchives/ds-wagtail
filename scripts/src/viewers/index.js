674f991f96511c4d6ec3c51cc50816416e14402a
Jane <jane.hughes@torchbox.com>
2024-01-04 10:21:25 +0000

    linting and tidy up

--- scripts/src/viewers/index.js
+++ scripts/src/viewers/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Clover from "./Clover.js";
-import UniversalViewer from "./universal-viewer.js";
 
 const root = createRoot(document.getElementById("clover-viewer"));
 root.render(<Clover />);
-
-document.addEventListener("DOMContentLoaded", () => {
-    for (const uv of document.querySelectorAll("[data-uv]")) {
-        new UniversalViewer(uv);
-    }
-});
