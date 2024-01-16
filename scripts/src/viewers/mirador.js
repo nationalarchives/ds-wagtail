9f7741d5e32dbe4baa631b7c7c1830a46c506da8
Joshua Munn <public@elysee-munn.family>
2024-01-16 16:24:06 +0000

    Add Mirador to image viewers comparison page

--- scripts/src/viewers/mirador.js
+++ scripts/src/viewers/mirador.js
@@ -1,18 +1,11 @@
-import Mirador from "mirador/dist/es/src/index";
-import { miradorImageToolsPlugin } from "mirador-image-tools";
+import Mirador from "./mirador.min.js";
 
 const viewer = document.getElementById("mirador-viewer");
-const manifestID = viewer.getAttribute("data-id");
+const manifestId = viewer.getAttribute("data-id");
 
-const config = {
-    id: "mirador-viewer",
-    windows: [
-        {
-            imageToolsEnabled: true,
-            imageToolsOpen: true,
-            manifestId: manifestID,
-        },
-    ],
+const miradorInstance = Mirador.viewer({
+    // see https://github.com/ProjectMirador/mirador/blob/5cb692ed31480c1e130f4a8715726688cb35796d/src/config/settings.js#L264 for config options
+    id: 'mirador-viewer', // id selector where Mirador should be instantiated
     theme: {
         palette: {
             primary: {
@@ -20,6 +13,30 @@ const config = {
             },
         },
     },
-};
-
-Mirador.viewer(config, [...miradorImageToolsPlugin]);
+    windows: [{
+        manifestId,
+        view: 'single',
+    }],
+    window: {
+        allowClose: false, // Prevent the user from closing this window
+        allowMaximize: false,
+        allowFullscreen: true,
+        defaultSideBarPanel: 'info',
+        sideBarOpenByDefault: false,
+        highlightAllAnnotations: true,
+        views: [ // Only allow the user to select single and gallery view
+            { key: 'single' },
+            { key: 'gallery' },
+        ]
+    },
+    thumbnailNavigation: {
+        defaultPosition: 'far-bottom',
+    },
+    workspace: {
+        type: 'mosaic',
+        showZoomControls: true,
+    },
+    workspaceControlPanel: {
+        enabled: false, // Remove extra workspace settings
+    },
+});
