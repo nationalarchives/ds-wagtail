cc382ff8a60d8f5254abe83ff33c2649636876fc
Tomasz Knapik <tomasz.knapik@torchbox.com>
2024-03-05 15:45:27 +0000

    Remove old import

--- etna/records/views/__init__.py
+++ etna/records/views/__init__.py
@@ -4,8 +4,4 @@ from .choosers import (
     RecordChooserViewSet,
 )
 from .images import image_browse, image_serve, image_viewer
-from .records import (
-    record_detail_view,
-    record_disambiguation_view,
-    record_iiif_manifest_view,
-)
+from .records import record_detail_view, record_disambiguation_view
