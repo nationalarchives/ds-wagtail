3de0ae73c04848b4d909cf503f1b9815a674888c
Tomasz Knapik <tomasz.knapik@torchbox.com>
2024-03-15 11:18:39 +0000

    strtobool to return bool, not an integer

--- config/settings/util.py
+++ config/settings/util.py
@@ -1,4 +1,4 @@
-def strtobool(val):
+def strtobool(val) -> bool:
     """Convert a string representation of truth to true (1) or false (0).
     True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
     are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
@@ -6,8 +6,8 @@ def strtobool(val):
     """
     val = val.lower()
     if val in ("y", "yes", "t", "true", "on", "1"):
-        return 1
+        return True
     elif val in ("n", "no", "f", "false", "off", "0"):
-        return 0
+        return False
     else:
         raise ValueError("invalid truth value %r" % (val,))
