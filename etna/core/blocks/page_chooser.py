d8c22592075173fa88af418536ccaf362f891e7c
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2024-04-03 12:27:28 +0100

    Refactored code

--- etna/core/blocks/page_chooser.py
+++ etna/core/blocks/page_chooser.py
@@ -35,60 +35,60 @@ class APIPageChooserBlock(blocks.PageChooserBlock):
         super().__init__(required=required, help_text=help_text, **kwargs)
 
     def get_api_representation(self, value, context=None):
-        if value:
-            specific = value.specific
+        if not value:
+            return None
 
+        specific = value.specific
+        attributes = {
+            "teaser_image": (None, None),
+            "type_label": None,
+            "is_newly_published": None,
+        }
+
+        for attr, default in attributes.items():
+            attributes[attr] = getattr(specific, attr, default)
+
+        if attributes["teaser_image"]:
             try:
-                jpeg_image = specific.teaser_image.get_rendition(
+                jpeg_image = attributes["teaser_image"].get_rendition(
                     f"{self.rendition_size}|format-jpeg|jpegquality-{self.jpeg_quality}"
                 )
-                webp_image = specific.teaser_image.get_rendition(
+                webp_image = attributes["teaser_image"].get_rendition(
                     f"{self.rendition_size}|format-webp|webpquality-{self.webp_quality}"
                 )
             except AttributeError:
-                jpeg_image = None
-                webp_image = None
+                jpeg_image = webp_image = None
+            attributes["teaser_image"] = (jpeg_image, webp_image)
 
-            try:
-                type_label = specific.type_label()
-            except AttributeError:
-                type_label = None
-
-            try:
-                is_newly_published = specific.is_newly_published
-            except AttributeError:
-                is_newly_published = None
-
-            return {
-                "id": specific.id,
-                "title": specific.title,
-                "teaser_image_jpeg": (
-                    {
-                        "url": jpeg_image.url,
-                        "full_url": jpeg_image.full_url,
-                        "width": jpeg_image.width,
-                        "height": jpeg_image.height,
-                    }
-                    if jpeg_image
-                    else None
-                ),
-                "teaser_image_webp": (
-                    {
-                        "url": webp_image.url,
-                        "full_url": webp_image.full_url,
-                        "width": webp_image.width,
-                        "height": webp_image.height,
-                    }
-                    if webp_image
-                    else None
-                ),
-                "type_label": type_label,
-                **(
-                    {"is_newly_published": is_newly_published}
-                    if is_newly_published is not None
-                    else {}
-                ),
-                "url": specific.url,
-                "full_url": specific.full_url,
-            }
-        return None
+        return {
+            "id": specific.id,
+            "title": specific.title,
+            "teaser_image_jpeg": (
+                {
+                    "url": jpeg_image.url,
+                    "full_url": jpeg_image.full_url,
+                    "width": jpeg_image.width,
+                    "height": jpeg_image.height,
+                }
+                if jpeg_image
+                else None
+            ),
+            "teaser_image_webp": (
+                {
+                    "url": webp_image.url,
+                    "full_url": webp_image.full_url,
+                    "width": webp_image.width,
+                    "height": webp_image.height,
+                }
+                if webp_image
+                else None
+            ),
+            "type_label": attributes["type_label"](),
+            **(
+                {"is_newly_published": attributes["is_newly_published"]}
+                if attributes["is_newly_published"] is not None
+                else {}
+            ),
+            "url": specific.url,
+            "full_url": specific.full_url,
+        }
