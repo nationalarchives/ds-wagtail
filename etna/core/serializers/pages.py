425acae31a4a4f67f40adc43638376b62ee57bb0
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2024-04-11 08:57:17 +0100

    Move functions and add init override

--- etna/core/serializers/pages.py
+++ etna/core/serializers/pages.py
@@ -3,6 +3,39 @@ from wagtail.images.api.fields import ImageRenditionField
 from rest_framework import serializers
 
 
+def get_api_fields(object, required_api_fields:list=[]) -> list:
+    """
+    Get the selected fields (required_api_fields) from the object's api_fields
+    attribute, and return them as a dictionary with the field name as the key,
+    and the serializer as the value.
+    """
+    fields = []
+    if api_fields := object.api_fields:
+        for field in required_api_fields:
+            for api_field in api_fields:
+                if field == api_field.name:
+                    fields.append(api_field)
+                    break
+    return fields
+
+def get_api_data(object, required_api_fields:list=[]) -> dict:
+    if object:
+        api_representation = {}
+        specific = object.specific
+        if api_fields := get_api_fields(object=specific, required_api_fields=required_api_fields):
+            for field in api_fields:
+                field_data = getattr(specific, field.name, None)
+                if serializer := field.serializer:
+                    if source := serializer.source:
+                        field_data = serializer.to_representation(getattr(specific, source, None))
+                    else:
+                        field_data = serializer.to_representation(field_data)
+                if callable(field_data):
+                    field_data = field_data()
+                api_representation[field.name] = field_data
+    return api_representation
+
+
 class LinkedPageSerializer(serializers.ModelSerializer):
     """
     This Serializer should be inherited as a base class for all "linked" or "secondary"
