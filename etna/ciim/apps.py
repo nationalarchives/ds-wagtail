1bc1a31dc4e38736df96015d1ff2c9f244f665d3
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2024-05-16 13:56:27 +0100

    CHORE:Add comment with expanded CIIM acronym

--- etna/ciim/apps.py
+++ etna/ciim/apps.py
@@ -2,6 +2,9 @@ from django.apps import AppConfig
 
 
 class CIIMAppConfig(AppConfig):
+    """
+    CIIM - Collections Information Integration Middleware
+    """
     default_auto_field = "django.db.models.AutoField"
     name = "etna.ciim"
     verbose_name = "CIIM"
