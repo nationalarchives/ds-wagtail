9a0d8b7744d9d109d659ea490a138ca9f609f05c
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-06-05 11:29:14 +0100

    Created DocumentBlock to add link text and a related file

---
+++ etna/core/blocks/document.py
@@ -0,0 +1,17 @@
+from wagtail import blocks
+from wagtail.documents.blocks import DocumentChooserBlock
+
+class DocumentBlock(blocks.StructBlock):
+    """
+    A block for embedding a document file in a page.
+    """
+
+    link_text = blocks.CharBlock(
+        required=True,
+        help_text="The text to display for the link to the document",
+    )
+    file = DocumentChooserBlock(required=True)
+
+    class Meta:
+        icon = "doc-full"
+        label = "Document"
