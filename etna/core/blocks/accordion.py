d8138488136e0ab3b217cc546653cdfbda01cfb4
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-06-06 12:18:54 +0100

    Used APIRichTextBlock and restricted features

--- etna/core/blocks/accordion.py
+++ etna/core/blocks/accordion.py
@@ -1,9 +1,15 @@
+from django.conf import settings
+
 from wagtail import blocks
+
+from .paragraph import APIRichTextBlock
 
 
 class AccordionBlock(blocks.StructBlock):
     title = blocks.CharBlock(required=True)
-    content = blocks.RichTextBlock(required=True)
+    content = APIRichTextBlock(
+        required=True, features=settings.RESTRICTED_RICH_TEXT_FEATURES
+    )
 
     class Meta:
         icon = "list-ul"
