103624b60b2a5f5b84cf2c8b0decb95c54ec8a84
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-06-06 11:52:13 +0100

    Created AccordionsBlock to add multiple AccordionBlocks

---
+++ etna/core/blocks/accordion.py
@@ -0,0 +1,13 @@
+from wagtail import blocks
+
+
+class AccordionBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True)
+    content = blocks.RichTextBlock(required=True)
+
+    class Meta:
+        icon = 'list-ul'
+        label = 'Accordion'
+
+class AccordionsBlock(blocks.StructBlock):
+    accordion = blocks.ListBlock(AccordionBlock())
