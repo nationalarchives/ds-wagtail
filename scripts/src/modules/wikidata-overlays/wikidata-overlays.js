58b0fe804d577819f047134b32f7b56457a41a0b
Gareth Mead <40268781+garethmead@users.noreply.github.com>
2024-06-11 12:25:11 +0100

    HERITAGE-305 - Wikidata overlay fixes (#97)
    * HERITAGE-304 Initial investigation around fetching data for wikidata overlays
    
    * HERITAGE-304 Add styles to wikidata overlays proof of concept
    
    * HERITAGE-305 Work to move wikidata query to SPARQL and improve functionality around overlay
    
    * HERITAGE-305 Add fallback colour to tag variable
    
    * HERITAGE-305 Change CSS variable to SCSS variable
    
    * HERITAGE-305 Add defense mechanism around API response data
    
    * HERITAGE-305 Fix bug introduced converting CSS var to SCSS
    
    * Format JS
    
    * HERITAGE-305 Change position of wikidata overlays across breakpoints
    
    * HERITAGE-305 Fix focus outline visibility of wikidata link
    
    * HERITAGE-305 Add wikidata logo
    
    * HERITAGE-305 Wikidata overlay typography improvements
    
    * HERITAGE-305 Alter SPARQL query to handle data from entities that redirect
    
    * Format JS

--- scripts/src/modules/wikidata-overlays/wikidata-overlays.js
+++ scripts/src/modules/wikidata-overlays/wikidata-overlays.js
@@ -28,7 +28,9 @@ const createTemplate = (data, tagType) => {
     const countryTemplate = data.countryLabel
         ? `<p class="wikidata__header-meta">${data.countryLabel}</p>`
         : "";
-    const titleTemplate = data.label ? `<h3>${data.label}</h3>` : "";
+    const titleTemplate = data.label
+        ? `<h3 class="wikidata__label">${data.label}</h3>`
+        : "";
     const descriptionTemplate = data.description
         ? `<p>${data.description}</p>`
         : "";
@@ -107,25 +109,38 @@ const uniqueWikidataIds = [...new Set(wikidataIds)];
  * @returns {String}
  */
 const buildSparqlQuery = (ids) => `
-  # Properties that will be returned by the API
-  SELECT ?item ?label ?description ?image ?countryLabel
-  WHERE {
-    VALUES ?item { ${ids.map((id) => `wd:${id}`).join(" ")} }
+# Properties that will be returned by the API
+SELECT ?item ?finalItem ?label ?description ?image ?countryLabel
+WHERE {
+  VALUES ?item { ${ids.map((id) => `wd:${id}`).join(" ")} }
 
-    # Specific fields for each item
-    ?item rdfs:label ?label .
-    ?item schema:description ?description .
-    FILTER(LANG(?label) = "en")
-    FILTER(LANG(?description) = "en")
-
-    OPTIONAL { ?item wdt:P18 ?image . }
-
-    OPTIONAL {
-      ?item wdt:P17 ?country .
-      ?country rdfs:label ?countryLabel .
-      FILTER(LANG(?countryLabel) = "en")
-    }
+  # Handle redirects
+  OPTIONAL {
+    ?item owl:sameAs ?redirectedItem .
   }
+
+  # Use the redirected item if it exists, otherwise use the original item
+  BIND(COALESCE(?redirectedItem, ?item) AS ?finalItem)
+
+  # Specific fields for each item
+  OPTIONAL {
+    ?finalItem rdfs:label ?label .
+    FILTER(LANG(?label) = "en")
+  }
+
+  OPTIONAL {
+    ?finalItem schema:description ?description .
+    FILTER(LANG(?description) = "en")
+  }
+
+  OPTIONAL { ?finalItem wdt:P18 ?image . }
+
+  OPTIONAL {
+    ?finalItem wdt:P17 ?country .
+    ?country rdfs:label ?countryLabel .
+    FILTER(LANG(?countryLabel) = "en")
+  }
+}
 `;
 
 const executeSparqlQuery = async (query) => {
