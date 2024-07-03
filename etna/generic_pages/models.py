4688c5568421153968ad424c2fcd42b9d3163845
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-07-03 10:38:38 +0100

    Created LinkItem and added to HubPage to create internal or external links

--- etna/generic_pages/models.py
+++ etna/generic_pages/models.py
@@ -1,5 +1,16 @@
-from wagtail.admin.panels import FieldPanel
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from modelcluster.fields import ParentalKey
+
+from wagtail.api import APIField
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.fields import StreamField
+from wagtail.images import get_image_model_string
+from wagtail.models import Orderable
+
+from etna.core.serializers.pages import DefaultPageSerializer
+
 
 from etna.core.models import BasePage
 
@@ -13,11 +24,71 @@ class GeneralPage(BasePage):
         FieldPanel("body"),
     ]
 
-    api_fields = BasePage.api_fields + ["body"]
+    api_fields = BasePage.api_fields + [APIField("body")]
+
+
+class LinkItem(Orderable):
+    page = ParentalKey("HubPage", related_name="links")
+
+    title = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ForeignKey(
+        get_image_model_string(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    description = models.CharField(max_length=255, null=True, blank=True)
+    url = models.URLField(verbose_name="URL", blank=True, null=True)
+
+    internal_page = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [
+        FieldPanel("internal_page"),
+        MultiFieldPanel(
+            [
+                FieldPanel("title"),
+                FieldPanel("image"),
+                FieldPanel("description"),
+                FieldPanel("url"),
+            ],
+            heading="External page details",
+        ),
+    ]
+
+    def clean(self) -> None:
+        if self.internal_page and (self.url or self.title or self.image or self.description):
+            raise ValidationError(
+                {
+                    "internal_page": "You can only select an internal page or provide external page details, not both."
+                }
+            )
+        elif not self.internal_page and not (self.url and self.title and self.image and self.description):
+            raise ValidationError(
+                {
+                    "internal_page": "You must select an internal page or provide external page details."
+                }
+            )
+        return super().clean()
+
+    api_fields = [
+        APIField("title"),
+        APIField("image"),
+        APIField("description"),
+        APIField("url"),
+        APIField("internal_page", serializer=DefaultPageSerializer()),
+    ]
 
 
 class HubPage(BasePage):
+    content_panels = BasePage.content_panels + [
+        InlinePanel("links", label="Link"),
+    ]
 
-    content_panels = BasePage.content_panels
-
-    api_fields = BasePage.api_fields
+    api_fields = BasePage.api_fields + [APIField("links")]
