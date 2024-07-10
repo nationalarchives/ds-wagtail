e7c2d0b9912a5e4dcbe1cb7c318bd1ada2591032
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-07-10 14:07:57 +0100

    Further formatting

--- etna/core/models/documents.py
+++ etna/core/models/documents.py
@@ -17,7 +17,7 @@ class CustomDocument(AbstractDocument):
         while pretty_file_size >= 1000 and i < len(suffixes) - 1:
             pretty_file_size /= 1000
             i += 1
-        return re.sub(r"\.0+$", "", f"{pretty_file_size:.{max(i-1, 0)}f}{suffixes[i]}")
+        return re.sub(r"\.0+$", "", f"{pretty_file_size:.{max(i - 1, 0)}f}{suffixes[i]}")
 
     admin_form_fields = Document.admin_form_fields + (
         "extent",
