75779786f0f5bef41c003609ae323a67de89ea38
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-07-10 13:51:05 +0100

    Differing decimal places, and strip .0 or .00 endings

--- etna/core/models/documents.py
+++ etna/core/models/documents.py
@@ -1,3 +1,5 @@
+import re
+
 from django.db import models
 
 from wagtail.documents.models import AbstractDocument, Document
@@ -15,7 +17,7 @@ class CustomDocument(AbstractDocument):
         while pretty_file_size >= 1000 and i < len(suffixes) - 1:
             pretty_file_size /= 1000
             i += 1
-        return f"{pretty_file_size:.1f}{suffixes[i]}"
+        return re.sub(r'\.0+$', '', f"{pretty_file_size:.{max(i-1, 0)}f}{suffixes[i]}")
 
     admin_form_fields = Document.admin_form_fields + (
         "extent",
