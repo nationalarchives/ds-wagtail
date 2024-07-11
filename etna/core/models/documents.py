fc74256c7fea708b353f3b10c9732aa5c130eacf
James Biggs <62654785+jamesbiggs@users.noreply.github.com>
2024-07-11 10:38:51 +0100

    CHORE: Add help text to CustomDocument (#1673)

--- etna/core/models/documents.py
+++ etna/core/models/documents.py
@@ -1,13 +1,27 @@
 import re
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from wagtail.documents.models import AbstractDocument, Document
 
 
 class CustomDocument(AbstractDocument):
-    extent = models.CharField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    title = models.CharField(
+        max_length=255,
+        verbose_name=_("title"),
+        help_text="The name of the document as it will appear on the webpage. Please format this in sentence case with spaces between the words. e.g. Preservation policy part one.",
+    )
+    extent = models.CharField(
+        blank=True,
+        null=True,
+        help_text="The volume of the file so that users understand how much there is to consume. E.g. '3 pages' or '120 images'.",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        help_text="A short summary of what the document contains to help users understand what they are downloading.",
+    )
 
     @property
     def pretty_file_size(self):
