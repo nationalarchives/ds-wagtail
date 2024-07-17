1539ca44e56b1f4ce5fa2ec4ad08c54849aac1c6
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-07-17 11:43:03 +0100

    Added tables and documents to the accordions

--- etna/core/blocks/accordion.py
+++ etna/core/blocks/accordion.py
@@ -2,18 +2,30 @@ from django.conf import settings
 
 from wagtail import blocks
 
+from .document import DocumentsBlock
 from .paragraph import APIRichTextBlock
+from .tables import TableBlock
+
+
+class AccordionContentBlock(blocks.StreamBlock):
+    text = APIRichTextBlock(
+        required=True, features=settings.RESTRICTED_RICH_TEXT_FEATURES
+    )
+    table = TableBlock()
+    documents = DocumentsBlock()
+
+    class Meta:
+        icon = "list-ul"
+        label = "Accordion Body"
 
 
 class AccordionBlock(blocks.StructBlock):
     title = blocks.CharBlock(required=True)
-    body = APIRichTextBlock(
-        required=True, features=settings.RESTRICTED_RICH_TEXT_FEATURES
-    )
+    body = AccordionContentBlock(max_num=1)
 
     class Meta:
         icon = "list-ul"
-        label = "Accordion"
+        label = "Accordion Item"
 
 
 class AccordionsBlock(blocks.StructBlock):
