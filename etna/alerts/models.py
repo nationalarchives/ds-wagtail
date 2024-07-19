f31a264a7058fb4c27986a8422f19db106791f75
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-07-19 15:37:52 +0100

    alert_check -> alerts

--- etna/alerts/models.py
+++ etna/alerts/models.py
@@ -76,14 +76,14 @@ class AlertMixin(models.Model):
     )
 
     @property
-    def alert_check(self):
+    def alerts(self):
         """
         Retrieve the parent-most alert that is active and has cascade enabled.
         If there is no parent alert, then return the current alert if it is active.
         """
         if parent := self.get_parent():
             if type(parent.specific) is not Page:
-                if parent_alert := parent.specific.alert_check:
+                if parent_alert := parent.specific.alerts:
                     if parent_alert.cascade:
                         return parent_alert
         if self.alert and self.alert.active:
@@ -94,7 +94,7 @@ class AlertMixin(models.Model):
         FieldPanel("alert"),
     ]
 
-    api_fields = [APIField("alert_check", serializer=AlertSerializer())]
+    api_fields = [APIField("alerts", serializer=AlertSerializer())]
 
     class Meta:
         abstract = True
