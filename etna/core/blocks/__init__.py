9a041123bc0aab666939401123186b9ed26a03bb
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-07-23 17:49:39 +0100

    Created sub-sub-heading

--- etna/core/blocks/__init__.py
+++ etna/core/blocks/__init__.py
@@ -20,7 +20,7 @@ from .promoted_links import (
     PromotedListBlock,
 )
 from .quote import QuoteBlock
-from .section import SectionBlock, SubHeadingBlock
+from .section import SectionBlock, SubHeadingBlock, SubSubHeadingBlock
 from .tables import ContentTableBlock
 from .text import InsetTextBlock, WarningTextBlock
 from .video import YouTubeBlock
@@ -54,6 +54,7 @@ __all__ = [
     "SectionBlock",
     "SectionDepthAwareStructBlock",
     "SubHeadingBlock",
+    "SubSubHeadingBlock",
     "WarningTextBlock",
     "YouTubeBlock",
 ]
