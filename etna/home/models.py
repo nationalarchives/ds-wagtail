13a32ec5037d1792e47c472020d7df28d27a4c64
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-07-30 08:49:04 +0100

    Added `mourning_notice` to `HomePage`

--- etna/home/models.py
+++ etna/home/models.py
@@ -1,10 +1,37 @@
-from wagtail.admin.panels import FieldPanel
+from django.db import models
+
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.api import APIField
 from wagtail.fields import StreamField
+from wagtail.models import Page
 
 from etna.core.models import BasePageWithRequiredIntro
 
 from .blocks import HomePageStreamBlock
+
+
+class MourningNotice(models.Model):
+    """
+    A model to hold mourning notice information.
+    """
+
+    page = ParentalKey(Page, on_delete=models.CASCADE, related_name="mourning_notice")
+    name = models.CharField(max_length=255)
+    birth_date = models.CharField()
+    death_date = models.CharField()
+
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("birth_date"),
+        FieldPanel("death_date"),
+    ]
+
+    api_fields = [
+        APIField("name"),
+        APIField("birth_date"),
+        APIField("death_date"),
+    ]
 
 
 class HomePage(BasePageWithRequiredIntro):
@@ -14,9 +41,14 @@ class HomePage(BasePageWithRequiredIntro):
         FieldPanel("body"),
     ]
 
+    settings_panels = BasePageWithRequiredIntro.settings_panels + [
+        InlinePanel("mourning_notice", label="Mourning Notice", max_num=1),
+    ]
+
     # DataLayerMixin overrides
     gtm_content_group = "Homepage"
 
     api_fields = BasePageWithRequiredIntro.api_fields + [
+        APIField("mourning_notice"),
         APIField("body"),
     ]
