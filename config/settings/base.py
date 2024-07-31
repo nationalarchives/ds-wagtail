d440f51327fa9f4cd400d1b0516b96193110384a
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-07-31 17:47:12 +0100

    Remove unused import

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 
-from sysconfig import get_path
-
 import sentry_sdk
 
 from sentry_sdk.integrations.django import DjangoIntegration
