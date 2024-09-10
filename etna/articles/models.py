0700f3f1fcde3b508dd2129854c2eb0e89650d0a
James Biggs <62654785+jamesbiggs@users.noreply.github.com>
2024-09-10 13:31:40 +0100

    EDEV-36: RecordSerializer (#1714)
    Co-authored-by: Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>

--- etna/articles/models.py
+++ etna/articles/models.py
@@ -45,6 +45,7 @@ from etna.core.serializers import (
 from etna.core.utils import skos_id_from_text
 from etna.people.models import AuthorPageMixin
 from etna.records.fields import RecordField
+from etna.records.serializers import RecordSerializer
 
 from .blocks import ArticlePageStreamBlock
 
@@ -755,7 +756,7 @@ class RecordArticlePage(
             APIField("type_label"),
             APIField("date_text"),
             APIField("about", serializer=RichTextSerializer()),
-            APIField("record"),
+            APIField("record", serializer=RecordSerializer()),
             APIField("gallery_heading"),
             APIField("gallery_items", serializer=GallerySerializer(many=True)),
             APIField("image_library_link"),
