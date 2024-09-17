2bd590c8ba0d14f7c18c6e5e713f66f066f6fdbb
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-09-17 19:19:38 +0100

    Hard code ALLOWED_HOSTS (for now)

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,4 +9,6 @@ from .base import *  # noqa: F401
 # CLIENT_VERIFY_CERTIFICATES = True
 
 SECRET_KEY = os.getenv("SECRET_KEY", "")
-# Need to get the IP of the load balancer or reverse proxy
+
+# TODO: Remove later
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
