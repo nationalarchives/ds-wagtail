2c1e31c0957012ff1fa6cc8549c7f51efa4e427f
James Biggs <james.biggs@nationalarchives.gov.uk>
2024-10-14 10:27:58 +0100

    Remove sub-sub-heading validation

--- etna/blog/blocks.py
+++ etna/blog/blocks.py
@@ -39,21 +39,6 @@ class ContentSectionBlock(SectionDepthAwareStructBlock):
         label = "Section"
         template = "articles/blocks/section.html"
 
-    def clean(self, value):
-        clean = super().clean(value)
-        has_sub_heading = False
-        content = clean.get("content")
-
-        for block in content:
-            block_type = block.block_type
-            if block_type == "sub_heading":
-                has_sub_heading = True
-            elif block_type == "sub_sub_heading" and not has_sub_heading:
-                raise ValidationError(
-                    "A sub-sub-heading was found before any sub-headings."
-                )
-        return clean
-
 
 class BlogPostPageStreamBlock(blocks.StreamBlock):
     """
