b02157fc8bb96eeebb6eb3ec89c7e7b24b73359b
James Biggs <62654785+jamesbiggs@users.noreply.github.com>
2024-10-14 15:35:42 +0100

    DSRC-129: Blogs (#1725)

--- etna/core/serializers/__init__.py
+++ etna/core/serializers/__init__.py
@@ -1,9 +1,11 @@
+from .date import DateTimeSerializer
 from .images import DetailedImageSerializer, HighlightImageSerializer, ImageSerializer
 from .pages import DefaultPageSerializer
 from .richtext import RichTextSerializer
 from .tags import MourningSerializer, TaggableSerializer
 
 __all__ = [
+    "DateTimeSerializer",
     "DefaultPageSerializer",
     "DetailedImageSerializer",
     "HighlightImageSerializer",
