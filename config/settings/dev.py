6a35d50a979db9e2471b516c208a3531e76c5e25
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-01-07 12:35:20 +0000

    Set DEBUG_TOOLBAR_ENABLED as False by default

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -7,7 +7,7 @@ from .util import strtobool
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = strtobool(os.getenv("DEBUG", "True"))  # noqa: F405
 DEBUG_TOOLBAR_ENABLED = strtobool(  # noqa: F405
-    os.getenv("DEBUG_TOOLBAR_ENABLED", "True")  # noqa: F405
+    os.getenv("DEBUG_TOOLBAR_ENABLED", "False")  # noqa: F405
 )
 
 WAGTAILADMIN_BASE_URL = os.getenv("WAGTAILADMIN_BASE_URL", "http://localhost:8000")
