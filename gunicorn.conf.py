88c20bd27e0947e26ebde90489af4aa42d7e2d89
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-01-09 12:26:25 +0000

    Reduce Gunicorn worker count

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,5 +5,5 @@ import multiprocessing
 # http://docs.gunicorn.org/en/19.3/design.html#how-many-workers
 #
 # Platform's environment appear to be very memory constrained.
-# Reduce the number of workers to half the recommended number.
-workers = int(multiprocessing.cpu_count() / 2) + 1
+# Reduce the number of workers to a quarter of the recommended number.
+workers = int(multiprocessing.cpu_count() / 4) + 1
