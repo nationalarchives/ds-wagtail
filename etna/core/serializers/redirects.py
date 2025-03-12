c0eb94ddada3055844089c39fe226aaffca81872
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-03-12 16:07:18 +0000

    Rename 'title' to 'path' in RedirectSerializer representation

--- etna/core/serializers/redirects.py
+++ etna/core/serializers/redirects.py
@@ -5,7 +5,7 @@ class RedirectSerializer(serializers.Serializer):
     def to_representation(self, instance):
         return {
             "id": instance.id,
-            "title": instance.title,
+            "path": instance.title,
             "is_permanent": instance.is_permanent,
             "link": instance.link,
         }
