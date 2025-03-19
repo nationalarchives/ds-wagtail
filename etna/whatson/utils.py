ee56529a64d5bd0b695370dea8e6f9faa001d911
James Biggs <james.biggs@nationalarchives.gov.uk>
2025-03-19 17:30:58 +0000

    Retry API call after removing 400 error IDs

--- etna/whatson/utils.py
+++ etna/whatson/utils.py
@@ -1,5 +1,6 @@
 import requests
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.http import Http404
 from requests.exceptions import JSONDecodeError
 
@@ -12,6 +13,19 @@ def eventbrite_api_request_handler(uri, params={}):
     request = requests.get(url, params=params)
     if request.status_code == 404:
         raise Http404("Resource not found")
+    if request.status_code == 400:
+        error_detail = None
+        try:
+            error_detail = request.json().get("error_detail")
+        except JSONDecodeError:
+            pass
+        if error_detail and error_detail.get("ARGUMENTS_ERROR"):
+            error_indices = []
+            for id in error_detail.get("ARGUMENTS_ERROR"):
+                id = id.strip("event_ids.")
+                error_indices.append(id)
+            return {"error_indices": error_indices}
+        raise ValidationError("Bad request")
     if request.status_code == requests.codes.ok:
         try:
             return request.json()
@@ -37,7 +51,15 @@ def get_events_listings(page, page_size, params={}):
                 "time_filter": "current_future",
             }
         )
-    return eventbrite_api_request_handler(uri, params)
+    response = eventbrite_api_request_handler(uri, params)
+    if "error_indices" in response:
+        error_indices = response["error_indices"]
+        event_ids_list = params["event_ids"].split(",")
+        for index in sorted(error_indices, reverse=True):
+            del event_ids_list[int(index)]
+        params["event_ids"] = ",".join(event_ids_list)
+        return get_events_listings(page, page_size, params)
+    return response
 
 
 def get_event_details(event_id):
