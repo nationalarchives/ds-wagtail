63dfd5c6f4b4953faedda5541f5b36360e45acdc
James Biggs <james.biggs@nationalarchives.gov.uk>
2025-03-25 19:05:08 +0000

    Removed FEATURE_BETA_BANNER_ENABLED (and related code)

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -36,9 +36,6 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 IMAGE_VIEWER_REQUIRE_LOGIN = False
 RECORD_DETAIL_REQUIRE_LOGIN = False
 SEARCH_VIEWS_REQUIRE_LOGIN = False
-FEATURE_BETA_BANNER_ENABLED = strtobool(
-    os.getenv("FEATURE_BETA_BANNER_ENABLED", "True")
-)
 FEATURE_COOKIE_BANNER_ENABLED = strtobool(
     os.getenv("FEATURE_COOKIE_BANNER_ENABLED", "True")
 )
