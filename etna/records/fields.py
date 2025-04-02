a951f810284b3c921653fa1c9885c0d411931ab7
James Biggs <james.biggs@nationalarchives.gov.uk>
2025-04-02 12:20:17 +0100

    Partial tidy up

--- etna/records/fields.py
+++ etna/records/fields.py
@@ -1,48 +1,6 @@
-from typing import Any
-
-from django.core.exceptions import ValidationError
 from django.db.models.fields import Field
 from django.forms import CharField
-from django.utils.functional import SimpleLazyObject
-from requests import HTTPError
-
-from etna.ciim.exceptions import DoesNotExist
-
-from .api import records_client
-from .models import Record
-from .widgets import RecordChooser
 from .views.choosers import BaseRecordChooserWidget
-
-
-class LazyRecord(SimpleLazyObject):
-    """
-    The return type for ``RecordField``, which lazily fetches the
-    record details from CIIM when the value is interacted with in some way;
-    For example, when requesting a ``Record`` attribute value other than
-    ``iaid``, or requesting a string representation of it.
-    """
-
-    def __init__(self, iaid: str):
-        self.__dict__["iaid"] = iaid
-
-        def _fetch_record():
-            try:
-                return records_client.fetch(iaid=iaid)
-            except DoesNotExist:
-                return None
-
-        super().__init__(_fetch_record)
-
-    def __getattribute__(self, name: str) -> Any:
-        """
-        Overrides ``SimpleLazyObject.__getattribute__()`` to allow access to
-        ``iaid`` without fetching the full record details from CIIM.
-        """
-        if name == "iaid" and "iaid" in self.__dict__:
-            # The stored ``iaid`` value is not preserved when copying or
-            # pickling; hence the extra conditional.
-            return self.__dict__["iaid"]
-        return super().__getattribute__(name)
 
 
 class RecordChoiceField(CharField):
