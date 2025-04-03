b83c01db52f476cc319503ba7eead662071d74dc
James Biggs <james.biggs@nationalarchives.gov.uk>
2025-04-03 16:41:09 +0100

    Refined error handling

--- etna/ciim/client.py
+++ etna/ciim/client.py
@@ -1,14 +1,14 @@
 from django.conf import settings
 
 from etna.core.api import JSONAPIClient
-
+from sentry_sdk import capture_message
 
 class CIIMClient(JSONAPIClient):
     """
     Client for interacting with the CIIM API.
     """
 
-    def __init__(self, api_url: str = settings.CLIENT_BASE_URL, params: dict = {}):
+    def __init__(self, api_url: str = f"{settings.CLIENT_BASE_URL}", params: dict = {}):
         self.api_url: str = api_url
         self.params: dict = params
 
@@ -16,26 +16,31 @@ class CIIMClient(JSONAPIClient):
         """
         Get a single record instance from the CIIM API.
         """
-        response = self.get(path=path, headers={})
+        try:
+            response = self.get(path=path, headers={})
+        except Exception as e:
+            capture_message(f"CIIMClient.get_record_instance: {e}", level="error")
+            response = {"data": []}
 
         try:
             result = response.get("data", [])[0].get("@template", {}).get("details", {})
-            return result
-        except Exception as e:
-            return {"error": str(e)}
+        except IndexError:
+            result = {"referenceNumber": None, "summaryTitle": "CLIENT ERROR", "iaid": self.params.get("id")}
+        return result
 
     def get_record_list(self, path: str = "/search"):
         """
         Get a list of records from the CIIM API.
         """
-        response = self.get(path=path, headers={})
-
         try:
-            results = response.get("data", [])
-            total = response.get("stats", {}).get("total", 0)
-            return results, total
+            response = self.get(path=path, headers={})
         except Exception as e:
-            return {"error": str(e)}
+            capture_message(f"CIIMClient.get_record_list: {e}", level="error")
+            response = {"data": []}
+
+        results = response.get("data", [])
+        total = response.get("stats", {}).get("total", 0)
+        return results, total
 
     def get_serialized_record(self):
         """
