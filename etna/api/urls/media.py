4ca8d7c43b304a9be3f74c7e8a5e3a2efe4d1924
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-04-17 13:45:50 +0100

    Add CustomMediaAPIViewSet to handle media API endpoints

---
+++ etna/api/urls/media.py
@@ -0,0 +1,15 @@
+from wagtailmedia.api.views import MediaAPIViewSet
+
+
+class CustomMediaAPIViewSet(MediaAPIViewSet):
+    body_fields = MediaAPIViewSet.body_fields + [
+        "title",
+        "file",
+        "tags",
+        "date",
+        "duration",
+        "subtitles_file",
+        "subtitles_file_full_url",
+        "chapters_file",
+        "chapters_file_full_url",
+    ]
