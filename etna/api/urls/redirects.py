76555d9df646dc7db34c7eead607d36365629738
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-04-25 09:16:27 +0100

    Refactor RedirectsAPIViewSet to extend from BaseRedirectsAPIViewSet and remove unused RedirectSerializer

--- etna/api/urls/redirects.py
+++ etna/api/urls/redirects.py
@@ -1,33 +1,9 @@
-from rest_framework.response import Response
-from wagtail.api.v2.views import BaseAPIViewSet
-from wagtail.contrib.redirects.models import Redirect
-
-from etna.core.serializers.redirects import RedirectSerializer
+from wagtail.contrib.redirects.api import RedirectsAPIViewSet as BaseRedirectsAPIViewSet
 
 
-class RedirectsAPIViewSet(BaseAPIViewSet):
-    model = Redirect
+class RedirectsAPIViewSet(BaseRedirectsAPIViewSet):
+    body_fields = BaseRedirectsAPIViewSet.body_fields + ["is_permanent"]
 
-    def listing_view(self, request):
-        queryset = self.get_queryset()
-        self.check_query_parameters(queryset)
-        queryset = self.filter_queryset(queryset)
-        queryset = self.paginate_queryset(queryset)
-        serializer = RedirectSerializer(queryset, many=True)
-        return self.get_paginated_response(serializer.data)
-
-    def detail_view(self, request, pk):
-        instance = self.get_object()
-        serializer = RedirectSerializer(instance)
-        return Response(serializer.data)
-
-    def find_object(self, queryset, request):
-        if "path" in request.GET:
-            # Taken from https://github.com/wagtail/wagtail/blob/main/wagtail/contrib/redirects/models.py#L165-L169
-            path = request.GET["path"]
-            path = path.strip()
-            if not path.startswith("/"):
-                path = "/" + path
-            if path.endswith("/") and len(path) > 1:
-                path = path[:-1]
-            return queryset.get(old_path=path)
+    listing_default_fields = BaseRedirectsAPIViewSet.listing_default_fields + [
+        "is_permanent",
+    ]
