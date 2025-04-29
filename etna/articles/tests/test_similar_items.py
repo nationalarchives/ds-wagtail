580f34d0822460809959c9ebae3f3d4a3e110e96
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-04-29 12:51:39 +0100

    Reduce expected query count in test_search_prevented_if_no_tag_matches_identified

--- etna/articles/tests/test_similar_items.py
+++ etna/articles/tests/test_similar_items.py
@@ -97,7 +97,7 @@ class TestArticlePageSimilarItems(TestCase):
 
     def test_search_prevented_if_no_tag_matches_identified(self):
         test_page = ArticlePage.objects.get(id=self.different_tags_page.id)
-        with self.assertNumQueries(4):
+        with self.assertNumQueries(3):
             self.assertFalse(test_page.similar_items)
 
 
