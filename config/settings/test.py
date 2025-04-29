0fa01df9bca9f9f2944f962ad37d90e7862a736c
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-04-29 12:48:15 +0100

    Ignore linting errors

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *  # noqa: F401
 
 SECRET_KEY = "abc123"
@@ -13,7 +15,7 @@ INSTALLED_APPS.remove("birdbath")
 # Allow integration tests to run without needing to collectstatic
 # See https://docs.djangoproject.com/en/3.1/ref/contrib/staticfiles/#staticfilesstorage
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # noqa: F405
 
 ROSETTA_API_URL = "https://rosetta.test/data"
 
