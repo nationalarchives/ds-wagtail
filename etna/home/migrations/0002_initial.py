c0db3b38e9ba71f984025dbf3ad18422095b568a
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-05-02 09:57:38 +0100

    Remove homepage creation and deletion logic from initial migration

--- etna/home/migrations/0002_initial.py
+++ etna/home/migrations/0002_initial.py
@@ -4,53 +4,6 @@ import django.db.models.deletion
 import modelcluster.fields
 from django.db import migrations, models
 
-
-def create_homepage(apps, schema_editor):
-    # Get models
-    ContentType = apps.get_model("contenttypes.ContentType")
-    Page = apps.get_model("wagtailcore.Page")
-    Site = apps.get_model("wagtailcore.Site")
-    HomePage = apps.get_model("home.HomePage")
-
-    # Delete the default homepage
-    # If migration is run multiple times, it may have already been deleted
-    Page.objects.filter(id=2).delete()
-
-    # Create content type for homepage model
-    homepage_content_type, __ = ContentType.objects.get_or_create(
-        model="homepage", app_label="home"
-    )
-
-    # Create a new homepage
-    homepage = HomePage.objects.create(
-        title="Home",
-        draft_title="Home",
-        slug="home",
-        content_type=homepage_content_type,
-        path="00010001",
-        depth=2,
-        numchild=0,
-        url_path="/home/",
-        locale_id=1
-    )
-
-    # Create a site with the new homepage set as the root
-    Site.objects.create(hostname="localhost", root_page=homepage, is_default_site=True)
-
-
-def remove_homepage(apps, schema_editor):
-    # Get models
-    ContentType = apps.get_model("contenttypes.ContentType")
-    HomePage = apps.get_model("home.HomePage")
-
-    # Delete the default homepage
-    # Page and Site objects CASCADE
-    HomePage.objects.filter(slug="home", depth=2).delete()
-
-    # Delete content type for homepage model
-    ContentType.objects.filter(model="homepage", app_label="home").delete()
-
-
 class Migration(migrations.Migration):
 
     initial = True
@@ -82,5 +35,4 @@ class Migration(migrations.Migration):
             name='page',
             field=modelcluster.fields.ParentalKey(on_delete=django.db.models.deletion.CASCADE, related_name='mourning', to='wagtailcore.page'),
         ),
-        migrations.RunPython(create_homepage, remove_homepage),
     ]
