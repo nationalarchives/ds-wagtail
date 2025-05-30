b2305d2a009530ebf4dbce21d8765dc5ee105a09
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-05-30 15:43:18 +0100

    Remove logging configuration from development settings

--- config/settings/develop.py
+++ config/settings/develop.py
@@ -1,15 +1,9 @@
-import logging
 import os
 
 from .base import *  # noqa: F401
 from .util import strtobool
 
 SENTRY_SAMPLE_RATE = float(os.getenv("SENTRY_SAMPLE_RATE", "1.0"))
-
-if DEBUG:  # noqa: F405
-    from .base import LOGGING
-
-    LOGGING["root"]["level"] = "DEBUG"
 
 if DEBUG and strtobool(os.getenv("DEBUG_TOOLBAR_ENABLED", "False")):  # noqa: F405
     from .base import INSTALLED_APPS, MIDDLEWARE
