4da05e21c3e11f964975f074bfe37203a387172e
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-07-01 14:11:22 +0100

    Enhance RedirectsAPIViewSet to support site filtering

--- etna/api/urls/redirects.py
+++ etna/api/urls/redirects.py
@@ -1,9 +1,43 @@
+from django.db.models import Q
+from wagtail.api.v2.utils import BadRequestError
 from wagtail.contrib.redirects.api import RedirectsAPIViewSet as BaseRedirectsAPIViewSet
+from wagtail.contrib.redirects.models import Redirect
+from wagtail.models import Site
 
 
 class RedirectsAPIViewSet(BaseRedirectsAPIViewSet):
     body_fields = BaseRedirectsAPIViewSet.body_fields + ["is_permanent"]
 
     listing_default_fields = BaseRedirectsAPIViewSet.listing_default_fields + [
-        "is_permanent",
+        "is_permanent"
     ]
+
+    known_query_parameters = BaseRedirectsAPIViewSet.known_query_parameters.union(
+        ["site"]
+    )
+
+    def get_queryset(self):
+        request = self.request
+
+        queryset = Redirect.objects.all()
+
+        if "site" in request.GET:
+            if ":" in request.GET["site"]:
+                (hostname, port) = request.GET["site"].split(":", 1)
+                query = {
+                    "hostname": hostname,
+                    "port": port,
+                }
+            else:
+                query = {
+                    "hostname": request.GET["site"],
+                }
+            try:
+                site = Site.objects.get(**query)
+            except Site.MultipleObjectsReturned:
+                raise BadRequestError(
+                    "Your query returned multiple sites. Try adding a port number to your site filter."
+                )
+            queryset = queryset.filter(Q(site=site) | Q(site=None))
+
+        return queryset
