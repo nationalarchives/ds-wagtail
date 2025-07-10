d4ebd504477c8a74df0fb0a6d4c20e942ec9c4be
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-07-10 16:24:22 +0100

    Remove unused imports

--- app/core/models/basepage.py
+++ app/core/models/basepage.py
@@ -1,10 +1,7 @@
-from typing import Any, Dict
-
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import options
-from django.http import HttpRequest
 from django.utils.functional import cached_property
 from django.utils.text import capfirst
 from django.utils.translation import gettext_lazy as _
