23d96b9cbb78386803fbbae5765f2054bdd982a2
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-07-23 10:06:23 +0100

    Release version into Wagtail homepage

--- app/core/context_processors.py
+++ app/core/context_processors.py
@@ -4,4 +4,5 @@ from django.conf import settings
 def settings_vars(request):
     return {
         "ENVIRONMENT_NAME": settings.ENVIRONMENT_NAME,
+        "BUILD_VERSION": settings.BUILD_VERSION,
     }
