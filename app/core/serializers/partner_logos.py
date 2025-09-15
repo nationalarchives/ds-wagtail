90ed7df8f2f9ebe054b41e50f29564b058ffdf55
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-09-15 16:00:02 +0100

    Update serialisation of partner logos

--- app/core/serializers/partner_logos.py
+++ app/core/serializers/partner_logos.py
@@ -12,23 +12,9 @@ class PartnerLogoSerializer(serializers.Serializer):
     def to_representation(self, instance):
         return {
             "name": instance.name,
-            "image": (
-                (settings.WAGTAILADMIN_BASE_URL + instance.svg_file.url)
-                if instance.svg_file
-                else (
-                    ImageSerializer().to_representation(instance.raster_file)
-                    if instance.raster_file
-                    else None
-                )
-            ),
-            "image_dark": (
-                (settings.WAGTAILADMIN_BASE_URL + instance.svg_file_dark.url)
-                if instance.svg_file_dark
-                else (
-                    ImageSerializer().to_representation(instance.raster_file_dark)
-                    if instance.raster_file_dark
-                    else None
-                )
-            ),
+            "vector": (settings.WAGTAILADMIN_BASE_URL + instance.svg_file.url) if instance.svg_file else None,
+            "raster": ImageSerializer().to_representation(instance.raster_file) if instance.raster_file else None,
+            "vector_dark": (settings.WAGTAILADMIN_BASE_URL + instance.svg_file_dark.url) if instance.svg_file_dark else None,
+            "raster_dark": ImageSerializer().to_representation(instance.raster_file_dark) if instance.raster_file_dark else None,
             "alt_text": instance.alt_text,
         }
