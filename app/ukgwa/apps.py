aa064696f92fdbd5b69241bc2be19d33105517f9
Ian Meigh <ian.meigh@torchbox.com>
2025-12-26 11:29:52 +0000

    feat: add empty ukgwa app

---
+++ app/ukgwa/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UkgwaConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app.ukgwa"
+    verbose_name = "UK Government Web Archive"
