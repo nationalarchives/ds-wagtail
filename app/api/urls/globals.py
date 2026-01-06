cc7c5f2dfa60c5dfec475b2df4b64eb2daddf517
Ian Meigh <ian.meigh@torchbox.com>
2026-01-06 16:20:45 +0000

    fix: ensure global notifications always return default site data

--- app/api/urls/globals.py
+++ app/api/urls/globals.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from wagtail.models import Site
 
 from app.alerts.models import AlertSerializer
-from app.api.utils import get_site_from_request
 from app.core.models import BasePage
 from app.core.serializers import MourningSerializer
 from app.home.models import HomePage
@@ -14,41 +14,24 @@ class GlobalsAPIViewSet(GenericViewSet):
     model = BasePage
 
     def notifications_view(self, request):
-        site = get_site_from_request(request)
+        """
+        Returns global notifications for the default site.
+        """
+        site = Site.objects.get(is_default_site=True)
+        homepage = HomePage.objects.get(id=site.root_page_id)
 
-        # If no site found, return empty notifications
-        if not site:
-            return Response(
-                {
-                    "global_alert": None,
-                    "mourning_notice": None,
-                }
-            )
-
-        # Get the HomePage for specific site
-        try:
-            homepage = HomePage.objects.get(id=site.root_page_id)
-        except HomePage.DoesNotExist:
-            # If the root page isn't a HomePage, set notifications to None
-            return Response(
-                {
-                    "global_alert": None,
-                    "mourning_notice": None,
-                }
-            )
-
-        homepage_global_notification = homepage.global_alert
         global_alert = (
-            AlertSerializer(homepage_global_notification).data
-            if homepage_global_notification and homepage_global_notification.cascade
+            AlertSerializer(homepage.global_alert).data
+            if homepage.global_alert and homepage.global_alert.cascade
             else None
         )
-        homepage_mourning_notice = HomePage.objects.first().mourning_notice
+
         mourning_notice = (
-            MourningSerializer(homepage_mourning_notice).data
-            if homepage_mourning_notice
+            MourningSerializer(homepage.mourning_notice).data
+            if homepage.mourning_notice
             else None
         )
+
         return Response(
             {
                 "global_alert": global_alert,
