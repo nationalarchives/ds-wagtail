a8187b001eaf3bfd553319f85255ffb3d62bdd98
Ian Meigh <ian.meigh@torchbox.com>
2026-01-09 16:44:24 +0000

    refactor: update non-standard block naming

--- app/ukgwa/blocks.py
+++ app/ukgwa/blocks.py
@@ -1,35 +1,19 @@
-from django.conf import settings
-from django.utils.translation import gettext_lazy as _
 from wagtail import blocks
 
 from app.core.blocks import (
     CallToActionBlock,
     ContentImageBlock,
+    ParagraphBlock,
     QuoteBlock,
+    SubHeadingBlock,
     YouTubeBlock,
 )
-from app.core.blocks.paragraph import APIRichTextBlock
-
-
-class ExternalLinkBlock(blocks.StructBlock):
-    url = blocks.URLBlock(label="URL")
-    link_text = blocks.CharBlock()
-
-    class Meta:
-        label = _("External link")
-        icon = "link"
 
 
 class InformationPageStreamBlock(blocks.StreamBlock):
-    heading = blocks.CharBlock(
-        form_classname="title",
-        label="Heading",
-        icon="title",
-    )
-    paragraph = APIRichTextBlock(
-        required=True, features=settings.RESTRICTED_RICH_TEXT_FEATURES
-    )
+    sub_heading = SubHeadingBlock()
+    paragraph = ParagraphBlock()
     image = ContentImageBlock()
     quote = QuoteBlock()
-    embed = YouTubeBlock()
+    youtube_video = YouTubeBlock()
     call_to_action = CallToActionBlock()
