386016efe8d09a01a2be614c7af67660c45c17f6
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-01-13 13:32:11 +0000

    Implement RedirectsSiteFilter to enhance site-based filtering for redirects

--- app/api/urls/redirects.py
+++ app/api/urls/redirects.py
@@ -1,11 +1,12 @@
-from django.db.models import Q
-from wagtail.api.v2.utils import BadRequestError
 from wagtail.contrib.redirects.api import RedirectsAPIViewSet as BaseRedirectsAPIViewSet
 from wagtail.contrib.redirects.models import Redirect
-from wagtail.models import Site
+
+from ..filters import RedirectsSiteFilter
 
 
 class RedirectsAPIViewSet(BaseRedirectsAPIViewSet):
+    model = Redirect
+
     body_fields = BaseRedirectsAPIViewSet.body_fields + ["is_permanent"]
 
     listing_default_fields = BaseRedirectsAPIViewSet.listing_default_fields + [
@@ -16,38 +17,6 @@ class RedirectsAPIViewSet(BaseRedirectsAPIViewSet):
         ["site"]
     )
 
-    def get_queryset(self):
-        request = self.request
-
-        queryset = Redirect.objects.all()
-
-        if "site" in request.GET:
-            if ":" in request.GET["site"]:
-                (hostname, port) = request.GET["site"].split(":", 1)
-                query = {
-                    "hostname": hostname,
-                    "port": port,
-                }
-            else:
-                query = {
-                    "hostname": request.GET["site"],
-                }
-            try:
-                site = Site.objects.get(**query)
-            except Site.MultipleObjectsReturned:
-                raise BadRequestError(
-                    "Your query returned multiple sites. Try adding a port number to your site filter."
-                )
-            except Site.DoesNotExist:
-                site = queryset.none()
-        else:
-            # Otherwise, find the site from the request
-            # site = Site.find_for_request(self.request)
-
-            # Otherwise, use the default site
-            site = Site.objects.get(is_default_site=True)
-
-        if site:
-            queryset = queryset.filter(Q(site=site) | Q(site=None))
-
-        return queryset
+    filter_backends = [
+        RedirectsSiteFilter,
+    ]
