caa95d9fea56dda439a3309b3310a0f1d83dd33d
Ian Meigh <ian.meigh@torchbox.com>
2026-01-20 11:17:54 +0000

    chore: fix formatting

--- app/alerts/models.py
+++ app/alerts/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.api import APIField
 from wagtail.fields import RichTextField
-from wagtail.models import Page
 from wagtail.snippets.models import register_snippet
 
 from .serializers import AlertSerializer, ThemedAlertSerializer
