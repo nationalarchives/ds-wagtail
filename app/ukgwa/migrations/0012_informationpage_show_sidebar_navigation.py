69f8929ba4b547831e096b568d2ca09192c04d93
Ian Meigh <ian.meigh@torchbox.com>
2026-01-28 14:22:56 +0000

    fix: resolve migrations

--- app/ukgwa/migrations/0012_informationpage_show_sidebar_navigation.py
+++ app/ukgwa/migrations/0012_informationpage_show_sidebar_navigation.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("ukgwa", "0008_informationpage_themed_alert_and_more"),
+        ("ukgwa", "0011_listingpage"),
     ]
 
     operations = [
