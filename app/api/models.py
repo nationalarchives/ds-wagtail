15ac2e9ab93bc33bd48288595f74d3ab380b566d
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-02-11 13:58:28 +0000

    Enhance API token management commands

--- app/api/models.py
+++ app/api/models.py
@@ -1,4 +1,6 @@
-from uuid import uuid4
+import binascii
+import datetime
+import os
 
 from django.db import models
 
@@ -10,9 +12,20 @@ class APIToken(models.Model):
     """
 
     name = models.CharField(max_length=100, unique=True)
-    key = models.CharField(default=uuid4, max_length=40, unique=True)
+    key = models.CharField(max_length=40, unique=True)
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        self.updated = datetime.datetime.now()
+        return super().save(*args, **kwargs)
+
+    @classmethod
+    def generate_key(cls):
+        return binascii.hexlify(os.urandom(20)).decode()
 
     def __str__(self):
         return self.name
