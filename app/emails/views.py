32ddbe0c46b0e116faead69e173ec5cce856f0bb
rzarrouk <rachelzarrouk@googlemail.com>
2026-03-13 08:52:05 +0000

    Update views.py with dummy data to test

--- app/emails/views.py
+++ app/emails/views.py
@@ -1,5 +1,17 @@
+from types import SimpleNamespace
 from django.shortcuts import render
 
-
 def email(request):
-    return render(request, "wagtailadmin/account/password_reset/email.txt")
+
+    context = {
+        "uid": "Mg",
+        "token": "set-password-preview-token",
+        "protocol": request.scheme,
+        "domain": request.get_host(),
+        "user": SimpleNamespace(
+            USERNAME_FIELD="username",
+            get_username="MyUsername",
+        ),
+    }
+
+    return render(request, "wagtailadmin/account/password_reset/email.txt", context)
