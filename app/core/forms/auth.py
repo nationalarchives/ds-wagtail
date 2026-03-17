0852d4bb9a425f7e6fe9781799be047a838a7dde
rzarrouk <rachelzarrouk@googlemail.com>
2026-03-17 09:27:21 +0000

    Add fallback option for html to plaintext password emails.

---
+++ app/core/forms/auth.py
@@ -0,0 +1,13 @@
+from wagtail.admin.forms.auth import PasswordResetForm
+
+
+class HtmlPasswordResetForm(PasswordResetForm):
+    html_email_template_name = "wagtailadmin/account/password_reset/email.html"
+    email_template_name = "wagtailadmin/account/password_reset/email_plain.txt"
+
+    def save(self, *args, **kwargs):
+        kwargs["html_email_template_name"] = (
+            kwargs.get("html_email_template_name") or self.html_email_template_name
+        )
+        kwargs["email_template_name"] = self.email_template_name
+        return super().save(*args, **kwargs)
