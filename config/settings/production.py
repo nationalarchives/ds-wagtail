6f63cc56c13b56f428667c3179989af69db73c11
rzarrouk <rachelzarrouk@googlemail.com>
2026-04-15 17:04:26 +0100

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -136,6 +136,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "app.core.context_processors.settings_vars",
             ],
         },
     },
