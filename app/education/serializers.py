025c9f245753f85d7b20e93720942b44dcd42456
rzarrouk <rachelzarrouk@googlemail.com>
2026-04-23 13:36:13 +0100

    Update and clean up serialisers

--- app/education/serializers.py
+++ app/education/serializers.py
@@ -1,5 +1,13 @@
-from app.core.serializers import RichTextSerializer
+from app.core.serializers import DefaultPageSerializer
 from rest_framework import serializers
+
+
+class _StreamFieldRepresentationMixin:
+    @staticmethod
+    def _serialize_stream_field(instance, field_name):
+        field = instance._meta.get_field(field_name)
+        stream_value = getattr(instance, field_name)
+        return field.stream_block.get_api_representation(stream_value, context={})
 
 
 class KeyStageSerializer(serializers.Serializer):
@@ -17,11 +25,56 @@ class ThemeSerializer(serializers.Serializer):
     slug = serializers.SlugField()
 
 
-class CurriculumConnectionSerializer(serializers.Serializer):
+class CurriculumConnectionSerializer(_StreamFieldRepresentationMixin, serializers.Serializer):
     def to_representation(self, instance):
+        if not instance:
+            return None
+
         return {
             "key_stage": KeyStageSerializer().to_representation(instance.key_stage),
-            "connection_description": RichTextSerializer().to_representation(
-                instance.curriculum_connection_description
+            "connection_description": self._serialize_stream_field(
+                instance, "curriculum_connection_description"
             ),
+        }
+
+
+class SourceSerializer(_StreamFieldRepresentationMixin, serializers.Serializer):
+    def to_representation(self, instance):
+        if not instance:
+            return None
+
+        return {
+            "source_title": instance.source_title,
+            "source_image": self._serialize_stream_field(instance, "source_image"),
+            "source_media": self._serialize_stream_field(instance, "source_media"),
+            "source_youtube": self._serialize_stream_field(
+                instance, "source_youtube"
+            ),
+            "source_media_caption": self._serialize_stream_field(
+                instance, "source_media_caption"
+            ),
+            "source_media_featured_link": self._serialize_stream_field(
+                instance, "source_media_featured_link"
+            ),
+            "source_media_featured_external_link": (
+                instance.source_media_featured_external_link
+            ),
+            "source_description": self._serialize_stream_field(
+                instance, "source_description"
+            ),
+            "source_question": self._serialize_stream_field(
+                instance, "source_question"
+            ),
+        }
+
+
+class EducationReadMoreLinkSerializer(serializers.Serializer):
+    def to_representation(self, instance):
+        if not instance:
+            return None
+
+        return {
+            "selected_page": DefaultPageSerializer().to_representation(
+                instance.selected_page
+            )
         }
