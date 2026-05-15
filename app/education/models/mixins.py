90d78f4774c711833d0d9fcb556aec58e923a99b
rzarrouk <rachelzarrouk@googlemail.com>
2026-05-15 12:22:09 +0100

    Formatting changes

--- app/education/models/mixins.py
+++ app/education/models/mixins.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
-from wagtail.admin.panels import PageChooserPanel
+from wagtail.admin.panels import InlinePanel, PageChooserPanel
 from wagtail.models import Orderable
-from wagtail.admin.panels import InlinePanel
 
 
 class EducationTaxonomyMixin:
