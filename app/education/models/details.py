8a5cfe6732e8e7077b475fdea027d06ba18e1ed2
rzarrouk <rachelzarrouk@googlemail.com>
2026-05-15 13:56:44 +0100

    Fix missing ValidationError import

--- app/education/models/details.py
+++ app/education/models/details.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
