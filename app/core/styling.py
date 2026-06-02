91382f620c267e6dd0a5ab117b3e3d9631b1abd0
James Biggs <62654785+jamesbiggs@users.noreply.github.com>
2026-06-02 11:21:49 +0100

    Remove gettext_lazy (#1985)

--- app/core/styling.py
+++ app/core/styling.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 class BrandColourChoices(models.TextChoices):
@@ -8,13 +7,13 @@ class BrandColourChoices(models.TextChoices):
     on various components on the site.
     """
 
-    NONE = "none", _("None")
-    BLACK = "black", _("Black")
-    PINK = "pink", _("Pink")
-    ORANGE = "orange", _("Orange")
-    YELLOW = "yellow", _("Yellow")
-    GREEN = "green", _("Green")
-    BLUE = "blue", _("Blue")
+    NONE = "none", "None"
+    BLACK = "black", "Black"
+    PINK = "pink", "Pink"
+    ORANGE = "orange", "Orange"
+    YELLOW = "yellow", "Yellow"
+    GREEN = "green", "Green"
+    BLUE = "blue", "Blue"
 
 
 class HeroColourChoices(models.TextChoices):
@@ -23,10 +22,10 @@ class HeroColourChoices(models.TextChoices):
     on the hero component.
     """
 
-    NONE = "none", _("None")
-    CONTRAST = "contrast", _("Contrast")
-    TINT = "tint", _("Tint")
-    ACCENT = "accent", _("Accent")
+    NONE = "none", "None"
+    CONTRAST = "contrast", "Contrast"
+    TINT = "tint", "Tint"
+    ACCENT = "accent", "Accent"
 
 
 class HeroLayoutChoices(models.TextChoices):
@@ -35,6 +34,6 @@ class HeroLayoutChoices(models.TextChoices):
     on the hero component.
     """
 
-    DEFAULT = "default", _("Default")
-    SHIFT = "shift", _("Shifted")
-    SPLIT = "split", _("Split")
+    DEFAULT = "default", "Default"
+    SHIFT = "shift", "Shifted"
+    SPLIT = "split", "Split"
