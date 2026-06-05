8b956be681382880df919a5b0974531d74f2230d
rzarrouk <rachelzarrouk@googlemail.com>
2026-06-05 12:42:28 +0100

    Skeleton functions to split out logic

--- app/api/management/commands/manage_2fa_devices.py
+++ app/api/management/commands/manage_2fa_devices.py
@@ -3,32 +3,44 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = "Manage 2FA devices." #TODO make better
 
-#TODO copoied from api listing for skeleton
-    # def add_arguments(self, parser):
-    #     parser.add_argument(
-    #         "--quiet",
-    #         action="store_true",
-    #         help="Don't print human-readable output, just the token key (for create/refresh)",
-    #     )
+
+    def add_arguments(self, parser):
+
+    	parser.add_argument("--target-email", required=True, help="Email of the user to be reset.")
+    	parser.add_argument("--admin-username", required=True, help="Admin/username performing reset.")
+        parser.add_argument("--reason", default="Your 2FA devices have been removed from your account. Please reset your password. /n Note: Any active sessions you may have had have also been deleted.",  help="Optional reason to include in the notification email")
+
+	
+	#be authenticated with an admin account
+	def admin_authentication():
 
 
 
-    # def _list_tokens(self, quiet=False):
-    #     tokens = APIToken.objects.all()
-    #     if tokens:
-    #         if not quiet:
-    #             self.stdout.write(self.style.SUCCESS("API tokens:"))
-    #         for token in tokens:
-    #             self.stdout.write(
-    #                 f"- {token.name}{' (active)' if token.active else ' (inactive)'}"
-    #             )
-    #     else:
-    #         self.stdout.write("No API tokens found.")
+
+	# remove all 2FA devices
+	def remove_devices():
 
 
 
-    # def handle(self, *args, **options):
-    #     quiet = options["quiet"]
+	# reset the password
+	def reset_password():
 
-    #     self._list_tokens(quiet)
+
+
+	# remove all active sessions for that user
+	def remove_all_active_sessions():
+
+
+
+	# send them an email
+	def send_email():
+
+
+
+    def handle(self, *args, **options):
+        target_email = options["target_email"].strip().lower()
+        admin_username = options["admin_username"].strip()
+        reason = options["reason"]
+
+        self._list_tokens(quiet)
 
