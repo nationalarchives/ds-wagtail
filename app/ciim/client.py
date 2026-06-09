4b4a0602229092b7c408496c798c213dfcf82cfb
rzarrouk <rachelzarrouk@googlemail.com>
2026-06-09 13:56:48 +0100

    Remove now redundant check for api_url

--- app/ciim/client.py
+++ app/ciim/client.py
@@ -19,8 +19,6 @@ class CIIMClient(SimpleJsonApiClient):
 
     def __init__(self, api_url: str | None = None, default_params: dict = None):
         api_url = api_url or settings.ROSETTA_API_URL
-        if not api_url:
-            raise ValueError("ROSETTA_API_URL is not defined")
         if default_params is None:
             default_params = {}
         super().__init__(api_url, default_params=default_params)
