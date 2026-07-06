b23302109d0640195c2233036ce944f4c0413804
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-07-06 16:18:59 +0100

    Add validation for record IDs to RecordField

--- app/ciim/fields.py
+++ app/ciim/fields.py
@@ -1,3 +1,6 @@
+import re
+
+from django.core.exceptions import ValidationError
 from django.db.models import CharField
 
 
@@ -8,6 +11,24 @@ class RecordField(CharField):
     that record in the database.
     """
 
+    RECORD_ID_PATTERN = re.compile(
+        r"^(?:"
+        r"[0-9a-fA-F]{8}-"
+        r"[0-9a-fA-F]{4}-"
+        r"[0-9a-fA-F]{4}-"
+        r"[0-9a-fA-F]{4}-"
+        r"[0-9a-fA-F]{12}"
+        r"|"
+        r"[A-Z][0-9]+"
+        r")$"
+    )
+
     def __init__(self, *args, **kwargs):
         kwargs.update(max_length=50, null=True)
         super().__init__(*args, **kwargs)
+
+    def validate(self, value, model_instance):
+        if value and not self.RECORD_ID_PATTERN.fullmatch(value):
+            raise ValidationError("Enter a valid record ID")
+
+        return super().validate(value, model_instance)
