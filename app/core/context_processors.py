87d17f353c2f44236ffbdea266c9470ee358bad1
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-07-09 15:20:48 +0100

    Add MEDIA_PAGE_URL to settings and update context processor for image and video URLs

--- app/core/context_processors.py
+++ app/core/context_processors.py
@@ -2,7 +2,11 @@ from django.conf import settings
 
 
 def settings_vars(request):
+    MEDIA_PAGE_URL = getattr(settings, "MEDIA_PAGE_URL", "")
     return {
-        "ENVIRONMENT_NAME": settings.ENVIRONMENT_NAME,
-        "BUILD_VERSION": settings.BUILD_VERSION,
+        "ENVIRONMENT_NAME": getattr(settings, "ENVIRONMENT_NAME", ""),
+        "BUILD_VERSION": getattr(settings, "BUILD_VERSION", ""),
+        "IMAGE_PAGE_URL": MEDIA_PAGE_URL + "/image/",
+        "VIDEO_PAGE_URL": MEDIA_PAGE_URL + "/video/",
+        "AUDIO_PAGE_URL": MEDIA_PAGE_URL + "/video/", # TODO: Update to /audio/ when implemented in frontend
     }
