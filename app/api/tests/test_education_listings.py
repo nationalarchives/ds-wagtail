7b7b4ea37fa67a1cc77f14988a81d2afab21dbad
rzarrouk <rachelzarrouk@googlemail.com>
2026-07-13 11:59:53 +0100

    Update tests to ensure TNA isn't default filter

--- app/api/tests/test_education_listings.py
+++ app/api/tests/test_education_listings.py
@@ -354,6 +354,52 @@ class EducationListingsAPITest(WagtailPageTestCase):
         )
         self.assertEqual(past_session_response.json()["meta"]["total_count"], 0)
 
+    def test_sessions_endpoint_region_filter_does_not_default_to_national_archives(
+        self,
+    ):
+        future_date = localdate() + timedelta(days=7)
+
+        self.create_session_page(
+            slug="mouse-school-north-east",
+            title="Mouse school north east",
+            key_stages=[self.ks1],
+            time_periods=[self.early_modern],
+            themes=[self.medicine_welfare],
+            location_types=[SessionLocation.LocationType.YOUR_SCHOOL],
+            start_date=future_date,
+        )
+        self.create_session_page(
+            slug="mouse-custom-south-west",
+            title="Mouse custom south west",
+            key_stages=[self.ks1],
+            time_periods=[self.early_modern],
+            themes=[self.medicine_welfare],
+            location_types=[SessionLocation.LocationType.CUSTOM],
+            start_date=future_date,
+        )
+        self.create_session_page(
+            slug="mouse-at-tna",
+            title="Mouse at TNA",
+            key_stages=[self.ks1],
+            time_periods=[self.early_modern],
+            themes=[self.medicine_welfare],
+            location_types=[SessionLocation.LocationType.NATIONAL_ARCHIVES],
+            start_date=future_date,
+        )
+
+        response = self.request_api(
+            "/api/v2/education/sessions/?region=north-east&region=south-west"
+        )
+
+        self.assertEqual(response.status_code, 200, response.content.decode())
+        response_data = response.json()
+
+        self.assertEqual(response_data["meta"]["total_count"], 2)
+        self.assertEqual(
+            {item["title"] for item in response_data["items"]},
+            {"Mouse school north east", "Mouse custom south west"},
+        )
+
     def test_teaching_resources_listing_page_returns_ordered_search_filters(self):
         self.create_resource_page(
             slug="resources-order-one",
