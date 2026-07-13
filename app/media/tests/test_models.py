1c4d71860caece5e259bb6a2c869a605b3a811d9
rzarrouk <rachelzarrouk@googlemail.com>
2026-07-13 18:13:48 +0100

    Create test_models.py

---
+++ app/media/tests/test_models.py
@@ -0,0 +1,50 @@
+import json
+
+from django.db import connection
+from django.test import TestCase
+
+from app.media.models import EtnaMedia, MediaChapterSectionBlock
+
+
+class TestMediaChapterSectionBlock(TestCase):
+    def test_chapter_times_are_stored_as_seconds_but_rendered_as_hhmmss(self):
+        media = EtnaMedia.objects.create(
+            title="Test media",
+            file="media/test.mp4",
+            type="video",
+            duration=10,
+            width=1920,
+            height=1080,
+            thumbnail="media/test.jpg",
+            chapters=[
+                (
+                    "chapter",
+                    {
+                        "time": "01:02:03",
+                        "heading": "Intro",
+                        "transcript": "",
+                    },
+                )
+            ],
+        )
+
+        media.refresh_from_db()
+        chapter_value = media.chapters[0].value
+
+        self.assertEqual(chapter_value["time"], "01:02:03")
+        self.assertEqual(
+            MediaChapterSectionBlock().get_form_state(chapter_value)["time"],
+            "01:02:03",
+        )
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                f"SELECT chapters FROM {EtnaMedia._meta.db_table} WHERE id = %s",
+                [media.id],
+            )
+            stored_value = cursor.fetchone()[0]
+
+        if isinstance(stored_value, str):
+            stored_value = json.loads(stored_value)
+
+        self.assertEqual(stored_value[0]["value"]["time"], 3723)
