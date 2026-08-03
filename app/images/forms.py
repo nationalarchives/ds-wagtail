22a1e933885234ccb9772d218c2a6ffb5037f492
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-08-03 15:04:03 +0100

    CustomImageAdminForm to override "accept" value for forms.FileInput

---
+++ app/images/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from wagtail.images.forms import BaseImageForm
+
+
+class CustomImageAdminForm(BaseImageForm):
+    """
+    Custom form for the Wagtail admin interface to handle the alternative_format field, to provide the "accept" attribute for the file input,allowing only specified file types to be selected in the file dialog.
+    """
+
+    class Meta(BaseImageForm.Meta):
+        widgets = {
+            **BaseImageForm.Meta.widgets,
+            "alternative_format": forms.FileInput(attrs={"accept": ".csv,.xlsx,.xls"}),
+        }
