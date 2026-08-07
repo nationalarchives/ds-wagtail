c36b9d3c15e8c2a776cc4515de92db5084b6d119
rzarrouk <rachelzarrouk@googlemail.com>
2026-08-07 12:01:06 +0100

    Revert "Fix wagtail migrations?"
    This reverts commit 8fd587bad237364d54bc923f418f49562513e157.

--- app/users/migrations/0002_enforce_unique_user_email_for_login.py
+++ app/users/migrations/0002_enforce_unique_user_email_for_login.py
@@ -47,6 +47,7 @@ def remove_email_uniqueness(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
+        ("users", "0001_create_beta_testers_group"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
