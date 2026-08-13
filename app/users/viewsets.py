56e8ffb6483c92c93547577cffb6c9201f082967
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-08-13 16:38:45 +0100

    Create CustomUserViewSet

---
+++ app/users/viewsets.py
@@ -0,0 +1,10 @@
+from wagtail.users.views.users import UserViewSet as WagtailUserViewSet
+
+from .forms import CustomUserCreationForm, CustomUserEditForm
+
+
+class CustomUserViewSet(WagtailUserViewSet):
+    def get_form_class(self, for_update=False):
+        if for_update:
+            return CustomUserEditForm
+        return CustomUserCreationForm
