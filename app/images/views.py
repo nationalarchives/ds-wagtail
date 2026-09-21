e5fdcd28c30b7d66e25ec881bcc960f1d78a3b0a
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-09-21 12:08:58 +0100

    Add columns attribute to define table in report

--- app/images/views.py
+++ app/images/views.py
@@ -1,3 +1,4 @@
+from wagtail.admin.ui.tables import Column, TitleColumn
 from wagtail.admin.views.reports import ReportView
 from .models import CustomImage
 
@@ -8,6 +9,12 @@ class ImagesWithNoAltTextReport(ReportView):
     header_icon = "image"
     page_title = "Images with no alt text"
     template_name = "reports/images_with_no_alt_text_report.html"
+    results_template_name = "reports/_list_images_with_no_alt_text.html"
+    columns = [
+        TitleColumn("title", label="Title", url_name="wagtailimages:edit", sort_key="title"),
+        Column("usage_count", label="Usage count"),
+        Column("created_at", label="Created at"),
+    ]
 
     def get_queryset(self):
         return CustomImage.objects.filter(description="")
