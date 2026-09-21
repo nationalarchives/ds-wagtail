dbd9cc45747566e580999c976a7fa945328821b4
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-09-21 14:40:07 +0100

    Add `collection` column and remove reference to templates

--- app/images/views.py
+++ app/images/views.py
@@ -9,12 +9,11 @@ class ImagesWithNoAltTextReport(ReportView):
     index_results_url_name = "images_with_no_alt_text_report_results"
     header_icon = "image"
     page_title = "Images with no alt text"
-    template_name = "reports/images_with_no_alt_text_report.html"
-    results_template_name = "reports/_list_images_with_no_alt_text.html"
     columns = [
         TitleColumn(
             "title", label="Title", url_name="wagtailimages:edit", sort_key="title"
         ),
+        Column("collection", label="Collection"),
         Column("usage_count", label="Usage count"),
         Column("created_at", label="Created at"),
     ]
